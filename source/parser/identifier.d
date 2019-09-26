64a8d07f76c9658b844ed404636c0d52e9cb032a
drkameleon <yaniszaf@gmail.com>
2019-09-26 09:40:15 +0200

    added support for hierarchical identifiiers

---
+++ source/parser/identifier.d
@@ -0,0 +1,123 @@
+/*****************************************************************
+ * Arturo
+ * 
+ * Programming Language + Interpreter
+ * (c) 2019 Yanis Zafirópulos (aka Dr.Kameleon)
+ *
+ * @file: parser/identifier.d
+ *****************************************************************/
+
+module parser.identifier;
+
+// Imports
+
+import core.memory;
+
+import std.stdio;
+import std.conv;
+import std.array;
+import std.string;
+import std.algorithm;
+import std.regex;
+
+import value;
+
+import globals;
+
+import panic;
+
+import program;
+import compiler;
+
+import parser.expression;
+
+import var;
+
+// C Interface
+
+extern (C) {
+	void* new_IdentifierWithId(char* t) { return cast(void*)(new Identifier(to!string(t))); }
+
+	void add_IdToIdentifier(char* s, Identifier iden) { GC.addRoot(cast(void*)iden); iden.add(to!string(s)); }
+	void add_NumToIdentifier(char* l, Identifier iden) { GC.addRoot(cast(void*)iden); iden.add(to!int(to!string(l))); }
+	void add_ExprToIdentifier(Expression e, Identifier iden) { GC.addRoot(cast(void*)iden); iden.add(cast(Expression)e); }
+}
+
+// Aliases
+
+alias idPC = PathContentType.idPathContent;
+alias numPC = PathContentType.numPathContent;
+alias exprPC = PathContentType.exprPathContent;
+
+// Definitions
+
+enum PathContentType : string
+{
+	idPathContent = "id",
+	numPathContent = "num",
+	exprPathContent = "expr"
+}
+
+union PathContent
+{
+	string id;
+	long num;
+	Expression expr;
+}
+
+// Functions
+
+class Identifier {
+	PathContentType[] pathContentTypes;
+	PathContent[] pathContents;
+
+	this(string s) {
+		writeln("IN IDENTIFIER constructor: " ~ s);
+		pathContentTypes = [ idPC ];
+		PathContent pc = {s};
+		pathContents = [ pc ];
+		//pathContents = [];
+		//pathContents ~= {s};
+		//pathContents = [  ];
+	}
+
+	void add(string s) {
+		writeln("IN IDENTIFIER add: " ~ s);
+		pathContentTypes ~= idPC;
+		PathContent pc = {id:s};
+		pathContents ~= pc;
+	}
+
+	void add(int l) {
+		writeln("IN IDENTIFIER add: " ~ to!string(l));
+		pathContentTypes ~= numPC;
+		PathContent pc = {num:l};
+		pathContents ~= pc;
+	}
+
+	void add(Expression e) {
+		writeln("IN IDENTIFIER add: expr");
+		pathContentTypes ~= exprPC;
+		PathContent pc = {expr:e};
+		pathContents ~= pc;
+	}
+
+	string getId() {
+		return pathContents[0].id;
+	}
+
+	string inspect() {
+		string[] ret = [];
+		writeln("here");
+		for (size_t i=0; i<pathContentTypes.length; i++) {
+			PathContentType pct = pathContentTypes[i];
+			PathContent pc = pathContents[i];
+
+			ret ~= to!string(pct) ~ ":" ~ pc.id;
+		}
+
+		writeln("here");
+
+		return ret.join(", ");
+	}
+}
---
+++ source/parser/identifiers.d
@@ -0,0 +1,51 @@
+/*****************************************************************
+ * Arturo
+ * 
+ * Programming Language + Interpreter
+ * (c) 2019 Yanis Zafirópulos (aka Dr.Kameleon)
+ *
+ * @file: parser/identifiers.d
+ *****************************************************************/
+
+module parser.identifiers;
+
+// Imports
+
+import core.memory;
+import std.array;
+import std.stdio;
+
+import parser.identifier;
+
+import value;
+
+// C Interface
+
+extern (C) {
+	void* new_Identifiers() { return cast(void*)(new Identifiers()); }
+	void add_Identifier(Identifiers i, Identifier iden) { GC.addRoot(cast(void*)i); i.add(iden); }
+}
+
+// Functions
+
+class Identifiers {
+
+	Identifier[] lst;
+
+	this() {
+		lst = [];
+	}
+
+	void add(Identifier iden) {
+		lst ~= iden;
+	}
+
+	string inspect() {
+		string[] ret = [];
+		foreach (Identifier id; lst) {
+			ret ~= id.inspect();
+		}
+		return ret.join(", ");
+	}
+
+}
