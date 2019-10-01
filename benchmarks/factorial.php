eb560243e7f7b4940629ebb789fc82fd2fb23d12
drkameleon <yaniszaf@gmail.com>
2019-10-01 19:21:08 +0200

    added factorial benchmark

---
+++ benchmarks/factorial.php
@@ -0,0 +1,12 @@
+<?php
+$maxLimit = 1000;
+
+function factorial($x) {
+	if ($x>0) { return $x * factorial($x-1); }
+	else { return 1; }
+}
+
+foreach (range(0,$maxLimit+1) as $n) {
+	echo factorial($n)."\n";
+}
+?>
