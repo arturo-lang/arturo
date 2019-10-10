d8f9b27af61cef696e4c42c57317c8c43ed7a3f3
drkameleon <yaniszaf@gmail.com>
2019-10-10 20:01:28 +0200

    added 'createTable' method for databases + 'add' for tables

--- source/art/database.d
+++ source/art/database.d
@@ -30,11 +30,14 @@ import globals;
 
 import panic;
 
+import var;
+
 // Constants
 
 enum _ID										= "_id";
 enum _TYPE                                      = "_type";
 enum _OBJECT                                   	= "_object";
+enum _TITLE										= ":title";
 enum _PATH 										= ":path";
 
 const string[] SQLITE_RESULTSTR =
@@ -130,12 +133,55 @@ Value queryDatabase(sqlite3* db, string query) {
 	else throw new ERR_DatabaseError(SQLITE_RESULTSTR[result]);
 }
 
+Value createDatabaseTable(sqlite3* db, string title, Value fields) {
+	string query = "CREATE TABLE " ~ title ~ " (";
+	string[] items;
+	foreach (string key, Value val; fields.dictionaryKeyValues(true)) {
+		string currentField = "";
+		currentField ~= key ~ " ";
+		if (val.type==sV) {
+			currentField ~= val.content.s;
+		}
+		else if (val.type==aV) {
+			currentField ~= val.content.a[0].content.s;
+		}
+		items ~= currentField;
+	}
+	query ~= items.join(",");
+	query ~= ");";
+	return queryDatabase(db,query);
+}
+
+Value insertIntoDatabase(sqlite3* db, string title, Value fields) {
+	string query = "INSERT INTO " ~ title ~ " (";
+	string[] items;
+	foreach (string key, Value val; fields.dictionaryKeyValues(true)) {
+		items ~= key;
+	}
+	query ~= items.join(",");
+	query ~= ") VALUES (";
+
+	items = [];
+	foreach (string key, Value val; fields.dictionaryKeyValues(true)) {
+		if (val.type==sV) {
+			items ~= "'" ~  val.content.s ~ "'";
+		}
+		else if (val.type==nV) {
+			items ~= to!string(val.content.i);
+		}
+	}
+	query ~= items.join(",");
+	query ~= ");";
+
+	return queryDatabase(db,query);
+}
+
 Value openOrCreateDatabase(string path, bool open=false) {
 	sqlite3* db;
 	int result;
 
 	if (open) result = sqlite3_open_v2(toStringz(path~".db"), &db, SQLITE_OPEN_READWRITE, null);
-	else result = sqlite3_open_v2(toStringz(path~".db"), &db, SQLITE_OPEN_CREATE, null);
+	else result = sqlite3_open_v2(toStringz(path~".db"), &db, SQLITE_OPEN_CREATE | SQLITE_OPEN_READWRITE, null);
 
 	if (result==SQLITE_OK) 
 	{
@@ -146,16 +192,34 @@ Value openOrCreateDatabase(string path, bool open=false) {
 		obj[_TYPE] = new Value("database");
 
 		obj["close"] = new Value(new Func((Value vs){ 
-			sqlite3* db = cast(sqlite3*)obj["_object"].content.o;
+			sqlite3* db = cast(sqlite3*)obj[_OBJECT].content.o;
 			
 			return closeDatabase(db);
 		}));
 
 		obj["query"] = new Value(new Func((Value vs){
-			sqlite3* db = cast(sqlite3*)obj["_object"].content.o;
+			sqlite3* db = cast(sqlite3*)obj[_OBJECT].content.o;
 			string query  = vs.content.a[0].content.s;
 
 			return queryDatabase(db,query);
+		}));
+
+		obj["createTable"] = new Value(new Func((Value vs){
+			sqlite3* db = cast(sqlite3*)obj[_OBJECT].content.o;
+
+			string title = vs.content.a[0].content.s;
+			Value fields = vs.content.a[1];
+
+			obj[title] = Value.dictionary();
+			obj[title]["add"] = new Value(new Func((Value vs){ 
+				sqlite3* db = cast(sqlite3*)obj[_OBJECT].content.o;
+
+				Value fields = vs.content.a[0];
+			
+				return insertIntoDatabase(db,title,fields);
+			}));
+
+			return createDatabaseTable(db,title,fields);
 		}));
 
 		return obj;
@@ -184,3 +248,22 @@ class Database__Open_ : Func {
 		return openOrCreateDatabase(path,true);
 	}
 }
+/*
+class Database__Table : Func {
+	this(string ns="") { super(ns ~ "table","create new SQLite table using name and fields",[[sV,dV]],[dV]); }
+	override Value execute(Expressions ex, string hId=null) {
+		Value[] v = validate(ex);
+		alias title = S!(v,0);
+		Value fields = v[1];
+
+		writeln("creating new table");
+		Value obj = new Value(fields);
+
+		obj[_ID] = new Value(getUUID());
+		obj[_TYPE] = new Value("table");
+
+		obj[_TITLE] = new Value(title);
+
+		return obj;
+	}
+}*/
