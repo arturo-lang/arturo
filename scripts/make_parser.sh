53c4fa8354bc1954d34f7416bc6d1fe1646bdf90
drkameleon <yaniszaf@gmail.com>
2019-10-17 10:53:59 +0200

    added optimization flags for gcc when building the lexer and the parser

--- scripts/make_parser.sh
+++ scripts/make_parser.sh
@@ -2,8 +2,8 @@
 
 flex source/grammar/lexer.l
 bison -d source/grammar/parser.y
-gcc lex.yy.c -c
-gcc parser.tab.c -c
+gcc -O3 -Os lex.yy.c -c
+gcc -O3 -Os parser.tab.c -c
 mv lex.yy.o obj/lexer.o
 mv parser.tab.o obj/parser.o
 rm lex.yy.c parser.tab.c parser.tab.h
