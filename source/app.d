83f5a011e5d1be09bbcd5df696584f36e0329753
drkameleon <yaniszaf@gmail.com>
2019-10-19 11:27:18 +0200

    adding Val class

--- source/app.d
+++ source/app.d
@@ -21,6 +21,7 @@ import compiler;
 import globals;
 import panic;
 import repl;
+import val;
 import value;
 import versions;
 
@@ -29,6 +30,29 @@ import versions;
 extern (C) void ART_Compile(char[] s) {
     Compiler comp = new Compiler();
     comp.compileFromString(to!string(s));
+}
+
+// Utilities
+
+void runTests() {
+    import std.algorithm;
+    import std.array;
+    import std.datetime.stopwatch;
+    import std.functional;
+    import std.range;
+    import std.stdio;
+    import std.conv : to;
+
+    void f0() { auto value1 = new Value(1); auto value2 = new Value("done"); auto value3 = new Value(["one","two"]); }
+    void f1() { auto val1 = Val(1); auto val2 = Val("done"); auto val3 = Val(["one","two"]); }
+
+    auto r = benchmark!(f0, f1)(10_000_000);
+    Duration f0Result = r[0];
+    Duration f1Result = r[1]; 
+    
+    writeln("f0 : " ~ to!string(f0Result));
+    writeln("f1 : " ~ to!string(f1Result));
+
 }
 
 // Main
@@ -40,6 +64,7 @@ void main(string[] args) {
     bool versionCmd;
     bool helpCmd;
     bool dev;
+    bool test;
 
     try {
         auto cmdline = getopt(
@@ -48,12 +73,18 @@ void main(string[] args) {
             "warnings|w",   "Turn warnings ON", &warnings,
             "console|c",    "Interactive console mode (REPL)", &interactiveCmd,
             "version|v",    "Show version information", &versionCmd,
-            "dev|d",        "Show developer utility", &dev
+            "dev|d",        "Show developer utility", &dev,
+            "test|t",        "Show developer utility", &test
         );
 
         if (cmdline.helpWanted) {
             showLogo();
             defaultGetoptPrinter("\nusage: arturo [options] filename [args] ...\n", cmdline.options);
+            exit(0);
+        }
+
+        if (test) {
+            runTests();
             exit(0);
         }
 
