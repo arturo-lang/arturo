57bd72745d3d00b1c41385855f15abd9368ad162
drkameleon <yaniszaf@gmail.com>
2019-10-22 11:27:12 +0200

    cleaning up Context (almost completely)

--- source/context.d
+++ source/context.d
@@ -16,6 +16,7 @@ import std.array;
 import std.conv;
 import std.stdio;
 import std.string;
+import std.typecons;
 import std.typetuple;
 
 //import containers.hashmap;
@@ -29,46 +30,63 @@ import globals;
 import panic;
 import value;
 
-// Definitions
+alias Value[string] Context;
+//alias Tuple!(Value[string],"symbols", ContextType,"type") Context;
 
+// Globals
+
+__gshared Context NULLC = null;
+
+// Definitions
+/*
 enum ContextType : string
 {
     blockContext = "block",
     dictionaryContext = "dictionary",
-    functionContext = "function"
-}
+    functionContext = "function",
+    nullContext = "null"
+}*/
 
 // Functions
 
-class Context {
+
+/*
+struct Context {
     //HashMap!(string, Value) symbols;
     Value[string] symbols;
-    ContextType type;
+    ContextType type = ContextType.blockContext;
 
-    this(ContextType xctype=ContextType.blockContext) {
+    this(ContextType xctype) {
         type = xctype;
     }
 
+    static Context nullContext() {
+        Context ret;
+        ret.type = ContextType.nullContext;
+        return ret;
+    }*/
+/*
     Context dup() {
-        Context ret = new Context(type);
-        /*
-        writeln("DUP: context");
-        foreach (string k,Value v; symbols) {
-            ret.symbols[k] = v;
-        }*/
+        Context ret = Context();
+        
+        //writeln("DUP: context");
+        //foreach (string k,Value v; symbols) {
+        //    ret.symbols[k] = v;
+        //}
         ret.symbols = symbols.dup;
         return ret;
     }
 
     Value _getSymbol(string sym) @safe {
-        /*return symbols.get(sym,null);*/
-
-        if (sym in symbols) {
-            return symbols[sym];
-        }
-        else {
-            return null;
-        }
+        //return symbols.get(sym,null);
+        return symbols.get(sym,null);
+        
+        //if (sym in symbols) {
+        //    return symbols[sym];
+        //}
+        //else {
+        //    return null;
+        //}
     }
 
     void _setSymbol(string sym, Value v) @safe nothrow {
@@ -76,14 +94,14 @@ class Context {
     }
 
     void  _unsetSymbol(string sym) @safe nothrow {
-        /*symbols.remove(sym);*/
+        //symbols.remove(sym);
         symbols.remove(sym);
     }
 
     void _rehash() nothrow {
         symbols.rehash();
-    }
-
+    }*/
+/*
     void _inspectSymbol(string nm, Value va, bool full=false){
         if (full) {
             writeln("  Symbol : \x1B[37m\x1B[1m" ~ nm ~ "\x1B[0m");
@@ -118,5 +136,5 @@ class Context {
             }
             
         }
-    }
-}
+    }*/
+/*}*/
