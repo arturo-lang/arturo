3e1b40880ffb01ac8e9fec8fc8afc14b27679cd6
drkameleon <yaniszaf@gmail.com>
2019-10-23 18:51:35 +0200

    test main.nim

---
+++ nim/src/main.nim
@@ -0,0 +1,11 @@
+# This is just an example to get you started. A typical binary package
+# uses this file as the main entry point of the application.
+
+when isMainModule:
+  echo("Hello, World!")
+  echo("Hello, World!")
+  echo("Hello, World!")
+  echo("Hello, World!")
+  echo("Hello, World!")
+  for i in 1..10:
+    echo "boom"
