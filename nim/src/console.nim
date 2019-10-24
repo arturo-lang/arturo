b4efb598dea393f60bdac6fe60a470b8fe0cb0c5
drkameleon <yaniszaf@gmail.com>
2019-10-24 16:05:06 +0200

    added starter code for repl

--- nim/src/console.nim
+++ nim/src/console.nim
@@ -1,2 +1,52 @@
+#[*****************************************************************
+  * Arturo
+  * 
+  * Programming Language + Interpreter
+  * (c) 2019 Yanis Zafirópulos (aka Dr.Kameleon)
+  *
+  * @file: console.nim
+  *****************************************************************]#
+
+import rdstdin
+import strformat
+import strutils
+
+import version
+
+const
+    CONSOLE_PROMPT_HEAD =  "$ :" #"\x1B[38;5;208m\x1B[1m$ :"
+    CONSOLE_PROMPT_TAIL =  "> " #"\x1B[0m\x1B[38;5;208m\x1B[1m>\x1B[0m "
+
+    CONSOLE_INSTRUCTION = "\x1B[38;5;208m# Launching interactive console; Rock on.\n" &
+                          "# Type '?help' for help on console commands\x1B[0m\n"
+
+#[========================================
+   Helper functions
+  ========================================]#
+
+proc getPrompt(line: int):string =
+    return CONSOLE_PROMPT_HEAD & fmt"{line:03}" & CONSOLE_PROMPT_TAIL
+
+#[========================================
+   Methods
+  ========================================]#
+
 proc startRepl*() = 
-    echo "starting console"
+    showVersion()
+    echo CONSOLE_INSTRUCTION
+
+    var currentLine = 1
+    var currentExpression = ""
+
+    while true:
+        try:
+            currentExpression = readLineFromStdin(getPrompt(currentLine)).strip
+
+            case currentExpression
+                of "?exit": quit()
+                else: echo "got: " & currentExpression
+    
+            inc(currentLine)
+        except IOError:
+            continue
+    
