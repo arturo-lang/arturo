9de4f7821b9bc2de27ecd63ecd41c152b6a6416a
drkameleon <yaniszaf@gmail.com>
2019-10-24 16:39:55 +0200

    added mini-vs-release versioning information

--- nim/src/version.nim
+++ nim/src/version.nim
@@ -14,11 +14,16 @@ const
     ART_BUILD       = readFile("../source/resources/build.txt").strip
     ART_BUILD_DATE  = readFile("../source/resources/build_date.txt").strip
 
+when defined(mini): 
+    const ART_EDITION = "mini"
+else: 
+    const ART_EDITION = "full"
+
 #[========================================
    Methods
   ========================================]#
 
 template showVersion*() = 
-    const vers = "\x1B[32m\x1B[1mArturo " & ART_VERSION & "\x1B[0m (" & ART_BUILD_DATE & " build " & ART_BUILD & ") [" & hostCPU & "-" & hostOS & "]"
+    const vers = "\x1B[32m\x1B[1mArturo " & ART_VERSION & "\x1B[0m (" & ART_BUILD_DATE & " build " & ART_BUILD & ") [" & hostCPU & "-" & hostOS & "-" & ART_EDITION & "]"
     echo vers
     echo "(c) 2019 Yanis Zafirópulos\n"
