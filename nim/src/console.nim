1045899cb276ea2cba218640229811e9300184e8
drkameleon <yaniszaf@gmail.com>
2019-10-24 16:40:23 +0200

    added code for console commands

--- nim/src/console.nim
+++ nim/src/console.nim
@@ -10,7 +10,9 @@
 import rdstdin
 import strformat
 import strutils
+import terminal
 
+import panic
 import version
 
 const
@@ -20,12 +22,47 @@ const
     CONSOLE_INSTRUCTION = "\x1B[38;5;208m# Launching interactive console; Rock on.\n" &
                           "# Type '?help' for help on console commands\x1B[0m\n"
 
+    CONSOLE_EXIT_MSG    = "# Exiting interactive console\n"
+
+    CONSOLE_HELP_TXT    = readFile("src/rsrc/console_help.txt").replace("\\e","\e")
+
 #[========================================
    Helper functions
   ========================================]#
 
 proc getPrompt(line: int):string =
     return CONSOLE_PROMPT_HEAD & fmt"{line:03}" & CONSOLE_PROMPT_TAIL
+
+proc showInfo(symbol: string) =
+    echo "showInfo: " & symbol
+
+proc showSymbols() = 
+    echo "showSymbols"
+
+proc showFunctions() =
+    echo "showFunctions"
+
+proc showHelp() =
+    echo CONSOLE_HELP_TXT
+
+proc doRead(filePath: string) =
+    echo "doRead: " & filePath
+
+proc doWrite(filePath: string) = 
+    echo "doWrite: " & filePath
+
+proc doClear() =
+    eraseScreen()
+    setCursorPos(0,0)
+    flushFile(stdout)
+
+proc doExit() = 
+    echo CONSOLE_EXIT_MSG
+    quit()
+
+proc doCompile(script: string): string = 
+    return "=> " & script
+
 
 #[========================================
    Methods
@@ -42,11 +79,23 @@ proc startRepl*() =
         try:
             currentExpression = readLineFromStdin(getPrompt(currentLine)).strip
 
-            case currentExpression
-                of "?exit": quit()
-                else: echo "got: " & currentExpression
-    
-            inc(currentLine)
+            if currentExpression.startsWith("?"):
+                let parts = currentExpression.split(" ")
+                case parts[0]
+                    of "?info": showInfo(parts[1])
+                    of "?symbols": showSymbols()
+                    of "?functions": showFunctions()
+                    of "?read": doRead(parts[1])
+                    of "?write": doWrite(parts[1])
+                    of "?clear": doClear()
+                    of "?help": showHelp()
+                    of "?exit": doExit()
+                    else: consoleError("command '" & parts[0] & "' not found")
+            
+            else: 
+                echo doCompile(currentExpression)
+                inc(currentLine)
+
         except IOError:
             continue
     
