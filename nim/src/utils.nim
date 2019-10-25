886318c3bd1fdb76030bb94b8132769488939c40
drkameleon <yaniszaf@gmail.com>
2019-10-25 11:28:52 +0200

    added general utilities module

---
+++ nim/src/utils.nim
@@ -0,0 +1,22 @@
+#[*****************************************************************
+  * Arturo
+  * 
+  * Programming Language + Interpreter
+  * (c) 2019 Yanis Zafirópulos (aka Dr.Kameleon)
+  *
+  * @file: utils.nim
+  *****************************************************************]#
+
+import times, os, strutils
+
+#[######################################################
+    Methods
+  ======================================================]#
+
+template benchmark*(benchmarkName: string, code: untyped) =
+    block:
+        let t0 = epochTime()
+        code
+        let elapsed = epochTime() - t0
+        let elapsedStr = elapsed.formatFloat(format = ffDecimal, precision = 3)
+        echo "CPU Time [", benchmarkName, "] ", elapsedStr, "s"
