e7c9ca37cc6d4d90ece96924ed222101a8596788
drkameleon <yaniszaf@gmail.com>
2019-10-26 15:13:51 +0200

    code cleanup

--- nim/src/utils.nim
+++ nim/src/utils.nim
@@ -20,3 +20,5 @@ template benchmark*(benchmarkName: string, code: untyped) =
         let elapsed = epochTime() - t0
         let elapsedStr = elapsed.formatFloat(format = ffDecimal, precision = 3)
         echo "CPU Time [", benchmarkName, "] ", elapsedStr, "s"
+
+
