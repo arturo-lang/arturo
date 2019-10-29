2c9f589bb1de85a61b5dda4d107ba866337d9ed0
drkameleon <yaniszaf@gmail.com>
2019-10-29 19:37:17 +0100

    added capacity for command-line argument processing

--- nim/src/main.nim
+++ nim/src/main.nim
@@ -41,6 +41,7 @@ when isMainModule:
     var warnings = false
 
     var action = noAction
+    var done = false
 
     var p = initOptParser(commandLineParams(),  shortNoVal = {'w'},
                                                 longNoVal = @["warnings"])
@@ -51,41 +52,46 @@ when isMainModule:
         of ":c","console"   : action = consoleAction
         of ":h","help"      : action = helpAction
         of ":v","version"   : action = versionAction
-        else                : 
-            if p.key!="": cmdlineError("unrecognized command '" & p.key & "'")
-            else: cmdlineError("no command specified") 
-
-    p.next()
-    case action
-        of runAction:   
-            while true:
-                case p.kind
-                    of cmdEnd: break
-                    of cmdShortOption, cmdLongOption:
-                        case p.key
-                            of "i","include":   includePath = p.val
-                            of "w","warnings":  warnings = true
-                            else:               cmdlineError("run: unrecognized parameter '" & p.key & "'")
-                    of cmdArgument: scriptPath = p.key
-                p.next()
-
-            if scriptPath!="": compiler.runScript(scriptPath,includePath,warnings)
-            else: cmdlineError("run: no script specified")
-
-        of consoleAction:   
-            when not defined(mini):
-                if p.kind!=cmdEnd: cmdlineError("console: superfluous arguments given") 
-                else: console.startRepl()
+        else:
+            if p.key=="": startRepl(); done = true 
             else:
-                cmdlineError("console: not supported on 'mini' releases")
-        of helpAction:      
-            if p.kind!=cmdEnd: cmdlineError("help: superfluous arguments given") 
-            else: showHelp(); quit()
-        of versionAction:   
-            if p.kind!=cmdEnd: cmdlineError("version: superfluous arguments given")
-            else: showVersion(); quit()
-        else: 
-            cmdlineError("no command specified")   
+                if fileExists(p.key): 
+                    compiler.runScript(p.key,p.remainingArgs,includePath,warnings); done = true
+                else: cmdlineError("unrecognized command '" & p.key & "'")
+
+    if not done:                
+        case action
+            of runAction:   
+                while true:
+                    p.next()
+
+                    case p.kind
+                        of cmdEnd: break
+                        of cmdShortOption, cmdLongOption:
+                            case p.key
+                                of "i","include":   includePath = p.val
+                                of "w","warnings":  warnings = true
+                                else:               cmdlineError("run: unrecognized parameter '" & p.key & "'")
+                        of cmdArgument: scriptPath = p.key; break
+                    
+
+                if scriptPath!="": compiler.runScript(scriptPath,p.remainingArgs,includePath,warnings)
+                else: cmdlineError("run: no script specified")
+
+            of consoleAction:   
+                when not defined(mini):
+                    if p.kind!=cmdEnd: cmdlineError("console: superfluous arguments given") 
+                    else: console.startRepl()
+                else:
+                    cmdlineError("console: not supported on 'mini' releases")
+            of helpAction:      
+                if p.kind!=cmdEnd: cmdlineError("help: superfluous arguments given") 
+                else: showHelp(); quit()
+            of versionAction:   
+                if p.kind!=cmdEnd: cmdlineError("version: superfluous arguments given")
+                else: showVersion(); quit()
+            else: 
+                cmdlineError("no command specified")   
 
 #[****************************************
    This is the end,
