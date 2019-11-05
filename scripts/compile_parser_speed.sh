50a912d00535d342eb0fa53abbdfa5a2053feaf7
drkameleon <yaniszaf@gmail.com>
2019-11-05 18:19:39 +0100

    added "-F" option to lexer

--- scripts/compile_parser_speed.sh
+++ scripts/compile_parser_speed.sh
@@ -1,4 +1,4 @@
-flex src/parser/lexer_final.l
+flex -F src/parser/lexer_final.l
 bison -d src/parser/parser.y
 gcc -O4 -Ofast -flto -fno-strict-aliasing lex.yy.c -c
 gcc -O4 -Ofast -flto -fno-strict-aliasing parser.tab.c -c
