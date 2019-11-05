ccfbf81e5ab6bc2dded97367141ea6b8afa75e18
drkameleon <yaniszaf@gmail.com>
2019-11-05 13:21:24 +0100

    added append & append! functions

--- src/lib/system/collection.nim
+++ src/lib/system/collection.nim
@@ -11,6 +11,26 @@
     Functions
   ======================================================]#
 
+proc Collection_append*[F,X,V](f: F, xl: X): V {.inline.} =
+    let v = xl.validate("append", f.req)
+
+    case v[0].kind
+        of AV: result = ARR(A(0) & v[1])
+        of SV: result = STR(S(0) & S(1))
+        else: discard
+
+proc Collection_appendI*[F,X,V](f: F, xl: X): V {.inline.} =
+    let v = xl.validate("append!", f.req)
+
+    case v[0].kind
+        of AV: 
+            A(0).add(v[1])
+            result = v[0]
+        of SV: 
+            S(0).add(S(1))
+            result = v[0]
+        else: discard
+
 proc Collection_size*[F,X,V](f: F, xl: X): V {.inline.} =
     let v = xl.validate("size", f.req)
 
