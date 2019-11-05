6ff0421e5693438706f4b34806b8c3f6b4004a27
drkameleon <yaniszaf@gmail.com>
2019-11-05 14:00:36 +0100

    added map & map! to array module

--- src/lib/system/array.nim
+++ src/lib/system/array.nim
@@ -23,6 +23,18 @@ proc Array_filterI*[F,X,V](f: F, xl: X): V {.inline.} =
 
     result = v[0]
 
+proc Array_map*[F,X,V](f: F, xl: X): V {.inline.} =
+    let v = xl.validate("map", f.req)
+
+    result = ARR(A(0).map((x) => FN(1).execute(x)))
+
+proc Array_mapI*[F,X,V](f: F, xl: X): V {.inline.} =
+    let v = xl.validate("map!", f.req)
+
+    A(0).apply((x) => FN(1).execute(x))
+
+    result = v[0]
+
 proc Array_shuffle*[F,X,V](f: F, xl: X): V {.inline.} =
     let v = xl.validate("shuffle", f.req)
 
