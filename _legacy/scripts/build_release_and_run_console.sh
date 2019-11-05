25781e8485741f1093b797e9fd9a59238def45e4
drkameleon <yaniszaf@gmail.com>
2019-11-05 13:24:50 +0100

    removed _legacy D sources' folder

--- _legacy/scripts/build_release_and_run_console.sh
+++
@@ -1,1 +0,0 @@
-./scripts/build_release.sh && ./arturo -c
--- _legacy/scripts/make_parser.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-flex source/grammar/lexer.l
-bison -d source/grammar/parser.y
-gcc -O3 -Os lex.yy.c -c
-gcc -O3 -Os parser.tab.c -c
-mv lex.yy.o obj/lexer.o
-mv parser.tab.o obj/parser.o
-rm lex.yy.c parser.tab.c parser.tab.h
