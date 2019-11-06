3db83b72236b2d4e492bf610e70fc684fb3d1fc0
drkameleon <yaniszaf@gmail.com>
2019-11-06 18:10:21 +0100

    added dictionary library

---
+++ src/lib/system/dictionary.nim
@@ -0,0 +1,29 @@
+#[*****************************************************************
+  * Arturo
+  * 
+  * Programming Language + Interpreter
+  * (c) 2019 Yanis Zafirópulos (aka Dr.Kameleon)
+  *
+  * @file: lib/system/dictionary.nim
+  *****************************************************************]#
+
+#[######################################################
+    Functions
+  ======================================================]#
+
+
+#[******************************************************
+  ******************************************************
+    UnitTests
+  ******************************************************
+  ******************************************************]#
+
+# when defined(unittest):
+
+#     suite "Library: system/array":
+
+#         test "shuffle":
+#             check(not eq( callFunction("shuffle",@[ARR(@[INT(1),INT(2),INT(3),INT(4),INT(5),INT(6),INT(7),INT(8),INT(9)])]), ARR(@[INT(1),INT(2),INT(3),INT(4),INT(5),INT(6),INT(7),INT(8),INT(9)]) ))
+
+#         test "swap":
+#             check(eq( callFunction("swap",@[ARR(@[INT(1),INT(2),INT(3)]),INT(0),INT(2)]), ARR(@[INT(3),INT(2),INT(1)]) ))
