9c480f60fbd26d4f7b8b3d5870f7aed6c187e1bd
drkameleon <yaniszaf@gmail.com>
2019-11-06 19:43:25 +0100

    added hasKey, keys & values functions

--- src/lib/system/dictionary.nim
+++ src/lib/system/dictionary.nim
@@ -11,6 +11,20 @@
     Functions
   ======================================================]#
 
+proc Dictionary_hasKey*[F,X,V](f: F, xl: X): V {.inline.} =
+    let v = xl.validate("hasKey", f.req)
+
+    result = BOOL(S(1) in D(0).keys())  
+
+proc Dictionary_keys*[F,X,V](f: F, xl: X): V {.inline.} =
+    let v = xl.validate("keys", f.req)
+
+    result = STRARR(D(0).keys())
+
+proc Dictionary_values*[F,X,V](f: F, xl: X): V {.inline.} =
+    let v = xl.validate("values", f.req)
+
+    result = ARR(D(0).values())
 
 #[******************************************************
   ******************************************************
@@ -20,7 +34,7 @@
 
 # when defined(unittest):
 
-#     suite "Library: system/array":
+#     suite "Library: system/dictionary":
 
 #         test "shuffle":
 #             check(not eq( callFunction("shuffle",@[ARR(@[INT(1),INT(2),INT(3),INT(4),INT(5),INT(6),INT(7),INT(8),INT(9)])]), ARR(@[INT(1),INT(2),INT(3),INT(4),INT(5),INT(6),INT(7),INT(8),INT(9)]) ))
