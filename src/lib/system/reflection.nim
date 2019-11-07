b5aa2aeb340173722a833bc36d3a1f655967eb8b
drkameleon <yaniszaf@gmail.com>
2019-11-07 10:31:39 +0100

    add type + unittests

--- src/lib/system/reflection.nim
+++ src/lib/system/reflection.nim
@@ -16,3 +16,25 @@ proc Reflection_inspect*[F,X,V](f: F, xl: X): V {.inline.} =
 
     echo v[0].inspect()
     result = v[0]
+
+proc Reflection_type*[F,X,V](f: F, xl: X): V {.inline.} =
+    let v = xl.validate("type", f.req)
+
+    result = STR(valueKindToPrintable($(v[0].kind)))
+
+#[******************************************************
+  ******************************************************
+    UnitTests
+  ******************************************************
+  ******************************************************]#
+
+when defined(unittest):
+
+    suite "Library: system/reflection":
+
+        test "type":
+            check(eq( callFunction("type",@[INT(1)]), STR("int") ))
+            check(eq( callFunction("type",@[BIGINT("1123131313123123131231231231231")]), STR("bigInt") ))
+            check(eq( callFunction("type",@[STR("hello")]), STR("str") ))
+            check(eq( callFunction("type",@[TRUE]), STR("bool") ))
+            check(eq( callFunction("type",@[ARR(@[INT(1),INT(2),STR("boom")])]), STR("array") ))
