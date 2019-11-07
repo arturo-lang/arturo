a061d33c379c3dcaa647b6fc42b770f1216fd58f
drkameleon <yaniszaf@gmail.com>
2019-11-07 13:31:40 +0100

    migrated terminal-related functions to separate library

---
+++ src/lib/system/terminal.nim
@@ -0,0 +1,34 @@
+#[*****************************************************************
+  * Arturo
+  * 
+  * Programming Language + Interpreter
+  * (c) 2019 Yanis Zafirópulos (aka Dr.Kameleon)
+  *
+  * @file: lib/system/terminal.nim
+  *****************************************************************]#
+
+#[######################################################
+    Functions
+  ======================================================]#
+
+proc Terminal_input*[F,X,V](f: F, xl: X): V {.inline.} =
+    result = STR(readLine(stdin))
+
+proc Terminal_print*[F,X,V](f: F, xl: X): V {.inline.} =
+    let v = xl.validate("print", f.req)
+
+    echo v[0].stringify(quoted=false)
+    result = v[0]
+
+#[******************************************************
+  ******************************************************
+    UnitTests
+  ******************************************************
+  ******************************************************]#
+
+# when defined(unittest):
+
+#     suite "Library: system/core":
+
+#         test "get":
+#             check(eq( callFunction("get",@[ARR(@[INT(1),INT(2),INT(3)]),INT(0)]), INT(1) ))
