c680aae533a119e31283ef52cd489952a0aa0992
drkameleon <yaniszaf@gmail.com>
2019-11-09 14:30:53 +0100

    added `io` library

---
+++ src/lib/system/io.nim
@@ -0,0 +1,39 @@
+#[*****************************************************************
+  * Arturo
+  * 
+  * Programming Language + Interpreter
+  * (c) 2019 Yanis Zafirópulos (aka Dr.Kameleon)
+  *
+  * @file: lib/system/io.nim
+  *****************************************************************]#
+
+#[######################################################
+    Functions
+  ======================================================]#
+
+proc Io_read*[F,X,V](f: F, xl: X): V {.inline.} =
+    let v = xl.validate(f)
+
+    result = STR(readFile(S(0)))
+
+proc Io_write*[F,X,V](f: F, xl: X): V {.inline.} =
+    let v = xl.validate(f)
+
+    writeFile(S(0),S(1))
+    result = v[1]
+
+#[******************************************************
+  ******************************************************
+    UnitTests
+  ******************************************************
+  ******************************************************]#
+
+# when defined(unittest):
+
+#     suite "Library: system/dictionary":
+
+#         test "shuffle":
+#             check(not eq( callFunction("shuffle",@[ARR(@[INT(1),INT(2),INT(3),INT(4),INT(5),INT(6),INT(7),INT(8),INT(9)])]), ARR(@[INT(1),INT(2),INT(3),INT(4),INT(5),INT(6),INT(7),INT(8),INT(9)]) ))
+
+#         test "swap":
+#             check(eq( callFunction("swap",@[ARR(@[INT(1),INT(2),INT(3)]),INT(0),INT(2)]), ARR(@[INT(3),INT(2),INT(1)]) ))
