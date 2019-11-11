6a7a7d1f27f998bec5cc6dfefd89bb6726759eeb
drkameleon <yaniszaf@gmail.com>
2019-11-11 08:52:11 +0100

    playing with Flex optimization flags

--- scripts/compile_parser_speed.sh
+++ scripts/compile_parser_speed.sh
@@ -1,4 +1,4 @@
-flex -F -8 src/parser/lexer_final.l
+flex -F -8 src/parser/lexer_final.l # perhaps remove -F and add -I (interactive) ?
 bison -d src/parser/parser.y
 gcc -O4 -Ofast -flto -fno-strict-aliasing lex.yy.c -c
 gcc -O4 -Ofast -flto -fno-strict-aliasing parser.tab.c -c
