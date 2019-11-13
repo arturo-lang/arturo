5d64377e3890837aef1450bc451613baa2b39916
drkameleon <yaniszaf@gmail.com>
2019-11-13 07:45:33 +0100

    added net module + `download` function

---
+++ src/lib/system/net.nim
@@ -0,0 +1,33 @@
+#[*****************************************************************
+  * Arturo
+  * 
+  * Programming Language + Interpreter
+  * (c) 2019 Yanis Zafirópulos (aka Dr.Kameleon)
+  *
+  * @file: lib/system/math.nim
+  * @description: number-related & math operations
+  *****************************************************************]#
+
+#[######################################################
+    Functions
+  ======================================================]#
+
+proc Net_download*[F,X,V](f: F, xl: X): V {.inline.} =
+    let v = xl.validate(f)
+    
+    var client = newHttpClient()
+    result = STR(client.getContent(S(0)))
+
+#[******************************************************
+  ******************************************************
+    UnitTests
+  ******************************************************
+  ******************************************************]#
+
+# when defined(unittest):
+
+#     suite "Library: system/math":
+
+#         test "avg":
+#             check(eq( callFunction("avg",@[ARR(@[INT(2),INT(4)])]), REAL(3.0) ))
+
