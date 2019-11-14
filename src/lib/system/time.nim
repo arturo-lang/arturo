4ae19454b8fede40e829b95f2e0382257babb51a
drkameleon <yaniszaf@gmail.com>
2019-11-14 10:57:19 +0100

    added functions to `time` library

--- src/lib/system/time.nim
+++ src/lib/system/time.nim
@@ -27,6 +27,39 @@ proc Time_delay*[F,X,V](f: F, xl: X): V {.inline.} =
     sleep(I(0))
 
     result = v[0]
+
+proc Time_month*[F,X,V](f: F, xl: X): V {.inline.} =
+    let v = xl.validate(f)
+
+    var f: TimeFormat
+    if xl.list.len==2:
+        f = initTimeFormat(S(1))
+    else:
+        f = initTimeFormat("dd-MM-yyyy")
+    
+    let dt = S(0).parse(f)
+
+    result = STR($(dt.month))
+
+proc Time_now*[F,X,V](f: F, xl: X): V {.inline.} =
+    let v = xl.validate(f)
+
+    let dt = now()
+
+    if v[0].kind == SV:
+        result = STR(dt.format(S(0)))
+    else:
+        result = STR(dt.format("HH:mm:ss"))
+
+proc Time_today*[F,X,V](f: F, xl: X): V {.inline.} =
+    let v = xl.validate(f)
+
+    let dt = now()
+
+    if v[0].kind == SV:
+        result = STR(dt.format(S(0)))
+    else:
+        result = STR(dt.format("dd-MM-yyyy"))
     
 
 #[******************************************************
