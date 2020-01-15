94737c56e527968cab76fac22d01d8ef17f81ce8
drkameleon <yaniszaf@gmail.com>
2020-01-15 10:04:40 +0100

    main header file to be included by pretty much everything

---
+++ src/arturo.h
@@ -0,0 +1,84 @@
+/*****************************************************************
+ * Arturo :VM
+ * 
+ * Programming Language + Compiler
+ * (c) 2019 Yanis Zafirópulos (aka Dr.Kameleon)
+ *
+ * @file: src/include/arturo.h
+ *****************************************************************/
+
+#ifndef __ARTURO_H__
+#define __ARTURO_H__
+
+/**************************************
+  Main configuration
+ **************************************/
+
+#include "config/config.h"
+#include "config/types.h"
+
+/**************************************
+  Global includes
+ **************************************/
+
+#include <errno.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/resource.h>
+#include <sys/time.h>
+#include <sys/utsname.h>
+#include <unistd.h>
+
+#include <gmp.h>
+
+// Helpers
+
+#include "helpers/benchmark.h"
+#include "helpers/builtin.h"
+#include "helpers/debug.h"
+#include "helpers/endianess.h"
+#include "helpers/i32tos.h"
+#include "helpers/lowmath.h"
+#include "helpers/memory.h"
+#include "helpers/powersoftwo.h"
+#include "helpers/prints.h"
+#include "helpers/stoi32.h"
+
+// Core
+
+#include "core/string.h"
+#include "core/array.h"
+#include "core/bignum.h"
+
+// Repl
+
+#include "repl/repl.h"
+
+// VM
+
+#include "vm/bytecode.h"
+#include "vm/callframe.h"
+#include "vm/env.h"
+#include "vm/func.h"
+#include "vm/generator.h"
+#include "vm/objfile.h"
+#include "vm/opcodes.h"
+#include "vm/optimizer.h"
+#include "vm/panic.h"
+#include "vm/value.h"
+#include "vm/vm.h"
+
+/**************************************
+  Super-globals
+ **************************************/
+
+ByteArray*  BCode;
+ValueArray* BData;
+
+#endif
