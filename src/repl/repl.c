30500759c28a6e3804692cadb4b8295cb3cc4644
drkameleon <yaniszaf@gmail.com>
2020-01-15 10:07:46 +0100

    main REPL module

---
+++ src/repl/repl.c
@@ -0,0 +1,14 @@
+/*****************************************************************
+ * Arturo :VM
+ * 
+ * Programming Language + Compiler
+ * (c) 2019 Yanis Zafirópulos (aka Dr.Kameleon)
+ *
+ * @file: src/repl/lrepl.c
+ *****************************************************************/
+
+#include "../arturo.h"
+
+void replStart() {
+	printf("starting REPL\n");
+}
---
+++ src/repl/repl.h
@@ -0,0 +1,15 @@
+/*****************************************************************
+ * Arturo :VM
+ * 
+ * Programming Language + Compiler
+ * (c) 2019 Yanis Zafirópulos (aka Dr.Kameleon)
+ *
+ * @file: src/repl/repl.h
+ *****************************************************************/
+
+#ifndef __REPL_H__
+#define __REPL_H__
+
+void replStart();
+
+#endif
