267118aec157edbea48c75615924fe02eea6892e
drkameleon <yaniszaf@gmail.com>
2020-01-15 10:08:01 +0100

    main Version module

---
+++ src/version/version.h
@@ -0,0 +1,17 @@
+#ifndef __VERSION_H__
+#define __VERSION_H__
+
+unsigned char Version[] = {
+  0x30, 0x2e, 0x38
+};
+unsigned int Version_len = 3;
+unsigned char BuildNo[] = {
+  0x31, 0x30, 0x33, 0x38, 0x35
+};
+unsigned int BuildNo_len = 5;
+unsigned char BuildDate[] = {
+  0x31, 0x34, 0x2d, 0x4a, 0x61, 0x6e, 0x2d, 0x32, 0x30, 0x32, 0x30
+};
+unsigned int BuildDate_len = 11;
+
+#endif
