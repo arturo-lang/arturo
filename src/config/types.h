d802d3a1732eaeb8f174473cb80eba81c4767f35
drkameleon <yaniszaf@gmail.com>
2020-01-15 10:05:51 +0100

    global type definitions

---
+++ src/config/types.h
@@ -0,0 +1,20 @@
+/*****************************************************************
+ * Arturo :VM
+ * 
+ * Programming Language + Compiler
+ * (c) 2019 Yanis Zafirópulos (aka Dr.Kameleon)
+ *
+ * @file: src/config/types.h
+ *****************************************************************/
+
+#ifndef __TYPES_H__
+#define __TYPES_H__
+
+#include <stdint.h>
+
+typedef uint8_t  Byte;
+typedef uint16_t Word;
+typedef uint32_t Dword;
+typedef uint64_t Value;
+
+#endif
