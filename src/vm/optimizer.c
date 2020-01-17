9d6ce64c1f2f2382bcdc8f18818c49c957727b5f
drkameleon <yaniszaf@gmail.com>
2020-01-17 13:42:49 +0100

    added optimization for *2 & /2

--- src/vm/optimizer.c
+++ src/vm/optimizer.c
@@ -13,10 +13,34 @@
   Helpers
  **************************************/
 
+static inline void optimizeMulDivs() {
+  int IP = 0;
+    int cmd = -1;
+    OPCODE last_op;
+    int last_op_IP;
+
+    aEach(BCode,i) {
+        if (BCode->data[i]==IPUSH2) {
+            if (i+1<BCode->size) {
+                if (BCode->data[i+1]==MUL) {
+                    BCode->data[i] = IPUSH1;
+                    BCode->data[i+1] = SHL;
+                }
+                else if (BCode->data[i+1]==DIV) {
+                    BCode->data[i] = IPUSH1;
+                    BCode->data[i+1] = SHR;
+                }
+            }
+        }
+    }
+
+}
+
 /**************************************
   Methods
  **************************************/
 
-void optimizeBytecode() {	
-	printf("optimizing...\n");
+void optimizeBytecode() { 
+  printf("optimizing...\n");
+  optimizeMulDivs();
 }
