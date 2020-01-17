c91a9b5fe90dded44fd831951701b653fc92503b
drkameleon <yaniszaf@gmail.com>
2020-01-17 11:00:45 +0100

    build update (automatic)

--- src/version/version.h
+++ src/version/version.h
@@ -6,7 +6,7 @@ unsigned char Version[] = {
 };
 unsigned int Version_len = 3;
 unsigned char BuildNo[] = {
-  0x31, 0x30, 0x34, 0x38, 0x39
+  0x31, 0x30, 0x34, 0x39, 0x30
 };
 unsigned int BuildNo_len = 5;
 unsigned char BuildDate[] = {
