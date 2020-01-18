05327161adeb8783e4d022f06217739b4838f3b9
drkameleon <yaniszaf@gmail.com>
2020-01-18 14:52:59 +0100

    added main system library header

---
+++ src/lib/system.h
@@ -0,0 +1,15 @@
+/*****************************************************************
+ * Arturo :VM
+ * 
+ * Programming Language + Compiler
+ * (c) 2019-2020 Yanis Zafirópulos (aka Dr.Kameleon)
+ *
+ * @file: src/lib/system.h
+ *****************************************************************/
+
+#ifndef __LIB_SYSTEM_H__
+#define __LIB_SYSTEM_H__
+
+#include "system/generic.h"
+
+#endif
