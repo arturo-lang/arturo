1d7e87f6041df1e72d66ef6fd1b770d752c853f6
drkameleon <yaniszaf@gmail.com>
2020-01-18 13:23:53 +0100

    added basic pattern-matching macros + helper functions: optimizeMulDivs & optimizeDistributiveProperty

--- src/vm/optimizer.c
+++ src/vm/optimizer.c
@@ -9,31 +9,59 @@
 
 #include "arturo.h"
 
+#define matchesByte(POS,M)              ((POS < BCode->size) && ( \
+                                            (BCode->data[POS] == (M)) \
+                                        ))
+#define matchesWord(POS,M1,M2)          ((POS+1 < BCode->size) && ( \
+                                            (BCode->data[POS]==M1) && (BCode->data[POS+1]==M2) \
+                                        ))
+#define matchesDword(POS,M1,M2,M3,M4)   ((POS+3 < BCode->size) && ( \
+                                            (BCode->data[POS]==M1) && (BCode->data[POS+1]==M2) && \
+                                            (BCode->data[POS+2]==M3) && (BCode->data[POS+3]==M4) \
+                                        ))
+
+#define matchesByteInRange(POS,FROM,TO) ((POS < BCode->size) && \
+                                        ((BCode->data[POS]>=FROM) && (BCode->data[POS]<=TO)))
+
 /**************************************
   Helpers
  **************************************/
 
-static inline void optimizeMulDivs() {
-  int IP = 0;
-    int cmd = -1;
-    OPCODE last_op;
-    int last_op_IP;
+static inline void optimizeMulDivs(unsigned int i) {
+    // x * 2 => x << 1
+    if (matchesWord(i, IPUSH2, MUL)) {
+        BCode->data[i] = IPUSH1;
+        BCode->data[i+1] = SHL;
+    }
+    // x / 2 => x >> 1
+    else if (matchesWord(i, IPUSH2, DIV)) {
+        BCode->data[i] = IPUSH1;
+        BCode->data[i+1] = SHR;
+    }
+    // x * 1 => x
+    else if (matchesWord(i, IPUSH1, MUL)) {
+        BCode->data[i] = NOP;
+        BCode->data[i+1] = NOP;
+    }
+}
 
-    aEach(BCode,i) {
-        if (BCode->data[i]==IPUSH2) {
-            if (i+1<BCode->size) {
-                if (BCode->data[i+1]==MUL) {
-                    BCode->data[i] = IPUSH1;
-                    BCode->data[i+1] = SHL;
-                }
-                else if (BCode->data[i+1]==DIV) {
-                    BCode->data[i] = IPUSH1;
-                    BCode->data[i+1] = SHR;
-                }
+static inline void optimizeDistributiveProperty(unsigned int i) {
+    if (matchesByte(i+2,MUL) && matchesByte(i+5,MUL) && matchesByte(i+6,ADD)) {
+        if ((matchesByteInRange(i, GLOAD0, GLOAD9)   || matchesByteInRange(i, LLOAD0, LLOAD3) || matchesByteInRange(i, IPUSH0, IPUSH10)) && 
+            (matchesByteInRange(i+1, GLOAD0, GLOAD9) || matchesByteInRange(i+1, LLOAD0, LLOAD3) || matchesByteInRange(i+1, IPUSH0, IPUSH10))&&
+            (matchesByteInRange(i+3, GLOAD0, GLOAD9) || matchesByteInRange(i+3, LLOAD0, LLOAD3) || matchesByteInRange(i+3, IPUSH0, IPUSH10))&&
+            (matchesByteInRange(i+4, GLOAD0, GLOAD9) || matchesByteInRange(i, LLOAD0, LLOAD3) || matchesByteInRange(i+4, IPUSH0, IPUSH10))
+           ) {
+            if (BCode->data[i]==BCode->data[i+3]) {
+                BCode->data[i]=BCode->data[i+1];
+                BCode->data[i+1]=BCode->data[i+4];
+                BCode->data[i+2]=ADD;
+                BCode->data[i+4]=MUL;
+                BCode->data[i+5]=NOP;
+                BCode->data[i+6]=NOP;
             }
         }
     }
-
 }
 
 /**************************************
@@ -41,6 +69,8 @@ static inline void optimizeMulDivs() {
  **************************************/
 
 void optimizeBytecode() { 
-  printf("optimizing...\n");
-  optimizeMulDivs();
+    aEach(BCode, i) {
+        optimizeMulDivs(i);
+        optimizeDistributiveProperty(i);
+    }
 }
