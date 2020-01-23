e2e17e8346e8cd31235eb3c53fde121b4bcff80e
drkameleon <yaniszaf@gmail.com>
2020-01-23 19:25:25 +0100

    added checkIsPrime, getAbs & inInc

--- src/lib/system/numbers.h
+++ src/lib/system/numbers.h
@@ -10,7 +10,27 @@
 #ifndef __LIB_SYSTEM_NUMBERS_H__
 #define __LIB_SYSTEM_NUMBERS_H__
 
-#define sys_getAbs() {                             			\
+static INLINED bool isPrime(Int32 number) {
+ 	if (number < 2) return false;
+    if (number % 2 == 0) return (number == 2);
+
+    Int32 root = sqrt(number);
+    for (int i = 3; i <= root; i += 2) {
+        if (number % i == 0) return false;
+    }
+    return true;
+}
+
+#define sys_checkIsPrime() {                   					\
+	Value arg0 = popS();                           				\
+	switch (Kind(arg0)) {                          				\
+		case IV: pushS(toB(isPrime(I(arg0)))); break;  			\
+		case GV: pushS(toB(bIsPrime(G(arg0)))); break; 			\
+		default: printf("cannot get 'isprime' for value\n");	\
+	}                                              				\
+}
+
+#define sys_getAbs() {                                         \
     Value arg0 = popS();                           			\
     switch (Kind(arg0)) {                          			\
     	case IV: pushS(toI(abs(I(arg0)))); break;        	\
@@ -25,6 +45,10 @@
 		case RV: pushS(toR((float)sqrt(R(arg0)))); break;	\
 		default: printf("cannot get 'sqrt' for value\n");	\
 	}														\
+}
+
+#define sys_inInc(ARG) {\
+	ARG += 1;           \
 }
 
 #endif
