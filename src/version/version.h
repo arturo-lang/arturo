0aa5a69c99b23709a8e8dfbb4f99c0f768b117e2
drkameleon <yaniszaf@gmail.com>
2020-01-24 15:19:54 +0100

    build update (automatic)

--- src/version/version.h
+++ src/version/version.h
@@ -6,9 +6,9 @@ static unsigned char Version[] = {
 };
 static unsigned int Version_len = 3;
 static unsigned char BuildNo[] = {
-  0x34, 0x33, 0x30
+  0x31, 0x30, 0x38, 0x39, 0x37
 };
-static unsigned int BuildNo_len = 3;
+static unsigned int BuildNo_len = 5;
 static unsigned char BuildDate[] = {
   0x32, 0x34, 0x2d, 0x4a, 0x61, 0x6e, 0x2d, 0x32, 0x30, 0x32, 0x30
 };
@@ -17,7 +17,7 @@ static unsigned int BuildDate_len = 11;
 static inline void showVersion() {
 struct utsname unameData;
 uname(&unameData);
-printf("\x1B[32m\x1B[1mArturo %.*s\x1B[0m (%.*s build %s) [%s-%s]\n",3,Version,11,BuildDate,BuildNo,unameData.machine,unameData.sysname);
+printf("\x1B[32m\x1B[1mArturo %.*s\x1B[0m (%.*s build %.*s) [%s-%s]\n",3,Version,11,BuildDate,5,BuildNo,unameData.machine,unameData.sysname);
 printf("(c) 2019-2020 Yanis Zafirópulos\n");
 printf("\n");
 }
