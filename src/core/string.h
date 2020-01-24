f5b01da3e66f11d82eaac2ad0e2494c78ad975f3
drkameleon <yaniszaf@gmail.com>
2020-01-24 10:57:52 +0100

    added sCmp method for strings

--- src/core/string.h
+++ src/core/string.h
@@ -95,6 +95,42 @@ static INLINED void sPrint(String* str) {
     }
 }
 
+static INLINED int sCmp (const String* l, const String* r) {
+    unsigned int len = l->size;
+
+    int fast = len/sizeof(size_t) + 1;
+    int offset = (fast-1)*sizeof(size_t);
+    int current_block = 0;
+
+    if( len <= sizeof(size_t)){ fast = 0; }
+
+    const char* ptr0 = l->content;
+    const char* ptr1 = r->content;
+
+    size_t *lptr0 = (size_t*)ptr0;
+    size_t *lptr1 = (size_t*)ptr1;
+
+    while( current_block < fast ){
+        if( (lptr0[current_block] ^ lptr1[current_block] )){
+            int pos;
+            for(pos = current_block*sizeof(size_t); pos < len ; ++pos ){
+                if( (ptr0[pos] ^ ptr1[pos]) || (ptr0[pos] == 0) || (ptr1[pos] == 0) ){
+                    return  (int)((unsigned char)ptr0[pos] - (unsigned char)ptr1[pos]);
+                }
+            }
+        }
+        ++current_block;
+    }
+
+    while( len > offset ){
+        if( (ptr0[offset] ^ ptr1[offset] )){ 
+            return (int)((unsigned char)ptr0[offset] - (unsigned char)ptr1[offset]); 
+        }
+        ++offset;
+    }
+    return 0;
+}
+
 //-------------------------
 // Destructor
 //-------------------------
