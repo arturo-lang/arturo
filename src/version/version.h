aaf8589c1b46191124970def40eb63c39e02f2ac
drkameleon <yaniszaf@gmail.com>
2020-01-24 15:11:48 +0100

    build update (automatic)

--- src/version/version.h
+++ src/version/version.h
@@ -1,18 +1,18 @@
 #ifndef __ART_VERSION_H__
 #define __ART_VERSION_H__
 
-unsigned char Version[] = {
+static unsigned char Version[] = {
   0x30, 0x2e, 0x38
 };
-unsigned int Version_len = 3;
-unsigned char BuildNo[] = {
-  0x34, 0x32, 0x35
+static unsigned int Version_len = 3;
+static unsigned char BuildNo[] = {
+  0x34, 0x32, 0x36
 };
-unsigned int BuildNo_len = 3;
-unsigned char BuildDate[] = {
+static unsigned int BuildNo_len = 3;
+static unsigned char BuildDate[] = {
   0x32, 0x34, 0x2d, 0x4a, 0x61, 0x6e, 0x2d, 0x32, 0x30, 0x32, 0x30
 };
-unsigned int BuildDate_len = 11;
+static unsigned int BuildDate_len = 11;
 
 static inline void showVersion() {
 struct utsname unameData;
