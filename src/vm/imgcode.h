e25ac86197a2e5a94f88a22b85336365d6ba2263
drkameleon <yaniszaf@gmail.com>
2020-01-29 13:55:42 +0100

    added imgCode generation module

---
+++ src/vm/imgcode.h
@@ -0,0 +1,19 @@
+/*****************************************************************
+ * Arturo :VM
+ * 
+ * Programming Language + Compiler
+ * (c) 2019-2020 Yanis Zafirópulos (aka Dr.Kameleon)
+ *
+ * @file: src/vm/imgcode.h
+ *****************************************************************/
+
+#ifndef __IMGCODE_H__
+#define __IMGCODE_H__
+
+/**************************************
+  Methods
+ **************************************/
+
+void writeImgFile(const char* filename);
+
+#endif
