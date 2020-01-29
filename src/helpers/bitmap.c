43bb7d39ae966dcb6e9be20009dcdf9af1474778
drkameleon <yaniszaf@gmail.com>
2020-01-29 13:55:13 +0100

    added helper for bitmap generation

---
+++ src/helpers/bitmap.c
@@ -0,0 +1,51 @@
+/*****************************************************************
+ * Arturo :VM
+ * 
+ * Programming Language + Compiler
+ * (c) 2019-2020 Yanis Zafirópulos (aka Dr.Kameleon)
+ *
+ * @file: src/helpers/bitmap.c
+ *****************************************************************/
+
+#include "arturo.h"
+
+/**************************************
+  Methods
+ **************************************/
+
+int generateBitmap(const char *filename, int width, int height, unsigned char *data)
+{
+    BITMAPFILEHEADER bmp_head;
+    BITMAPINFOHEADER bmp_info;
+    int size = width * height * 3;
+
+    bmp_head.bfType = 0x4D42; // 'BM'
+    bmp_head.bfSize= size + sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER); // 24 + head + info no quad    
+    bmp_head.bfReserved1 = bmp_head.bfReserved2 = 0;
+    bmp_head.bfOffBits = bmp_head.bfSize - size;
+    // finish the initial of head
+
+    bmp_info.biSize = 40;
+    bmp_info.biWidth = width;
+    bmp_info.biHeight = height;
+    bmp_info.biPlanes = 1;
+    bmp_info.biBitCount = 24; // bit(s) per pixel, 24 is true color
+    bmp_info.biCompress = 0;
+    bmp_info.biSizeImage = size;
+    bmp_info.biXPelsPerMeter = 0;
+    bmp_info.biYPelsPerMeter = 0;
+    bmp_info.biClrUsed = 0 ;
+    bmp_info.biClrImportant = 0;
+    // finish the initial of infohead;
+
+    // copy the data
+    FILE *fp;
+    if (!(fp = fopen(filename,"wb"))) return 0;
+
+    fwrite(&bmp_head, 1, sizeof(BITMAPFILEHEADER), fp);
+    fwrite(&bmp_info, 1, sizeof(BITMAPINFOHEADER), fp);
+    fwrite(data, 1, size, fp);
+    fclose(fp);
+
+    return 1;
+}
