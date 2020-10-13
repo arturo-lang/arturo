685547fa8c7191979c5dc3fd7f0a342e78bef333
drkameleon <yaniszaf@gmail.com>
2020-10-13 17:44:17 +0200

    code cleanup + better implementation of isPrime for NormalInteger's

--- src/vm/lib/Numbers.nim
+++ src/vm/lib/Numbers.nim
@@ -10,7 +10,7 @@
 # Libraries
 #=======================================
 
-import extras/bignum, math, sequtils, sugar
+import extras/bignum, bitops, math, sequtils, sugar
 
 import vm/stack, vm/value
 
@@ -18,54 +18,71 @@ import vm/stack, vm/value
 # Helpers
 #=======================================
 
-proc isPrime*(n: uint64): bool =
-    if n in {0,1}: return false
-    if n in {2,7,61}: return true
-
-    # case n
-    #     of 0, 1: return false
-    #     of 2, 7, 61: return true
-    #     else: discard
+proc addmod*[T: SomeInteger](a, b, modulus: T): T =
+    let a_m = if a < modulus: a else: a mod modulus
+    if b == 0.T: return a_m
+    let b_m = if b < modulus: b else: b mod modulus
  
-    var
-        nm1 = n-1
-        d = nm1.int
-        s = 0
+    let b_from_m = modulus - b_m
+    if a_m >= b_from_m: return a_m - b_from_m
+    return a_m + b_m 
  
-    while d mod 2 == 0:
-        d = d shr 1
-        s += 1
+proc mulmod*[T: SomeInteger](a, b, modulus: T): T =
+    var a_m = if a < modulus: a else: a mod modulus
+    var b_m = if b < modulus: b else: b mod modulus
+    if b_m > a_m: swap(a_m, b_m)
+    while b_m > 0.T:
+        if (b_m and 1) == 1: result = addmod(result, a_m, modulus)
+        a_m = (a_m shl 1) - (if a_m >= (modulus - a_m): modulus else: 0)
+        b_m = b_m shr 1
  
-    for a in [2, 7, 61]:
-        var
-            x = 1.uint64
-            p = a.uint64
-            dr = d
+proc expmod*[T: SomeInteger](base, exponent, modulus: T): T =
+    result = 1
+    var (e, b) = (exponent, base)
+    while e > 0.T:
+        if (e and 1) == 1: result = mulmod(result, b, modulus)
+        e = e shr 1
+        b = mulmod(b, b, modulus)
  
-        while dr > 0:
-            if dr mod 2 == 1:
-                x = x * p mod n
-            p = p * p mod n
-            dr = dr shr 1
+proc miller_rabin_test[T: SomeInteger](num: T, witnesses: seq[uint64]): bool =
+    var d = num - 1
+    let (neg_one_mod, n) = (d, d)
+    d = d shr countTrailingZeroBits(d)
+    for b in witnesses:               
+        if b.T mod num == 0: continue 
+        var s = d
+        var y = expmod(b.T, d, num)
+        while s != n and y != 1 and y != neg_one_mod:
+            y = mulmod(y, y, num)
+            s = s shl 1
+        if y != neg_one_mod and (s and 1) != 1: return false
+    true
  
-        if x == 1 or x.uint64 == nm1:
-            continue
+proc selectWitnesses[T: SomeInteger](num: T): seq[uint64] =
+    if num < 341_531u:
+        result = @[9345883071009581737u64]
+    elif num < 1_050_535_501u:
+        result = @[336781006125u64, 9639812373923155u64]
+    elif num < 350_269_456_337u:
+        result = @[4230279247111683200u64, 14694767155120705706u64, 16641139526367750375u64]
+    elif num < 55_245_642_489_451u:
+        result = @[2u64, 141889084524735u64, 1199124725622454117u64, 11096072698276303650u64]
+    elif num < 7_999_252_175_582_851u:
+        result = @[2u64, 4130806001517u64, 149795463772692060u64, 186635894390467037u64, 3967304179347715805u64]
+    elif num < 585_226_005_592_931_977u:
+        result = @[2u64, 123635709730000u64, 9233062284813009u64, 43835965440333360u64, 761179012939631437u64, 1263739024124850375u64]
+    elif num.uint64 < 18_446_744_073_709_551_615u64:
+        result = @[2u64, 325, 9375, 28178, 450775, 9780504, 1795265022]  
+    else:
+        result = @[2u64, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
  
-        var r = 1
-        while true:
-            if r >= s: return false
-
-            x = x * x mod n
-
-            if x == 1: return false
-            if x.uint64 == nm1: break
-
-            inc(r)
- 
-    return true
- 
-proc isPrime*(n: int): bool =
-    n >= 0 and n.uint64.isPrime
+proc isPrime*[T: SomeInteger](n: T): bool =
+    let primes = @[2u64, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
+    if n <= primes[^1].T: return (n in primes)
+    let modp47 = 614889782588491410u
+    if gcd(n, modp47) != 1: return false
+    let witnesses = selectWitnesses(n)
+    miller_rabin_test(n, witnesses)
 
 proc pollardG*(n: var Int, m: Int) {.inline.} =
     discard mul(n,n,n)
@@ -120,21 +137,7 @@ proc factors*(n: int): seq[int] =
     result = res
 
 proc primeFactors*(n: int): seq[int] =   
-    factors(n).filter((x)=>isPrime(x)) 
-    # var res: seq[int64] = @[]
-    # var maxq = int64(floor(sqrt(float(n))))
-    # var d = 1
-    # var q: int64 = (n %% 2) and 2 or 3    # either 2 or 3, alternating
-    # while (q <= maxq) and ((n %% q) != 0):
-    #     q = getStep(d)
-    #     d += 1
-    # if q <= maxq:        
-    #     var q1: seq[int64] = primeFactors(n /% q)
-    #     var q2: seq[int64] = primeFactors(q)
-    #     res = concat(q2, q1, res)
-    # else: 
-    #     res.add(n)    
-    # result = res
+    factors(n).filter((x)=>isPrime(x.uint64)) 
 
 proc primeFactors*(num: Int): seq[Int] =
     result = @[]
@@ -345,7 +348,7 @@ template IsPrime*():untyped =
     require(opPrime)
 
     if x.iKind==NormalInteger:
-        stack.push(newBoolean(isPrime(x.i)))
+        stack.push(newBoolean(isPrime(x.i.uint64)))
     else:
         stack.push(newBoolean(probablyPrime(x.bi,10)==0))
 
