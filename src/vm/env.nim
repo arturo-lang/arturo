3f71abd4c026170753315782e1b0a207e0c66269
drkameleon <yaniszaf@gmail.com>
2020-10-15 09:52:02 +0200

    move environment preset definitions here + naming cleanup + path information added

--- src/vm/env.nim
+++ src/vm/env.nim
@@ -10,7 +10,9 @@
 # Libraries
 #=======================================
 
-import os
+import os, strutils, tables, times
+
+import vm/bytecode, vm/value
 
 #=======================================
 # Globals
@@ -20,6 +22,13 @@ var
     PathStack*  : seq[string]
     HomeDir*    : string
     TmpDir*     : string
+
+    #--------------------
+    # private
+    #--------------------
+    Arguments   : ValueArray
+    Version     : string
+    Build       : string
 
 #=======================================
 # Methods
@@ -32,8 +41,33 @@ proc addPath*(newPath: string) =
     var (dir, _, _) = splitFile(newPath)
     PathStack.add(dir)
 
-proc initEnv*() =
-    PathStack = @[]
+proc getEnvDictionary*(): ValueDict =
+    result = initOrderedTable[string,Value]()
 
+    result["arg"] = newBlock(Arguments)
+
+    result["sys"] = newDictionary({
+        "author"    : newString("Yanis Zafirópulos"),
+        "copyright" : newString("(c) 2019-2020"),
+        "version"   : newString(Version),
+        "build"     : newInteger(parseInt(Build)),
+        "buildDate" : newDate(now()),
+        "cpu"       : newString(hostCPU),
+        "os"        : newString(hostOS),
+        "builtin"   : newBlock(getBuiltins())
+    }.toOrderedTable)
+
+    result["path"] = newDictionary({
+        "current"   : newString(getCurrentDir()),
+        "home"      : newString(HomeDir),
+        "temp"      : newString(TmpDir),
+    }.toOrderedTable)
+
+proc initEnv*(arguments: ValueArray, version: string, build: string) =
+    Arguments = arguments
+    Version = version
+    Build = build
+
+    PathStack = @[]
     HomeDir = getHomeDir()
     TmpDir  = getTempDir()
