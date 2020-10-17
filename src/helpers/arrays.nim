7ecaaa4021eef5bf362f067125eb0e829fbdfe26
drkameleon <yaniszaf@gmail.com>
2020-10-17 19:55:02 +0200

    added arrays helper

---
+++ src/helpers/arrays.nim
@@ -0,0 +1,29 @@
+######################################################
+# Arturo
+# Programming Language + Bytecode VM compiler
+# (c) 2019-2020 Yanis Zafirópulos
+#
+# @file: helpers/arrays.nim
+######################################################
+
+#=======================================
+# Libraries
+#=======================================
+
+import sequtils, sugar
+
+import vm/stack, vm/value
+
+#=======================================
+# Methods
+#=======================================
+
+proc flattened*(v: Value): Value =
+    result = newBlock()
+
+    for item in v.a:
+        if item.kind==Block:
+            for subitem in flattened(item).a:
+                result.a.add(subitem)
+        else:
+            result.a.add(item)
