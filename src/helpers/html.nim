2a798500ae16958ac49541826a4f068f4e4d9a21
drkameleon <yaniszaf@gmail.com>
2020-10-19 12:21:19 +0200

    added html helper module

---
+++ src/helpers/html.nim
@@ -0,0 +1,42 @@
+######################################################
+# Arturo
+# Programming Language + Bytecode VM compiler
+# (c) 2019-2020 Yanis Zafirópulos
+#
+# @file: helpers/csv.nim
+######################################################
+
+#=======================================
+# Libraries
+#=======================================
+
+import htmlparser, sequtils, strtabs
+import sugar, tables, xmltree, unicode
+
+import vm/stack, vm/value
+
+#=======================================
+# Methods
+#=======================================
+
+proc parseHtmlNode(node: XmlNode): Value =
+    result = newDictionary()
+    if node.kind()==xnElement:
+        result.d["attrs"] = newDictionary()
+        if node.attrsLen() > 0:
+            for k,v in pairs(node.attrs()):
+                result.d["attrs"].d[k] = newString(v)
+
+        result.d["text"] = newString(node.innerText())
+        for subnode in items(node):
+            if subnode.kind()==xnElement:
+                if result.d.hasKey(subnode.tag()) and result.d[subnode.tag()].kind==Dictionary:
+                    result.d[subnode.tag()] = newBlock(@[result.d[subnode.tag()]])
+
+                if result.d.hasKey(subnode.tag()):
+                    result.d[subnode.tag()].a.add(parseHtmlNode(subnode))
+                else:
+                    result.d[subnode.tag()] = parseHtmlNode(subnode)
+
+proc parseHtmlInput*(input: string): Value =
+    parseHtmlNode(parseHtml(input)).d["html"]
