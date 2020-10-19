852ac5cbc74a4a552b9ecf434fcc318fcd0f9c36
drkameleon <yaniszaf@gmail.com>
2020-10-19 18:36:09 +0200

    moved unidecode import & unidecode to separate strings helper + added MINI switch for lighter builds

---
+++ src/helpers/strings.nim
@@ -0,0 +1,29 @@
+######################################################
+# Arturo
+# Programming Language + Bytecode VM compiler
+# (c) 2019-2020 Yanis Zafirópulos
+#
+# @file: helpers/string.nim
+######################################################
+
+#=======================================
+# Libraries
+#=======================================
+
+when not defined(MINI):
+    import unidecode
+
+import vm/stack, vm/value
+
+#=======================================
+# Methods
+#=======================================
+
+when not defined(MINI):
+    proc convertToAscii*(input: string): Value =
+        return newString(unidecode(input))
+
+else:
+    proc convertToAscii*(input: string): Value =
+        echo "- feature not supported in MINI builds"
+        return VNULL
