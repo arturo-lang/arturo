92026b4d8cdb77bb438e3b1902c40d3d704396c4
drkameleon <yaniszaf@gmail.com>
2020-10-24 11:09:19 +0200

    code cleanup

--- src/vm/stack.nim
+++ src/vm/stack.nim
@@ -58,78 +58,26 @@ template emptyStack*() =
 
 template pushAttr*(label: string, v: Value) =
     Attrs[label] = v
-    # Attrs[AP] = v
-    # AP += 1
-
-# template popAttr*(): Value =
-
-#     AP -= 1
-#     Attrs[AP]
-
-# template attrTop*(): Value =
-#     Stack[AP-1]
 
 template emptyAttrs*() =
     Attrs = initOrderedTable[string,Value]()
 
-proc printAttrs*() =
-    for k,v in pairs(Attrs):
-        echo k & " => " & $(v)
-    # var tmp = AP
-    # while tmp>0:
-    #     echo "attr: " & Attrs[tmp-1].r & " => " & $(Attrs[tmp-2])
-    #     tmp -= 2
-
 proc getAttr*(attr: string): Value =
     Attrs.getOrDefault(attr, VNULL)
-    # var tmp = AP
-    # while tmp>0:
-    #     if Attrs[tmp-1].r == attr: 
-    #         return Attrs[tmp-2]
-    #     tmp -= 2
-
-    # return VNULL
 
 proc popAttr*(attr: string): Value =
     result = Attrs.getOrDefault(attr, VNULL)
     Attrs.del(attr)
-    # if Attrs.hasKey(attr):
-    #     return 
-    # echo "trying to popAttr: " & attr
-    # var tmp = AP
-    # echo "after tmp=AP"
-    # while tmp>0:
-    #     echo "in while tmp>0"
-    #     if Attrs[tmp-1].r == attr: 
-    #         echo "- found it"
-    #         result = Attrs[tmp-2]
-    #         echo "deleting"
-    #         echo "AP=" & $(AP) & " tmp=" & $(tmp)
-    #         echo "deleting: " & $(tmp-1)
-    #         Attrs.delete(tmp-1)
-    #         echo "deleting: " & $(tmp-1)
-    #         Attrs.delete(tmp-1)
-    #         #delete(Attrs,tmp-2,tmp-1)
-    #         AP -= 2
-    #         echo "before returning: AP=" & $(AP)
-    #         #printAttrs()
-    #         return
-    #     echo "after if - before tmp-=2: " & $(tmp)
-    #     tmp -= 2
-    #     echo "after tmp-=2: " & $(tmp)
-
-    # echo "- not found"
-    # #printAttrs()
-
-    # return VNULL
 
 proc getAttrsDict*(): Value =
     result = newDictionary(Attrs)
-    # result = newDictionary()
-    # var tmp = AP
-    # while tmp>0:
-    #     result.d[Attrs[tmp-1].r] = Attrs[tmp-2]
-    
-    #     tmp -= 2
 
     emptyAttrs()
+
+# Debugging
+
+when defined(VERBOSE):
+
+    proc printAttrs*() =
+        for k,v in pairs(Attrs):
+            echo k & " => " & $(v)
