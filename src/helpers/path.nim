a7a51c15ae2710a2cefe421fc5bf4a14fdda6874
drkameleon <yaniszaf@gmail.com>
2020-10-31 14:54:59 +0100

    added path helper

---
+++ src/helpers/path.nim
@@ -0,0 +1,44 @@
+######################################################
+# Arturo
+# Programming Language + Bytecode VM compiler
+# (c) 2019-2020 Yanis Zafirópulos
+#
+# @file: helpers/url.nim
+######################################################
+
+#=======================================
+# Libraries
+#=======================================
+
+import os, tables, uri
+
+import vm/value
+
+#=======================================
+# Methods
+#=======================================
+
+proc parsePathComponents*(s: string): OrderedTable[string,Value] {.inline.} =
+    var (dir, name, ext) = splitFile(s)
+
+    let details = {
+        "directory": newString(dir),
+        "basename": newString(name & "." & ext),
+        "filename": newString(name),
+        "extension": newString(ext)
+        }.toOrderedTable
+
+proc parseUrlComponents*(s: string): OrderedTable[string,Value] {.inline.} =
+    var res = initUri()
+    parseUri(s, res)
+
+    let details = {
+        "scheme":   newString(res.scheme),
+        "host":     newString(res.hostname),
+        "port":     newString(res.port),
+        "user":     newString(res.username),
+        "password": newString(res.password),
+        "path":     newString(res.password),
+        "query":    newString(res.query),
+        "anchor":   newString(res.anchor)
+        }.toOrderedTable
