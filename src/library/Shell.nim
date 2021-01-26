ef9e7c08b229f3bce5b67f0701be73dde2bb8f18
drkameleon <yaniszaf@gmail.com>
2021-01-26 18:07:08 +0100

    migrated Shell module

--- src/library/Shell.nim
+++ src/library/Shell.nim
@@ -3,60 +3,69 @@
 # Programming Language + Bytecode VM compiler
 # (c) 2019-2020 Yanis Zafirópulos
 #
-# @file: library/Core.nim
+# @file: library/Shell.nim
 ######################################################
-
-#=======================================
-# Libraries
-#=======================================
-
-import vm/stack, vm/value
 
 #=======================================
 # Methods
 #=======================================
 
-template Execute*():untyped =
-    # EXAMPLE:
-    # print execute "pwd"
-    # ; /Users/admin/Desktop
-    #
-    # split.lines execute "ls"
-    # ; => ["tests" "var" "data.txt"]
+builtin "execute",
+    alias       = unaliased, 
+    description = "execute given shell command",
+    args        = {
+        "command"   : {String}
+    },
+    attrs       = NoAttrs,
+    returns     = {String},
+    example     = """
+        print execute "pwd"
+        ; /Users/admin/Desktop
+        
+        split.lines execute "ls"
+        ; => ["tests" "var" "data.txt"]
+    """:
+        ##########################################################
+        let res = execCmdEx(x.s)
+        
+        stack.push(newString(res[0]))
 
-    require(opExecute)
-
-    let res = execCmdEx(x.s)
-    
-    stack.push(newString(res[0]))
-
-template List*():untyped =
-    # EXAMPLE:
-    # loop list "." 'file [
-    # ___print file
-    # ]
-    # 
-    # ; ./tests
-    # ; ./var
-    # ; ./data.txt
-    #
-    # loop list.relative "tests" 'file [
-    # ___print file
-    # ]
-    # 
-    # ; test1.art
-    # ; test2.art
-    # ; test3.art
-
-    require(opList)
-
-    if (let aSelect = popAttr("select"); aSelect != VNULL):
-        if (popAttr("relative") != VNULL):
-            stack.push(newStringBlock((toSeq(walkDir(x.s, relative=true)).map((x)=>x[1])).filter((x) => x.contains aSelect.s)))
+builtin "list",
+    alias       = unaliased, 
+    description = "get files at given path",
+    args        = {
+        "path"  : {String}
+    },
+    attrs       = {
+        "select"    : ({String},"select files satisfying given pattern"),
+        "relative"  : ({Boolean},"get relative paths")
+    },
+    returns     = {Block},
+    example     = """
+        loop list "." 'file [
+        ___print file
+        ]
+        
+        ; ./tests
+        ; ./var
+        ; ./data.txt
+        
+        loop list.relative "tests" 'file [
+        ___print file
+        ]
+        
+        ; test1.art
+        ; test2.art
+        ; test3.art
+    """:
+        ##########################################################
+        if (let aSelect = popAttr("select"); aSelect != VNULL):
+            if (popAttr("relative") != VNULL):
+                stack.push(newStringBlock((toSeq(walkDir(x.s, relative=true)).map((x)=>x[1])).filter((x) => x.contains aSelect.s)))
+            else:
+                stack.push(newStringBlock((toSeq(walkDir(x.s)).map((x)=>x[1])).filter((x) => x.contains aSelect.s)))
         else:
-            stack.push(newStringBlock((toSeq(walkDir(x.s)).map((x)=>x[1])).filter((x) => x.contains aSelect.s)))
-    else:
-        if (popAttr("relative") != VNULL):
-            stack.push(newStringBlock(toSeq(walkDir(x.s, relative=true)).map((x)=>x[1])))
-        else:
-            stack.push(newStringBlock(toSeq(walkDir(x.s)).map((x)=>x[1])))
+            if (popAttr("relative") != VNULL):
+                stack.push(newStringBlock(toSeq(walkDir(x.s, relative=true)).map((x)=>x[1])))
+            else:
+                stack.push(newStringBlock(toSeq(walkDir(x.s)).map((x)=>x[1])))
