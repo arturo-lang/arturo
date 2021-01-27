b5d5c7a348e4d9b0f0e1d5f72c2dc2daff3195e4
drkameleon <yaniszaf@gmail.com>
2021-01-27 19:48:52 +0100

    created new VM module for error handling

---
+++ src/vm/errors.nim
@@ -0,0 +1,24 @@
+######################################################
+# Arturo
+# Programming Language + Bytecode VM compiler
+# (c) 2019-2020 Yanis Zafirópulos
+#
+# @file: vm/errors.nim
+######################################################
+
+#=======================================
+# Libraries
+#=======================================
+
+import vm/value
+
+#=======================================
+# Templates
+#=======================================
+
+template showConversionError*():untyped =
+    echo "cannot convert argument of type :" & ($(y.kind)).toLowerAscii() & " to :" & ($(x.t)).toLowerAscii()
+
+template invalidConversionError*(origin: string):untyped =
+    echo "cannot convert " & origin & " to :" & ($(x.t)).toLowerAscii()
+
