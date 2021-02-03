42de6bb110353a87a3b21fe2a41148b9d0aeb19f
drkameleon <yaniszaf@gmail.com>
2021-02-03 17:40:52 +0100

    commented-out obsolete code

--- src/vm/bytecode.nim
+++ src/vm/bytecode.nim
@@ -2274,40 +2274,40 @@ const
 # Methods
 #=======================================
 
-proc writeBytecode*(trans: Translation, target: string): bool =
-    let marshaled = $$(trans[0])
-    let bcode = trans[1]
+# proc writeBytecode*(trans: Translation, target: string): bool =
+#     let marshaled = $$(trans[0])
+#     let bcode = trans[1]
 
-    var f = newFileStream(target, fmWrite)
-    if not f.isNil:
-        f.write(len(marshaled))
-        f.write(marshaled)
-        f.write(len(bcode))
-        for b in bcode:
-            f.write(b)
-        f.flush
+#     var f = newFileStream(target, fmWrite)
+#     if not f.isNil:
+#         f.write(len(marshaled))
+#         f.write(marshaled)
+#         f.write(len(bcode))
+#         for b in bcode:
+#             f.write(b)
+#         f.flush
 
-        return true
-    else:
-        return false
+#         return true
+#     else:
+#         return false
 
-proc readBytecode*(origin: string): Translation =
-    var f = newFileStream(origin, fmRead)
-    if not f.isNil:
-        var s: int
-        f.read(s)           # read constants size
-        var t: string
-        f.readStr(s,t)      # read the marshaled constants
+# proc readBytecode*(origin: string): Translation =
+#     var f = newFileStream(origin, fmRead)
+#     if not f.isNil:
+#         var s: int
+#         f.read(s)           # read constants size
+#         var t: string
+#         f.readStr(s,t)      # read the marshaled constants
 
-        f.read(s)           # read bytecode size
+#         f.read(s)           # read bytecode size
 
-        var bcode: ByteArray = newSeq[byte](s)
-        var indx = 0
-        while not f.atEnd():
-            bcode[indx] = f.readUint8()         # read bytes one-by-one
-            indx += 1
+#         var bcode: ByteArray = newSeq[byte](s)
+#         var indx = 0
+#         while not f.atEnd():
+#             bcode[indx] = f.readUint8()         # read bytes one-by-one
+#             indx += 1
 
-        return (t.to[:ValueArray], bcode)       # return the Translation
+#         return (t.to[:ValueArray], bcode)       # return the Translation
 
 proc hash*(x: OpCode): Hash {.inline.}=
     cast[Hash](ord(x))
