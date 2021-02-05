ab15b3fe9d5db5565c639929f6ed0b45ebed66cd
drkameleon <yaniszaf@gmail.com>
2021-02-05 08:00:17 +0100

    removed `eval` function (not needed for now, or needing more complete implementation?)

--- src/library/Reflection.nim
+++ src/library/Reflection.nim
@@ -259,26 +259,6 @@ proc defineSymbols*() =
             ##########################################################
             stack.push(newBoolean(x.kind==Dictionary))
 
-    builtin "eval",
-        alias       = unaliased, 
-        rule        = PrefixPrecedence,
-        description = "evaluate given block into bytecode",
-        args        = {
-            "code"  : {Block}
-        },
-        attrs       = NoAttrs,
-        returns     = {Dictionary},
-        example     = """
-        """:
-            ##########################################################
-            var ret: ValueDict = initOrderedTable[string,Value]()
-
-            let evaled = doEval(x)
-            ret["constants"] = newBlock(evaled[0])
-            ret["bytecode"] = newIntegerBlock(evaled[1])
-
-            stack.push(newDictionary(ret))
-
     builtin "help",
         alias       = unaliased, 
         rule        = PrefixPrecedence,
