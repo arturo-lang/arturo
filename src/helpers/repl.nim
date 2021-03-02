ac88fb8e7193a81402edc5eb4cfd0679691cac15
drkameleon <yaniszaf@gmail.com>
2021-03-02 13:17:34 +0100

    added conditional compilation (NOT to be compiled for Windows - linenoise not supported)

--- src/helpers/repl.nim
+++ src/helpers/repl.nim
@@ -14,103 +14,105 @@ import os, sequtils, strutils, sugar, tables
 
 import vm/value
 
-#=======================================
-# Types
-#=======================================
+when not defined(windows):
 
-type
-    LinenoiseCompletions* = object
-        len*: csize_t
-        cvec*: cstringArray
+    #=======================================
+    # Types
+    #=======================================
 
-    LinenoiseCompletionCallback*    = proc (buf: cstring; lc: ptr LinenoiseCompletions) {.cdecl.}
-    LinenoiseHintsCallback*         = proc (buf: cstring; color: var cint; bold: var cint): cstring {.cdecl.}
-    LinenoiseFreeHintsCallback*     = proc (buf: cstring; color: var cint; bold: var cint) {.cdecl.}
+    type
+        LinenoiseCompletions* = object
+            len*: csize_t
+            cvec*: cstringArray
 
-#=======================================
-# Constants
-#=======================================
+        LinenoiseCompletionCallback*    = proc (buf: cstring; lc: ptr LinenoiseCompletions) {.cdecl.}
+        LinenoiseHintsCallback*         = proc (buf: cstring; color: var cint; bold: var cint): cstring {.cdecl.}
+        LinenoiseFreeHintsCallback*     = proc (buf: cstring; color: var cint; bold: var cint) {.cdecl.}
 
-const
-    ReplHistoryPath = joinPath(joinPath(getHomeDir(), ".arturo"), "history.txt")
+    #=======================================
+    # Constants
+    #=======================================
 
-#=======================================
-# Variables
-#=======================================
+    const
+        ReplHistoryPath = joinPath(joinPath(getHomeDir(), ".arturo"), "history.txt")
 
-var
-    ReplInitialized = false
+    #=======================================
+    # Variables
+    #=======================================
 
-#=======================================
-# C Imports
-#=======================================
+    var
+        ReplInitialized = false
 
-proc linenoiseSetCompletionCallback*(cback: ptr LinenoiseCompletionCallback) {.importc: "linenoiseSetCompletionCallback".}
-proc linenoiseSetHintsCallback(cback: ptr LinenoiseHintsCallback) {.importc: "linenoiseSetHintsCallback".}
-proc linenoiseAddCompletion*(a2: ptr LinenoiseCompletions; a3: cstring) {.importc: "linenoiseAddCompletion".}
-proc linenoiseReadLine*(prompt: cstring): cstring {.importc: "linenoise".}
-proc linenoiseHistoryAdd*(line: cstring): cint {.importc: "linenoiseHistoryAdd", discardable.}
-proc linenoiseHistorySetMaxLen*(len: cint): cint {.importc: "linenoiseHistorySetMaxLen".}
-proc linenoiseHistorySave*(filename: cstring): cint {.importc: "linenoiseHistorySave".}
-proc linenoiseHistoryLoad*(filename: cstring): cint {.importc: "linenoiseHistoryLoad".}
-proc linenoiseClearScreen*() {.importc: "linenoiseClearScreen".}
-proc linenoiseSetMultiLine*(ml: cint) {.importc: "linenoiseSetMultiLine".}
-proc linenoisePrintKeyCodes*() {.importc: "linenoisePrintKeyCodes".}
-proc free*(s: cstring) {.importc: "free", header: "<stdlib.h>".}
+    #=======================================
+    # C Imports
+    #=======================================
 
-#=======================================
-# C Exports
-#=======================================
+    proc linenoiseSetCompletionCallback*(cback: ptr LinenoiseCompletionCallback) {.importc: "linenoiseSetCompletionCallback".}
+    proc linenoiseSetHintsCallback(cback: ptr LinenoiseHintsCallback) {.importc: "linenoiseSetHintsCallback".}
+    proc linenoiseAddCompletion*(a2: ptr LinenoiseCompletions; a3: cstring) {.importc: "linenoiseAddCompletion".}
+    proc linenoiseReadLine*(prompt: cstring): cstring {.importc: "linenoise".}
+    proc linenoiseHistoryAdd*(line: cstring): cint {.importc: "linenoiseHistoryAdd", discardable.}
+    proc linenoiseHistorySetMaxLen*(len: cint): cint {.importc: "linenoiseHistorySetMaxLen".}
+    proc linenoiseHistorySave*(filename: cstring): cint {.importc: "linenoiseHistorySave".}
+    proc linenoiseHistoryLoad*(filename: cstring): cint {.importc: "linenoiseHistoryLoad".}
+    proc linenoiseClearScreen*() {.importc: "linenoiseClearScreen".}
+    proc linenoiseSetMultiLine*(ml: cint) {.importc: "linenoiseSetMultiLine".}
+    proc linenoisePrintKeyCodes*() {.importc: "linenoisePrintKeyCodes".}
+    proc free*(s: cstring) {.importc: "free", header: "<stdlib.h>".}
 
-var
-    completions {.exportc.} : ValueArray
-    hints       {.exportc.} : ValueDict
+    #=======================================
+    # C Exports
+    #=======================================
 
-#=======================================
-# Helpers
-#=======================================
+    var
+        completions {.exportc.} : ValueArray
+        hints       {.exportc.} : ValueDict
 
-proc initRepl*(path: string, completionsArray: ValueArray = @[], hintsTable: ValueDict = initOrderedTable[string,Value]()) =
-    completions = completionsArray
-    hints = hintsTable
+    #=======================================
+    # Helpers
+    #=======================================
 
-    proc completionsCback(buf: cstring; lc: ptr LinenoiseCompletions) {.cdecl.} =
-        for item in completions.map((x) => x.s).filter((x) => x.startsWith($(buf))):
-            linenoiseAddCompletion(lc, item.cstring)
+    proc initRepl*(path: string, completionsArray: ValueArray = @[], hintsTable: ValueDict = initOrderedTable[string,Value]()) =
+        completions = completionsArray
+        hints = hintsTable
+
+        proc completionsCback(buf: cstring; lc: ptr LinenoiseCompletions) {.cdecl.} =
+            for item in completions.map((x) => x.s).filter((x) => x.startsWith($(buf))):
+                linenoiseAddCompletion(lc, item.cstring)
 
 
-    proc hintsCback(buf: cstring; color: var cint; bold: var cint): cstring {.cdecl.} =
-        if hints.hasKey($(buf)):
-            color = 35
-            bold = 0
-            return " " & hints[$(buf)].s
-        return nil
-           
-    if not ReplInitialized:
-        if not fileExists(parentDir(path)):
-            createDir(parentDir(path))
-        discard linenoiseHistoryLoad(path)
+        proc hintsCback(buf: cstring; color: var cint; bold: var cint): cstring {.cdecl.} =
+            if hints.hasKey($(buf)):
+                color = 35
+                bold = 0
+                return " " & hints[$(buf)].s
+            return nil
+            
+        if not ReplInitialized:
+            if not fileExists(parentDir(path)):
+                createDir(parentDir(path))
+            discard linenoiseHistoryLoad(path)
 
-        linenoiseSetCompletionCallback(cast[ptr LinenoiseCompletionCallback](completionsCback))
-        linenoiseSetHintsCallback(cast[ptr LinenoiseHintsCallback](hintsCback))
+            linenoiseSetCompletionCallback(cast[ptr LinenoiseCompletionCallback](completionsCback))
+            linenoiseSetHintsCallback(cast[ptr LinenoiseHintsCallback](hintsCback))
 
-        ReplInitialized = true
+            ReplInitialized = true
 
-#=======================================
-# Methods
-#=======================================
+    #=======================================
+    # Methods
+    #=======================================
 
-proc replInput*(
-    prompt: string, 
-    historyPath: string = ReplHistoryPath, 
-    completionsArray: ValueArray = @[],
-    hintsTable: ValueDict = initOrderedTable[string,Value]()
-): string =
-    initRepl(historyPath, completionsArray, hintsTable)
+    proc replInput*(
+        prompt: string, 
+        historyPath: string = ReplHistoryPath, 
+        completionsArray: ValueArray = @[],
+        hintsTable: ValueDict = initOrderedTable[string,Value]()
+    ): string =
+        initRepl(historyPath, completionsArray, hintsTable)
 
-    let got = linenoiseReadLine(prompt.cstring)
-    linenoiseHistoryAdd(got)
-    discard linenoiseHistorySave(historyPath)
-    result = $(got)
+        let got = linenoiseReadLine(prompt.cstring)
+        linenoiseHistoryAdd(got)
+        discard linenoiseHistorySave(historyPath)
+        result = $(got)
 
-    free(got)
+        free(got)
