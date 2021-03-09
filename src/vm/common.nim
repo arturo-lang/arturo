23a2e888edddebf2d018bbf76c1f056fc1ac37a2
drkameleon <yaniszaf@gmail.com>
2021-03-09 14:24:34 +0100

    omit example data from MINI builds

--- src/vm/common.nim
+++ src/vm/common.nim
@@ -36,7 +36,11 @@ template builtin*(n: string, alias: SymbolKind, rule: PrecedenceKind, descriptio
     else:                               
         const argsLen = static args.len
 
-    const cleanExample = replace(strutils.strip(example),"\n            ","\n")
+    when defined(MINI):
+        const cleanExample = ""
+    else:
+        const cleanExample = replace(strutils.strip(example),"\n            ","\n")
+        
     let b = newBuiltin(n, alias, rule, static (instantiationInfo().filename).replace(".nim"), description, static argsLen, args.toOrderedTable, attrs.toOrderedTable, returns, cleanExample, proc () =
         require(n, args)
         act
