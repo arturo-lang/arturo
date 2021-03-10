3d0871a2b8b01903542d9263ea369fe8a05c9bfa
drkameleon <yaniszaf@gmail.com>
2021-03-10 07:20:06 +0100

    commented out to fix Declared but not used

--- src/extras/markdown.nim
+++ src/extras/markdown.nim
@@ -772,8 +772,8 @@ method parse*(this: UlParser, doc: string, start: int): ParseResult =
 
   return ParseResult(token: ulToken, pos: pos)
 
-proc parseUnorderedList(doc: string, start: int): ParseResult {.locks: "unknown".} =
-  UlParser().parse(doc, start)
+# proc parseUnorderedList(doc: string, start: int): ParseResult {.locks: "unknown".} =
+#   UlParser().parse(doc, start)
 
 method parse*(this: OlParser, doc: string, start: int): ParseResult =
   var pos = start
