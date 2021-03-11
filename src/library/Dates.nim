83d2b4b1455452a36f8290e062e1b00c6175316d
drkameleon <yaniszaf@gmail.com>
2021-03-11 13:03:10 +0100

    implemented `after` & `before` (for adding and subtracting intervals to given date)

--- src/library/Dates.nim
+++ src/library/Dates.nim
@@ -32,34 +32,112 @@ proc defineSymbols*() =
     builtin "after",
         alias       = unaliased, 
         rule        = PrefixPrecedence,
-        description = "get date by adding given interval",
+        description = "get date after given one using interval",
         args        = {
-            "date"  : {Date}
+            "date"  : {Literal, Date}
         },
-        attrs       = NoAttrs,
+        attrs       = {
+            "nanoseconds"   : ({Integer},"add given number of nanoseconds"),
+            "milliseconds"  : ({Integer},"add given number of milliseconds"),
+            "seconds"       : ({Integer},"add given number of seconds"),
+            "minutes"       : ({Integer},"add given number of minutes"),
+            "hours"         : ({Integer},"add given number of hours"),
+            "days"          : ({Integer},"add given number of days"),
+            "weeks"         : ({Integer},"add given number of weeks"),
+            "months"        : ({Integer},"add given number of months"),
+            "years"         : ({Integer},"add given number of years")
+        },
         returns     = {Date},
         # TODO(Dates\after) add example for documentation
         #  labels: library,documentation,easy
         example     = """
         """:
             ##########################################################
-            discard
+            var nanoseconds, milliseconds, seconds,
+                minutes, hours, days, weeks,
+                months, years = 0
+
+            if (let aNanoseconds = popAttr("nanoseconds"); aNanoseconds != VNULL):      nanoseconds = aNanoseconds.i
+            if (let aMilliseconds = popAttr("milliseconds"); aMilliseconds != VNULL):   milliseconds = aMilliseconds.i
+            if (let aSeconds = popAttr("seconds"); aSeconds != VNULL):                  seconds = aSeconds.i
+            if (let aMinutes = popAttr("minutes"); aMinutes != VNULL):                  minutes = aMinutes.i
+            if (let aHours = popAttr("hours"); aHours != VNULL):                        hours = aHours.i
+            if (let aDays = popAttr("days"); aDays != VNULL):                           days = aDays.i
+            if (let aWeeks = popAttr("weeks"); aWeeks != VNULL):                        weeks = aWeeks.i
+            if (let aMonths = popAttr("months"); aMonths != VNULL):                     months = aMonths.i
+            if (let aYears = popAttr("years"); aYears != VNULL):                        years = aYears.i
+
+            let ti = initTimeInterval(
+                nanoseconds=nanoseconds, 
+                milliseconds=milliseconds,
+                seconds=seconds,
+                minutes=minutes,
+                hours=hours,
+                days=days,
+                weeks=weeks,
+                months=months,
+                years=years
+            )
+            
+            if x.kind==Literal:
+                SetInPlace(newDate(InPlace.eobj + ti))
+            else:
+                stack.push(newDate(x.eobj + ti))
 
     builtin "before",
         alias       = unaliased, 
         rule        = PrefixPrecedence,
-        description = "get date by subtracting given interval",
+        description = "get date before given one using interval",
         args        = {
             "date"  : {Date}
         },
-        attrs       = NoAttrs,
+        attrs       = {
+            "nanoseconds"   : ({Integer},"subtract given number of nanoseconds"),
+            "milliseconds"  : ({Integer},"subtract given number of milliseconds"),
+            "seconds"       : ({Integer},"subtract given number of seconds"),
+            "minutes"       : ({Integer},"subtract given number of minutes"),
+            "hours"         : ({Integer},"subtract given number of hours"),
+            "days"          : ({Integer},"subtract given number of days"),
+            "weeks"         : ({Integer},"subtract given number of weeks"),
+            "months"        : ({Integer},"subtract given number of months"),
+            "years"         : ({Integer},"subtract given number of years")
+        },
         returns     = {Date},
         # TODO(Dates\before) add example for documentation
         #  labels: library,documentation,easy
         example     = """
         """:
             ##########################################################
-            discard
+            var nanoseconds, milliseconds, seconds,
+                minutes, hours, days, weeks,
+                months, years = 0
+
+            if (let aNanoseconds = popAttr("nanoseconds"); aNanoseconds != VNULL):      nanoseconds = aNanoseconds.i
+            if (let aMilliseconds = popAttr("milliseconds"); aMilliseconds != VNULL):   milliseconds = aMilliseconds.i
+            if (let aSeconds = popAttr("seconds"); aSeconds != VNULL):                  seconds = aSeconds.i
+            if (let aMinutes = popAttr("minutes"); aMinutes != VNULL):                  minutes = aMinutes.i
+            if (let aHours = popAttr("hours"); aHours != VNULL):                        hours = aHours.i
+            if (let aDays = popAttr("days"); aDays != VNULL):                           days = aDays.i
+            if (let aWeeks = popAttr("weeks"); aWeeks != VNULL):                        weeks = aWeeks.i
+            if (let aMonths = popAttr("months"); aMonths != VNULL):                     months = aMonths.i
+            if (let aYears = popAttr("years"); aYears != VNULL):                        years = aYears.i
+
+            let ti = initTimeInterval(
+                nanoseconds=nanoseconds, 
+                milliseconds=milliseconds,
+                seconds=seconds,
+                minutes=minutes,
+                hours=hours,
+                days=days,
+                weeks=weeks,
+                months=months,
+                years=years
+            )
+            
+            if x.kind==Literal:
+                SetInPlace(newDate(InPlace.eobj - ti))
+            else:
+                stack.push(newDate(x.eobj - ti))
 
     builtin "leap?",
         alias       = unaliased, 
