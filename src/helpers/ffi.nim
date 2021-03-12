6668a51f98445405071f5546d3b82674687936bb
drkameleon <yaniszaf@gmail.com>
2021-03-12 17:48:11 +0100

    added initial implementation

--- src/helpers/ffi.nim
+++ src/helpers/ffi.nim
@@ -10,7 +10,7 @@
 # Libraries
 #=======================================
 
-import dynlib
+import dynlib, sequtils, sugar
 
 import vm/[errors, globals, value]
 
@@ -19,7 +19,12 @@ import vm/[errors, globals, value]
 #=======================================
 
 type
-    VVCaller*       = proc() {.nimcall.}
+    V_VCaller*      = proc()                {.nimcall.}
+    I_VCaller*      = proc(a: int)          {.nimcall.}
+    S_VCaller*      = proc(a: cstring)      {.nimcall.}
+    V_ICaller*      = proc():int            {.nimcall.}
+    I_ICaller*      = proc(a: int):int      {.nimcall.}
+    S_ICaller*      = proc(a: cstring):int  {.nimcall.}
 
 #=======================================
 # Helpers
@@ -35,21 +40,57 @@ proc loadLibrary*(path: string): LibHandle =
 proc unloadLibrary*(lib: LibHandle) =
     unloadLib(lib)
 
-#=======================================
-# Methods
-#=======================================
-
-proc execForeignMethod*(path: string, meth: string): Value =
-    let lib = loadLibrary(path)
-    let runner = cast[VVCaller](lib.symAddr(meth))
-
-    if runner == nil:
+template checkRunner*(r: pointer):untyped =
+    if r == nil:
         echo "Error loading '" & meth & 
              "' function from library at: " & path
         quit(QuitFailure)
 
-    runner()
+#=======================================
+# Methods
+#=======================================
+
+proc execForeignMethod*(path: string, meth: string, params: ValueArray = @[], expected: ValueKind = Nothing): Value =
+    result = VNULL
+
+    let lib = loadLibrary(path)
+
+    case expected
+        of Nothing:
+            if params.len==0:
+                let runner = cast[V_VCaller](lib.symAddr(meth))
+                checkRunner(runner)
+                runner()
+            elif params.len==1:
+                case params[0].kind
+                    of Integer:
+                        let runner = cast[I_VCaller](lib.symAddr(meth))
+                        checkRunner(runner)
+                        runner(params[0].i)
+                    of String:
+                        let runner = cast[S_VCaller](lib.symAddr(meth))
+                        checkRunner(runner)
+                        runner(cstring(params[0].s))
+                    else:
+                        discard
+        of Integer:
+            if params.len==0:
+                let runner = cast[V_ICaller](lib.symAddr(meth))
+                checkRunner(runner)
+                result = newInteger(runner())
+            elif params.len==1:
+                case params[0].kind
+                    of Integer:
+                        let runner = cast[I_ICaller](lib.symAddr(meth))
+                        checkRunner(runner)
+                        result = newInteger(runner(params[0].i))
+                    of String:
+                        let runner = cast[S_ICaller](lib.symAddr(meth))
+                        checkRunner(runner)
+                        result = newInteger(runner(cstring(params[0].s)))
+                    else:
+                        discard
+        else:
+            discard
 
     unloadLibrary(lib)
-    
-    return VNULL
