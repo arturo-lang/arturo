9eccb58300c480fe60b777d6818293480ecb8acc
drkameleon <yaniszaf@gmail.com>
2021-03-16 10:21:39 +0100

    cleaned up comments

--- src/helpers/templates.nim
+++ src/helpers/templates.nim
@@ -55,47 +55,12 @@ proc renderTemplate(s: string, recursive: bool, useReference: bool, reference: V
         keepGoing = result.contains(Embeddable)
 
     while keepGoing:
-
-        # # split input by tags
-        # var splitted = result.split(Embeddable)
-
-        # var blk: seq[string] = @[]
-
-        # # go through the token one-by-one
-        # for i,spl in splitted:
-
-        #     if spl.match(Embeddable).isNone:
-        #         # if it's not an embedded tag,
-        #         # added as a string - split by lines
-        #         blk.add(codify(newString(spl), safeStrings=true))
-        #         # let stripped = spl.strip()
-        #         # if stripped != "" and (stripped[^1] in {'\r','\n'}):
-        #         #         blk.add("\"\\n\"")
-        #     else:
-        #         # otherwise, clean it up
-        #         var parseable = spl.strip(chars = {'<', '>', '|'})
-        #         var output = false
-        #         if parseable[0] == '=': 
-        #             output = true
-        #             parseable = parseable.strip(chars = {'='})
-
-        #         # if it's a <|: something |> tag, stringify it
-        #         if output:
-        #             blk.add("to")
-        #             blk.add(":string")
-
-        #         blk.add(parseable)
-
-        # let subscript = blk.join(" ")
-        # let parsed = doParse(subscript, isFile=false)
+        # make necessary substitutions
         result = "««" & result.replace("<||=","<|| to :string ")
                               .replace("||>","««")
                               .replace("<||","»»") & "»»"
 
-        # result = re.replace(re.replace(result, re.re"<\|=","<|to :string"),
-        #                                        re.re"\|>(.*?)<\|","««$1»»")
-
-        #echo result
+        # parse string template
         let parsed = doParse(result, isFile=false)
 
         # execute/reduce ('array') the resulting block
