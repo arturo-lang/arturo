e73ad8702573fc60e07e98bb86ee6291ce5410a4
drkameleon <yaniszaf@gmail.com>
2021-04-07 10:21:54 +0200

    added `<` support for Version

--- src/vm/value.nim
+++ src/vm/value.nim
@@ -1190,6 +1190,14 @@ proc `<`*(x: Value, y: Value): bool =
         case x.kind:
             of Null: return false
             of Boolean: return false
+            of Version:
+                if x.major > y.major: return false
+                if x.minor > y.minor: return false
+                if x.patch > y.patch: return false
+                if not (x.major == y.major and x.minor == y.minor and x.patch == y.patch):
+                    return true
+                else:
+                    return false
             of Type: return false
             of Char: return $(x.c) < $(y.c)
             of String,
