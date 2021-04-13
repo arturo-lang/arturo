0375c6b74563a28034181c2bc2b23f877c91634d
drkameleon <yaniszaf@gmail.com>
2021-04-13 13:10:58 +0200

    removed TODO

--- src/library/Converters.nim
+++ src/library/Converters.nim
@@ -354,9 +354,6 @@ proc defineSymbols*() =
 
             push(newFunction(x,y,imports,exports,exportable))
 
-    # TODO(Converters\to) Add option for custom number (:integer,:floating) formatting
-    #  this could be done pretty much like with `:date` values and the `.format` attribute
-    #  labels: library, enhancement
     builtin "to",
         alias       = unaliased, 
         rule        = PrefixPrecedence,
