e09b5354e6a00e6bc963b79d88adf63aeb390217
drkameleon <yaniszaf@gmail.com>
2021-04-28 10:30:21 +0200

    Helpers\JS: add overloads for `newRegExp` & `replace` (supporting string parameters, instead of cstring(

--- src/helpers/js.nim
+++ src/helpers/js.nim
@@ -17,8 +17,14 @@ import jsre
 # Methods
 #=======================================
 
+func newRegExp(pattern: string, flags: string): RegExp =
+    newRegExp(pattern.cstring, flags.cstring)
+
 func replace*(pattern: cstring, self: RegExp, replacement: cstring): cstring {.importjs: "#.replace(#,#)".}
   ## Returns a new string with some or all matches of a pattern replaced by given replacement
+
+func replace*(pattern: string, self: RegExp, replacement: string): cstring =
+    replace(pattern.cstring, self, replacement.cstring)
 
 func replaceAll*(pattern: cstring, self: RegExp, replacement: cstring): cstring {.importjs: "#.replaceAll(#,#)".}
   ## Returns a new string with all matches of a pattern replaced by given replacement
