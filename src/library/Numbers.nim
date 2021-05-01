0a06dfd73650c15965cdde34c0bc51c2c2472fb8
drkameleon <yaniszaf@gmail.com>
2021-05-01 12:29:22 +0200

    Numbers: added `acsec` (inverse cosecant) function

--- src/library/Numbers.nim
+++ src/library/Numbers.nim
@@ -96,6 +96,23 @@ proc defineSymbols*() =
             if x.kind==Complex: push(newComplex(arccosh(x.z)))
             else: push(newFloating(arccosh(asFloat(x))))
 
+    builtin "acsec",
+        alias       = unaliased, 
+        rule        = PrefixPrecedence,
+        description = "calculate the inverse cosecant of given angle",
+        args        = {
+            "angle" : {Integer,Floating,Complex}
+        },
+        attrs       = NoAttrs,
+        returns     = {Floating,Complex},
+        # TODO(Numbers\actan): add documentation example
+        #  labels: documentation, easy, library
+        example     = """
+        """:
+            ##########################################################
+            if x.kind==Complex: push(newComplex(arccsc(x.z)))
+            else: push(newFloating(arccsc(asFloat(x))))
+
     builtin "actan",
         alias       = unaliased, 
         rule        = PrefixPrecedence,
