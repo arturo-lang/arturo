fc7aca6b0e12a1f6cff133603fe3b28700e8313a
drkameleon <yaniszaf@gmail.com>
2021-05-18 13:57:52 +0200

    Colors: added documentation examples for all built-in functions + removed TODOs

--- src/library/Colors.nim
+++ src/library/Colors.nim
@@ -41,9 +41,11 @@ proc defineSymbols*() =
             "balance"   : ({Floating},"use different mix of color (0.0-1.0, default:0.5)")
         },
         returns     = {ValueKind.Color},
-        # TODO(Colors\blend) add documentation example
-        #  labels: documentation,library,easy
         example     = """
+            blend #red #CCCCCC                  ; => #E66666
+
+            blend .balance: 0.75 #red #CCCCCC   
+            ; => #D99999
         """:
             ##########################################################
             var balance = 0.5
@@ -65,9 +67,11 @@ proc defineSymbols*() =
         },
         attrs       = NoAttrs,
         returns     = {ValueKind.Color},
-        # TODO(Colors\darken) add documentation example
-        #  labels: documentation,library,easy
         example     = """
+            darken #red 0.2         ; => #CC0000
+            darken #red 0.5         ; => #7F0000
+
+            darken #9944CC 0.3      ; => #6B308F
         """:
             ##########################################################
             if x.kind == Literal:
@@ -85,9 +89,11 @@ proc defineSymbols*() =
         },
         attrs       = NoAttrs,
         returns     = {ValueKind.Color},
-        # TODO(Colors\desaturate) add documentation example
-        #  labels: documentation,library,easy
         example     = """
+            desaturate #red 0.2         ; => #E61919
+            desaturate #red 0.5         ; => #BF4040
+
+            desaturate #9944CC 0.3      ; => #9558B8
         """:
             ##########################################################
             if x.kind == Literal:
@@ -104,9 +110,10 @@ proc defineSymbols*() =
         },
         attrs       = NoAttrs,
         returns     = {ValueKind.Color},
-        # TODO(Colors\invert) add documentation example
-        #  labels: documentation,library,easy
         example     = """
+            print #orange               ; #FFA500
+
+            invert #orange              ; => #0059FF
         """:
             ##########################################################
             if x.kind == Literal:
@@ -124,9 +131,13 @@ proc defineSymbols*() =
         },
         attrs       = NoAttrs,
         returns     = {ValueKind.Color},
-        # TODO(Colors\lighten) add documentation example
-        #  labels: documentation,library,easy
         example     = """
+            print #lightblue            ; #ADD8E6
+
+            lighten #lightblue 0.2      ; => #D0FFFF
+            lighten #lightblue 0.5      ; => #FFFFFF
+
+            lighten #9944CC 0.3         ; => #C758FF
         """:
             ##########################################################
             if x.kind == Literal:
@@ -144,9 +155,13 @@ proc defineSymbols*() =
         },
         attrs       = NoAttrs,
         returns     = {ValueKind.Color},
-        # TODO(Colors\saturate) add documentation example
-        #  labels: documentation,library,easy
         example     = """
+            print #lightblue            ; #ADD8E6
+
+            saturate #lightblue 0.2     ; => #A7DBEC
+            saturate #lightblue 0.5     ; => #9FDFF4
+
+            saturate #9944CC 0.3        ; => #A030E0
         """:
             ##########################################################
             if x.kind == Literal:
