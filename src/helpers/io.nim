565f9b87ba6a3fa381d220a1d76c5051de08603c
drkameleon <yaniszaf@gmail.com>
2021-05-19 10:23:26 +0200

    added Io helper (for writing/reading files, etc)

---
+++ src/helpers/io.nim
@@ -0,0 +1,43 @@
+######################################################
+# Arturo
+# Programming Language + Bytecode VM compiler
+# (c) 2019-2021 Yanis Zafirópulos
+#
+# @file: helpers/io.nim
+######################################################
+
+#=======================================
+# Libraries
+#=======================================
+
+import vm/values/value
+
+#=======================================
+# Methods
+#=======================================
+
+proc writeToFile*(path: string, contents: string, append = false) {.tags: [WriteIOEffect].} =
+    var fm : FileMode = fmWrite
+    if append: fm = fmAppend
+
+    var f: File = nil
+    if open(f, path, fm):
+        try:
+            f.write(contents)
+        finally:
+            close(f)
+    else: 
+        raise newException(IOError, "cannot open: " & path)
+
+proc writeToFile*(path: string, contents: ByteArray, append = false) {.tags: [WriteIOEffect].} =
+    var fm : FileMode = fmWrite
+    if append: fm = fmAppend
+
+    var f: File = nil
+    if open(f, path, fm):
+        try:
+            f.writeBuffer(unsafeAddr contents[0], contents.len)
+        finally:
+            close(f)
+    else:
+        raise newException(IOError, "cannot open: " & path)
