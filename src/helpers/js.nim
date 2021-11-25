e598e32d26555c7e2262026db5ef5e83ef140ed8
drkameleon <yaniszaf@gmail.com>
2021-11-25 17:38:15 +0100

    removed extraneous function from JS helper (turns out my own implementations were integrated into the official Nim repo!)

--- src/helpers/js.nim
+++ src/helpers/js.nim
@@ -28,17 +28,3 @@ func replace*(pattern: string, self: RegExp, replacement: string): cstring =
 
 func replaceAll*(pattern: cstring, self: RegExp, replacement: cstring): cstring {.importjs: "#.replaceAll(#,#)".}
   ## Returns a new string with all matches of a pattern replaced by given replacement
-
-func split*(pattern: cstring, self: RegExp): seq[cstring] {.importjs: "#.split(#)".}
-  ## Divides a string into an ordered list of substrings and returns the array
-
-func match*(pattern: cstring, self: RegExp): seq[cstring] {.importjs: "#.match(#)".}
-  ## Returns an array of matches of a RegExp against given string
-
-func startsWith*(pattern: cstring; self: RegExp): bool =
-  ## Tests if string starts with given RegExp
-  test(newRegExp(("^" & $(self.source)).cstring, self.flags), pattern)
-
-func endsWith*(pattern: cstring; self: RegExp): bool =
-  ## Tests if string ends with given RegExp
-  test(newRegExp(($(self.source) & "$").cstring, self.flags), pattern)
