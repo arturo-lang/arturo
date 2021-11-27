3312f628eaa180dcae57cd5f6cba409088e4891b
drkameleon <yaniszaf@gmail.com>
2021-11-27 11:05:08 +0100

    minor cleanup

--- src/helpers/js.nim
+++ src/helpers/js.nim
@@ -21,10 +21,10 @@ func newRegExp(pattern: string, flags: string): RegExp =
     newRegExp(pattern.cstring, flags.cstring)
 
 func replace*(pattern: cstring, self: RegExp, replacement: cstring): cstring {.importjs: "#.replace(#,#)".}
-  ## Returns a new string with some or all matches of a pattern replaced by given replacement
+    ## Returns a new string with some or all matches of a pattern replaced by given replacement
 
 func replace*(pattern: string, self: RegExp, replacement: string): cstring =
     replace(pattern.cstring, self, replacement.cstring)
 
 func replaceAll*(pattern: cstring, self: RegExp, replacement: cstring): cstring {.importjs: "#.replaceAll(#,#)".}
-  ## Returns a new string with all matches of a pattern replaced by given replacement
+    ## Returns a new string with all matches of a pattern replaced by given replacement
