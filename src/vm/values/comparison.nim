e380087579c4c9ce6b988f175a170d8e0613916e
drkameleon <yaniszaf@gmail.com>
2022-01-27 11:40:12 +0100

    Values\comparison: fixed equality operator (`=`) for `:color` values

--- src/vm/values/comparison.nim
+++ src/vm/values/comparison.nim
@@ -10,7 +10,7 @@
 # Libraries
 #=======================================
 
-import lenientops, tables, unicode
+import colors, lenientops, tables, unicode
 when not defined(NOGMP):
     import extras/bignum
  
@@ -104,6 +104,8 @@ proc `==`*(x: Value, y: Value): bool {.inline.}=
                 when not defined(NOSQLITE):
                     if x.dbKind==SqliteDatabase: return cast[ByteAddress](x.sqlitedb) == cast[ByteAddress](y.sqlitedb)
                     #elif x.dbKind==MysqlDatabase: return cast[ByteAddress](x.mysqldb) == cast[ByteAddress](y.mysqldb)
+            of ValueKind.Color:
+                return x.l == y.l
             of Date:
                 return x.eobj == y.eobj
             else:
