ff4c3445f86deb7d50953fb55455e442a122565a
drkameleon <yaniszaf@gmail.com>
2022-02-17 18:01:40 +0100

    keep Webview initialization javascript in separate script file

---
+++ src/helpers/webviews.js
@@ -0,0 +1,21 @@
+// Initialize the main wrapper &
+// set up callbacks
+if (typeof arturo === 'undefined') {
+    arturo = {}
+}
+
+
+// call backend method 
+// with given arguments
+arturo.call = function (method){
+    return window.callback("call", JSON.stringify({
+        "method": method,
+        "args": Array.prototype.slice.call(arguments, 1)
+    }))
+},
+
+// execute arbitrary arturo code 
+// in backend
+arturo.exec = function (code){
+    return window.callback("exec", JSON.stringify(code))
+}
