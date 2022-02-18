c6d0381a113b287a4426673ff5e43a026d1575a3
drkameleon <yaniszaf@gmail.com>
2022-02-18 18:09:42 +0100

    added support for Portable File Dialogs (https://github.com/samhocevar/portable-file-dialogs)

---
+++ src/extras/pfd.nim
@@ -0,0 +1,67 @@
+######################################################
+# nim-portable-dialogs
+# Sam Hocevar's Portable File Dialogs wrapper
+# for Nim
+#
+# (c) 2022 Yanis Zafirópulos
+# 
+# @license: see LICENSE file
+# @file: extras/pfd.nim
+######################################################
+
+#=======================================
+# Libraries
+#=======================================
+
+import os
+
+#=======================================
+# Compilation & Linking
+#=======================================
+
+{.compile("pfd/pfd.cc","-std=c++11").}
+
+{.passC: "-I" & parentDir(currentSourcePath()) .}
+{.passL:"-lstdc++".}
+
+#=======================================
+# Types
+#=======================================
+
+type
+    DialogIcon* {.size: sizeof(cint).} = enum
+        NoIcon = 0
+        InfoIcon = 1
+        WarningIcon = 2
+        ErrorIcon = 3
+        QuestionIcon = 4
+
+    DialogType* {.size: sizeof(cint).} = enum
+        OKDialog = 0
+        OKCancelDialog = 1
+        YesNoDialog = 2
+        YesNoCancelDialog = 3
+        RetryCancelDialog = 4
+        RetryAbortIgnoreDialog = 5
+
+    DialogResult* {.size: sizeof(cint).} = enum
+        CancelResult = -1
+        OKResult = 0
+        YesResult = 1
+        NoResult = 2
+        AbortResult = 3
+        RetryResult = 4
+        IgnoreResult = 5
+
+#=======================================
+# Function prototypes
+#=======================================
+
+{.push header: "pfd/pfd.h", cdecl.}
+
+proc pfd_notification*(title: cstring, message: cstring, ic: DialogIcon) {.importc.}
+proc pfd_message*(title: cstring, message: cstring, tp: DialogType, ic: DialogIcon):DialogResult {.importc.}
+proc pfd_select_folder*(title: cstring, path: cstring): cstring {.importc.}
+proc pfd_select_file*(title: cstring, path: cstring): cstring {.importc.}
+
+{.pop.}
