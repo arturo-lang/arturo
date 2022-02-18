c3835fb0fbb2aa26a1b3a49a1248c386c7af701d
drkameleon <yaniszaf@gmail.com>
2022-02-18 16:31:23 +0100

    modernize js code

--- src/helpers/webviews.js
+++ src/helpers/webviews.js
@@ -1,28 +1,28 @@
 // Initialize the main wrapper &
 // set up callbacks
 if (typeof arturo === 'undefined') {
-    arturo = {}
-}
+    arturo = {
+        // call backend method 
+        // with given arguments
+        call: (method, ...args)=>{
+            return window.callback("call", JSON.stringify({
+                "method": method,
+                "args": args
+            }))
+        },
 
-// call backend method 
-// with given arguments
-arturo.call = function (method){
-    return window.callback("call", JSON.stringify({
-        "method": method,
-        "args": Array.prototype.slice.call(arguments, 1)
-    }))
-},
-
-// execute arbitrary arturo code 
-// in backend
-arturo.exec = function (code){
-    return window.callback("exec", JSON.stringify(code))
+        // execute arbitrary arturo code 
+        // in backend
+        exec: (code)=>{
+            return window.callback("exec", JSON.stringify(code))
+        }
+    }
 }
 
 // setup events
-window.onload = function (){
+window.onload = ()=>{
     return window.callback("event", JSON.stringify("load"))
 }
-window.onerror = function (){
+window.onerror = ()=>{
     return window.callback("event", JSON.stringify("error"))
 }
