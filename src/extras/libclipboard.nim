98195e9376c0ee5405d4dbfc47b26ff4c5bd4f59
drkameleon <yaniszaf@gmail.com>
2022-02-21 12:42:05 +0100

    added Nim wrapper for the libclipboard library

---
+++ src/extras/libclipboard.nim
@@ -0,0 +1,59 @@
+######################################################
+# nim-portable-dialogs
+# Jeremy Tan's libclipboard wrapper
+# for Nim
+#
+# (c) 2022 Yanis Zafirópulos
+# 
+# @license: see LICENSE file
+# @file: extras/libclipboard.nim
+######################################################
+
+#=======================================
+# Libraries
+#=======================================
+
+import os
+
+#=======================================
+# Compilation & Linking
+#=======================================
+
+{.passC: "-I" & parentDir(currentSourcePath()) .}
+
+{.compile("libclipboard/clipboard_common.c").}
+
+when defined(linux) or defined(freebsd):
+    {.compile("libclipboard/clipboard_x11.c", "-DLIBCLIPBOARD_BUILD_X11").}
+elif defined(macosx)
+    {.compile("libclipboard/clipboard_cocoa.c", "-x objective-c -DLIBCLIPBOARD_BUILD_COCOA -framework Foundation").}
+elif defined(windows)
+    {.compile("libclipboard/clipboard_win32.c", "-DLIBCLIPBOARD_BUILD_WIN32").}
+
+#=======================================
+# Types
+#=======================================
+
+type
+    ClipboardMode* {.size: sizeof(cing).} = enum
+        LCB_CLIPBOARD = 0
+        LCB_PRIMARY = 1
+        LCB_SELECTION = 2,
+        LCB_SECONDARY = 3
+        LCB_MODE_END = 4
+
+    ClipboardObj* {.importc: "clipboard_c".} = pointer
+
+#=======================================
+# Function prototypes
+#=======================================
+
+{.push header: "libclipboard/libclipboard.h", cdecl.}
+
+proc clipboard_new*(cb_opts: pointer) {.importc.}
+proc clipboard_clear*(cb: ClipboardObj, mode: ClipboardMode) {.importc.}
+proc clipboard_free*(cb: ClipboardObj) {.importc.}
+proc clipboard_set_text*(cb: ClipboardObj, src: cstring) {.importc.}
+proc clipboard_text*(cb: ClipboardObj):cstring {.importc.}
+
+{.pop.}
