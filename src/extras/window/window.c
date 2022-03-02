893f669c799fb80e6ca06896e416441740cbf464
drkameleon <yaniszaf@gmail.com>
2022-03-02 10:36:44 +0100

    attempt casts to WINDOW_TYPE (HWND on Windows)

--- src/extras/window/window.c
+++ src/extras/window/window.c
@@ -39,94 +39,94 @@ bool isFullscreen = false;
     RECT previousRect;
 #endif
 
-bool is_maximized_window(WINDOW_TYPE windowHandle){
+bool is_maximized_window(void* windowHandle){
     #if defined(__linux__) || defined(__FreeBSD__)
-        return gtk_window_is_maximized(GTK_WINDOW(windowHandle)) == 1;
+        return gtk_window_is_maximized(GTK_WINDOW((WINDOW_TYPE)windowHandle)) == 1;
     #elif defined(__APPLE__)
-        return [windowHandle isZoomed];
+        return [(WINDOW_TYPE)windowHandle isZoomed];
     #elif defined(_WIN32)
-        return IsZoomed(windowHandle) == 1;
+        return IsZoomed((WINDOW_TYPE)windowHandle) == 1;
     #endif
 }
 
-void maximize_window(WINDOW_TYPE windowHandle){
+void maximize_window(void* windowHandle){
     #if defined(__linux__) || defined(__FreeBSD__)
-        gtk_window_maximize(GTK_WINDOW(windowHandle));
+        gtk_window_maximize(GTK_WINDOW((WINDOW_TYPE)windowHandle));
     #elif defined(__APPLE__)
-        [windowHandle zoom:nil];
+        [(WINDOW_TYPE)windowHandle zoom:nil];
     #elif defined(_WIN32)
-        ShowWindow(windowHandle, SW_MAXIMIZE);
+        ShowWindow((WINDOW_TYPE)windowHandle, SW_MAXIMIZE);
     #endif
 }
 
-void unmaximize_window(WINDOW_TYPE windowHandle){
+void unmaximize_window(void* windowHandle){
     #if defined(__linux__) || defined(__FreeBSD__)
-        gtk_window_unmaximize(GTK_WINDOW(windowHandle));
+        gtk_window_unmaximize(GTK_WINDOW((WINDOW_TYPE)windowHandle));
     #elif defined(__APPLE__)
-        [windowHandle zoom:nil];
+        [(WINDOW_TYPE)windowHandle zoom:nil];
     #elif defined(_WIN32)
-        ShowWindow(windowHandle, SW_RESTORE);
+        ShowWindow((WINDOW_TYPE)windowHandle, SW_RESTORE);
     #endif
 }
 
-bool is_visible_window(WINDOW_TYPE windowHandle){
+bool is_visible_window(void* windowHandle){
     #if defined(__linux__) || defined(__FreeBSD__)
-        return gtk_widget_is_visible(windowHandle) == 1;
+        return gtk_widget_is_visible((WINDOW_TYPE)windowHandle) == 1;
     #elif defined(__APPLE__)
-        return [windowHandle isVisible];
+        return [(WINDOW_TYPE)windowHandle isVisible];
     #elif defined(_WIN32)
-        return IsWindowVisible(windowHandle) == 1;
+        return IsWindowVisible((WINDOW_TYPE)windowHandle) == 1;
     #endif
 }
 
-void show_window(WINDOW_TYPE windowHandle){
+void show_window(void* windowHandle){
     #if defined(__linux__) || defined(__FreeBSD__)
-        gtk_widget_show(windowHandle);
+        gtk_widget_show((WINDOW_TYPE)windowHandle);
     #elif defined(__APPLE__)
-        [windowHandle setIsVisible: true];
+        [(WINDOW_TYPE)windowHandle setIsVisible: true];
     #elif defined(_WIN32)
-        ShowWindow(windowHandle, SW_SHOW);
+        ShowWindow((WINDOW_TYPE)windowHandle, SW_SHOW);
     #endif
 }
 
-void hide_window(WINDOW_TYPE windowHandle){
+void hide_window(void* windowHandle){
     #if defined(__linux__) || defined(__FreeBSD__)
-        gtk_widget_hide(windowHandle);
+        gtk_widget_hide((WINDOW_TYPE)windowHandle);
     #elif defined(__APPLE__)
-        [windowHandle setIsVisible: false];
+        [(WINDOW_TYPE)windowHandle setIsVisible: false];
     #elif defined(_WIN32)
-        ShowWindow(windowHandle, SW_HIDE);
+        ShowWindow((WINDOW_TYPE)windowHandle, SW_HIDE);
     #endif
 }
 
-bool is_fullscreen_window(WINDOW_TYPE windowHandle){
+bool is_fullscreen_window(void* windowHandle){
     #if defined(__linux__) || defined(__FreeBSD__)
         return isFullscreen;
     #elif defined(__APPLE__)
-        return ([windowHandle styleMask] & NSWindowStyleMaskFullScreen) == NSWindowStyleMaskFullScreen;
+        return ([(WINDOW_TYPE)windowHandle styleMask] & NSWindowStyleMaskFullScreen) == NSWindowStyleMaskFullScreen;
     #elif defined(_WIN32)
         return isFullscreen;
     #endif
 }
 
-void fullscreen_window(WINDOW_TYPE windowHandle){
+void fullscreen_window(void* windowHandle){
     #if defined(__linux__) || defined(__FreeBSD__)
-        gtk_window_fullscreen(GTK_WINDOW(windowHandle));
+        gtk_window_fullscreen(GTK_WINDOW((WINDOW_TYPE)windowHandle));
     #elif defined(__APPLE__)
-        [windowHandle toggleFullScreen: nil];
+        [(WINDOW_TYPE)windowHandle toggleFullScreen: nil];
     #elif defined(_WIN32)
-        previousStyle = GetWindowLong(windowHandle, GWL_STYLE);
-        previousStyleX = GetWindowLong(windowHandle, GWL_EXSTYLE);
-        GetWindowRect(windowHandle, &previousRect);
+        previousStyle = GetWindowLong((WINDOW_TYPE)windowHandle, GWL_STYLE);
+        previousStyleX = GetWindowLong((WINDOW_TYPE)windowHandle, GWL_EXSTYLE);
+        GetWindowRect((WINDOW_TYPE)windowHandle, &previousRect);
 
         MONITORINFO monitor_info;
         DWORD newStyle = previousStyle & ~(WS_CAPTION | WS_THICKFRAME);
         DWORD newStyleX = previousStyleX & ~(WS_EX_DLGMODALFRAME | WS_EX_WINDOWEDGE |
                             WS_EX_CLIENTEDGE | WS_EX_STATICEDGE);
-        SetWindowLong(windowHandle, GWL_STYLE, newStyle);
-        SetWindowLong(windowHandle, GWL_EXSTYLE, newStyleX);
+        SetWindowLong((WINDOW_TYPE)windowHandle, GWL_STYLE, newStyle);
+        SetWindowLong((WINDOW_TYPE)windowHandle, GWL_EXSTYLE, newStyleX);
         monitor_info.cbSize = sizeof(monitor_info);
-        GetMonitorInfo(MonitorFromWindow(windowHandle, MONITOR_DEFAULTTONEAREST),
+        GetMonitorInfo(MonitorFromWindow((WINDOW_TYPE)windowHandle, MONITOR_DEFAULTTONEAREST),
                     &monitor_info);
         RECT r;
         r.left = monitor_info.rcMonitor.left;
@@ -134,7 +134,7 @@ void fullscreen_window(WINDOW_TYPE windowHandle){
         r.right = monitor_info.rcMonitor.right;
         r.bottom = monitor_info.rcMonitor.bottom;
         SetWindowPos(
-            windowHandle, 
+            (WINDOW_TYPE)windowHandle, 
             NULL, 
             r.left, 
             r.top, 
@@ -147,16 +147,16 @@ void fullscreen_window(WINDOW_TYPE windowHandle){
     #endif
 }
 
-void unfullscreen_window(WINDOW_TYPE windowHandle){
+void unfullscreen_window(void* windowHandle){
     #if defined(__linux__) || defined(__FreeBSD__)
-        gtk_window_unfullscreen(GTK_WINDOW(windowHandle));
+        gtk_window_unfullscreen(GTK_WINDOW((WINDOW_TYPE)windowHandle));
     #elif defined(__APPLE__)
-        [windowHandle toggleFullScreen: nil];
+        [(WINDOW_TYPE)windowHandle toggleFullScreen: nil];
     #elif defined(_WIN32)
-        SetWindowLong(windowHandle, GWL_STYLE, previousStyle);
-        SetWindowLong(windowHandle, GWL_EXSTYLE, previousStyleX);
+        SetWindowLong((WINDOW_TYPE)windowHandle, GWL_STYLE, previousStyle);
+        SetWindowLong((WINDOW_TYPE)windowHandle, GWL_EXSTYLE, previousStyleX);
         SetWindowPos(
-            windowHandle, 
+            (WINDOW_TYPE)windowHandle, 
             NULL, 
             previousRect.left, 
             previousRect.top, 
@@ -169,14 +169,14 @@ void unfullscreen_window(WINDOW_TYPE windowHandle){
     #endif
 }
 
-void set_topmost_window(WINDOW_TYPE windowHandle){
+void set_topmost_window(void* windowHandle){
     #if defined(__linux__) || defined(__FreeBSD__)
-        gtk_window_set_keep_above(GTK_WINDOW(windowHandle), true);
+        gtk_window_set_keep_above(GTK_WINDOW((WINDOW_TYPE)windowHandle), true);
     #elif defined(__APPLE__)
-        [windowHandle setLevel: NSFloatingWindowLevel];
+        [(WINDOW_TYPE)windowHandle setLevel: NSFloatingWindowLevel];
     #elif defined(_WIN32)
         SetWindowPos(
-            windowHandle,
+            (WINDOW_TYPE)windowHandle,
             HWND_TOPMOST,
             0,
             0,
@@ -187,14 +187,14 @@ void set_topmost_window(WINDOW_TYPE windowHandle){
     #endif
 }
 
-void unset_topmost_window(WINDOW_TYPE windowHandle){
+void unset_topmost_window(void* windowHandle){
     #if defined(__linux__) || defined(__FreeBSD__)
-        gtk_window_set_keep_above(GTK_WINDOW(windowHandle), false);
+        gtk_window_set_keep_above(GTK_WINDOW((WINDOW_TYPE)windowHandle), false);
     #elif defined(__APPLE__)
-        [windowHandle setLevel: NSNormalWindowLevel];
+        [(WINDOW_TYPE)windowHandle setLevel: NSNormalWindowLevel];
     #elif defined(_WIN32)
         SetWindowPos(
-            windowHandle,
+            (WINDOW_TYPE)windowHandle,
             HWND_NOTOPMOST,
             0,
             0,
@@ -205,17 +205,17 @@ void unset_topmost_window(WINDOW_TYPE windowHandle){
     #endif
 }
 
-void make_borderless_window(WINDOW_TYPE windowHandle){
+void make_borderless_window(void* windowHandle){
     #if defined(__linux__) || defined(__FreeBSD__)
-        gtk_window_set_decorated(GTK_WINDOW(windowHandle), false);
+        gtk_window_set_decorated(GTK_WINDOW((WINDOW_TYPE)windowHandle), false);
     #elif defined(__APPLE__)
-        [windowHandle setStyleMask: [windowHandle styleMask] & ~NSWindowStyleMaskTitled];
+        [(WINDOW_TYPE)windowHandle setStyleMask: [(WINDOW_TYPE)windowHandle styleMask] & ~NSWindowStyleMaskTitled];
     #elif defined(_WIN32)
-        DWORD currentStyle = GetWindowLong(windowHandle, GWL_STYLE);
+        DWORD currentStyle = GetWindowLong((WINDOW_TYPE)windowHandle, GWL_STYLE);
         currentStyle &= ~(WS_CAPTION | WS_THICKFRAME);
-        SetWindowLong(windowHandle, GWL_STYLE, currentStyle);
+        SetWindowLong((WINDOW_TYPE)windowHandle, GWL_STYLE, currentStyle);
         SetWindowPos(
-            windowHandle, 
+            (WINDOW_TYPE)windowHandle, 
             NULL, 
             0, 
             0, 
