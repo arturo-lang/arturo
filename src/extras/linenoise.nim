e04c9eb409a0ce640184621a609c856ffd158f97
drkameleon <yaniszaf@gmail.com>
2022-03-04 14:40:45 +0100

    unify linenoise wrapper implementation (same wrapper for Windows & Mac/*nix)

---
+++ src/extras/linenoise.nim
@@ -0,0 +1,37 @@
+when defined(windows):
+    {.compile("linenoise/linenoise.c").}
+
+#=======================================
+# Types
+#=======================================
+
+type
+    LinenoiseCompletions* = object
+        len*: csize_t
+        cvec*: cstringArray
+
+    LinenoiseCompletionCallback*    = proc (buf: cstring; lc: ptr LinenoiseCompletions) {.cdecl.}
+    LinenoiseHintsCallback*         = proc (buf: cstring; color: var cint; bold: var cint): cstring {.cdecl.}
+    LinenoiseFreeHintsCallback*     = proc (buf: cstring; color: var cint; bold: var cint) {.cdecl.}
+
+
+#=======================================
+# C Imports
+#=======================================
+
+when defined(windows):
+    {.push header: "linenoise/linenoise.h", cdecl.}
+proc linenoiseSetCompletionCallback*(cback: ptr LinenoiseCompletionCallback) {.importc: "linenoiseSetCompletionCallback".}
+proc linenoiseSetHintsCallback*(cback: ptr LinenoiseHintsCallback) {.importc: "linenoiseSetHintsCallback".}
+proc linenoiseAddCompletion*(a2: ptr LinenoiseCompletions; a3: cstring) {.importc: "linenoiseAddCompletion".}
+proc linenoiseReadLine*(prompt: cstring): cstring {.importc: "linenoise".}
+proc linenoiseHistoryAdd*(line: cstring): cint {.importc: "linenoiseHistoryAdd", discardable.}
+proc linenoiseHistorySetMaxLen*(len: cint): cint {.importc: "linenoiseHistorySetMaxLen".}
+proc linenoiseHistorySave*(filename: cstring): cint {.importc: "linenoiseHistorySave".}
+proc linenoiseHistoryLoad*(filename: cstring): cint {.importc: "linenoiseHistoryLoad".}
+proc linenoiseClearScreen*() {.importc: "linenoiseClearScreen".}
+proc linenoiseSetMultiLine*(ml: cint) {.importc: "linenoiseSetMultiLine".}
+proc linenoisePrintKeyCodes*() {.importc: "linenoisePrintKeyCodes".}
+when defined(windows):
+    {.pop.}
+proc free*(s: cstring) {.importc: "free", header: "<stdlib.h>".}
--- src/helpers/repl.nim
+++ src/helpers/repl.nim
@@ -12,27 +12,13 @@
 
 import os
 
-when defined(windows):
-    {.compile(normalizedPath(currentSourcePath().splitPath.head & "../extras/linenoise/linenoise.c")).}
-
 when not defined(WEB):
+
+    import extras/linenoise
 
     import sequtils, strutils, sugar, tables
 
     import vm/values/value
-
-    #=======================================
-    # Types
-    #=======================================
-
-    type
-        LinenoiseCompletions* = object
-            len*: csize_t
-            cvec*: cstringArray
-
-        LinenoiseCompletionCallback*    = proc (buf: cstring; lc: ptr LinenoiseCompletions) {.cdecl.}
-        LinenoiseHintsCallback*         = proc (buf: cstring; color: var cint; bold: var cint): cstring {.cdecl.}
-        LinenoiseFreeHintsCallback*     = proc (buf: cstring; color: var cint; bold: var cint) {.cdecl.}
 
     #=======================================
     # Constants
@@ -47,27 +33,6 @@ when not defined(WEB):
 
     var
         ReplInitialized = false
-
-    #=======================================
-    # C Imports
-    #=======================================
-
-    when defined(windows):
-        {.push header: normalizedPath(currentSourcePath().splitPath.head & "../extras/linenoise/linenoise.h"), cdecl.}
-    proc linenoiseSetCompletionCallback*(cback: ptr LinenoiseCompletionCallback) {.importc: "linenoiseSetCompletionCallback".}
-    proc linenoiseSetHintsCallback(cback: ptr LinenoiseHintsCallback) {.importc: "linenoiseSetHintsCallback".}
-    proc linenoiseAddCompletion*(a2: ptr LinenoiseCompletions; a3: cstring) {.importc: "linenoiseAddCompletion".}
-    proc linenoiseReadLine*(prompt: cstring): cstring {.importc: "linenoise".}
-    proc linenoiseHistoryAdd*(line: cstring): cint {.importc: "linenoiseHistoryAdd", discardable.}
-    proc linenoiseHistorySetMaxLen*(len: cint): cint {.importc: "linenoiseHistorySetMaxLen".}
-    proc linenoiseHistorySave*(filename: cstring): cint {.importc: "linenoiseHistorySave".}
-    proc linenoiseHistoryLoad*(filename: cstring): cint {.importc: "linenoiseHistoryLoad".}
-    proc linenoiseClearScreen*() {.importc: "linenoiseClearScreen".}
-    proc linenoiseSetMultiLine*(ml: cint) {.importc: "linenoiseSetMultiLine".}
-    proc linenoisePrintKeyCodes*() {.importc: "linenoisePrintKeyCodes".}
-    when defined(windows):
-        {.pop.}
-    proc free*(s: cstring) {.importc: "free", header: "<stdlib.h>".}
 
     #=======================================
     # C Exports
