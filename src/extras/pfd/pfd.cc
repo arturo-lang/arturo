803ed083051fcf36d24d6012efa8fab9d459c0b0
drkameleon <yaniszaf@gmail.com>
2022-03-04 10:44:18 +0100

    trying to solve MINGW-related issue

--- src/extras/pfd/pfd.cc
+++ src/extras/pfd/pfd.cc
@@ -16,6 +16,8 @@
 #ifndef WIN32_LEAN_AND_MEAN
 #   define WIN32_LEAN_AND_MEAN 1
 #endif
+#define NTDDI_VERSION 0x0A000006 //NTDDI_WIN10_RS5
+#define _WIN32_WINNT 0x0A00 // _WIN32_WINNT_WIN10, the _WIN32_WINNT macro must also be defined when defining NTDDI_VERSION
 #include <Windows.h>
 #include <commdlg.h>
 #include <ShlObj.h>
