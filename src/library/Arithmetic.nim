ec6fa8fb9df9e550ee2e501653f2aff432e2164b
drkameleon <yaniszaf@gmail.com>
2022-07-22 13:01:11 +0200

    Arithmetic\inc: added support for `:quantity` values

--- src/library/Arithmetic.nim
+++ src/library/Arithmetic.nim
@@ -113,10 +113,10 @@ proc defineSymbols*() =
         rule        = PrefixPrecedence,
         description = "increase given value by 1",
         args        = {
-            "value" : {Integer,Floating,Rational,Literal}
+            "value" : {Integer,Floating,Rational,Quantity,Literal}
         },
         attrs       = NoAttrs,
-        returns     = {Integer,Floating,Rational,Nothing},
+        returns     = {Integer,Floating,Rational,Quantity,Nothing},
         example     = """
             print inc 5        ; 6
             ..........
