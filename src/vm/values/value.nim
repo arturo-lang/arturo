d34d0bbfcb8b8496122b61c44339e1831ddfff30
drkameleon <yaniszaf@gmail.com>
2022-07-26 11:44:15 +0200

    VM/values/value: don't throw any error when in WEB build

--- src/vm/values/value.nim
+++ src/vm/values/value.nim
@@ -604,7 +604,8 @@ proc convertQuantityValue*(nm: Value, fromU: UnitName, toU: UnitName, fromKind =
     if toK==NoUnit: toK = quantityKindForName(toU)
 
     if fromK!=toK:
-        RuntimeError_CannotConvertQuantity($(nm), stringify(fromU), stringify(fromK), stringify(toU), stringify(toK))
+        when not defined(WEB):
+            RuntimeError_CannotConvertQuantity($(nm), stringify(fromU), stringify(fromK), stringify(toU), stringify(toK))
     
     if toK == TemperatureUnit:
         return convertToTemperatureUnit(nm, fromU, toU)
