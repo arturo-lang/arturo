a172321e4aa5876cbeb7f9cdcc1aee7c438144ef
drkameleon <yaniszaf@gmail.com>
2022-08-10 13:23:19 +0200

    Tests: removed /benchmarks folder altogether

--- tests/benchmarks/hash_lookup.rb
+++
@@ -1,9 +0,0 @@
-dict = {
-	"name" => "john",
-	"surname" => "doe",
-	"country" => "Spain"
-}
-
-200000.times.each{
-	puts dict["surname"]
-}
--- tests/benchmarks/loop_assign_incr_5m.py
+++
@@ -1,7 +0,0 @@
-
-a = 0
-
-for n in range(5000000+1):
-	a += 1
-
-print a
