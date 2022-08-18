fbbf1bb87f1f4cc436219e643d379e7c08bfbca2
drkameleon <yaniszaf@gmail.com>
2022-08-18 13:42:04 +0200

    added TODO

--- src/library/Collections.nim
+++ src/library/Collections.nim
@@ -74,6 +74,9 @@ proc defineSymbols*() =
             print b                   ; [1 2 3 4]
         """:
             ##########################################################
+            # TODO(Collections\append) Append for arrays vs strings seems to be much slower
+            #  https://github.com/arturo-lang/benchmarks/blob/main/results/17-8-2022/micro.md
+            #  labels: enhancement, library, performance
             if x.kind==Literal:
                 if InPlace.kind==String:
                     if y.kind==String:
