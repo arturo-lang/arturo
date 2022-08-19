a4b0b663baceb5655e8e54c0583aeb28cdfbd2d6
drkameleon <yaniszaf@gmail.com>
2022-08-19 14:00:18 +0200

    Helpers/bytes: added helper method for number (`int` or `float`) conversion to ByteArray

--- src/helpers/bytes.nim
+++ src/helpers/bytes.nim
@@ -10,9 +10,20 @@
 # Libraries
 #=======================================
 
+import algorithm, sequtils
+
 import vm/[values/value]
 
 #=======================================
 # Methods
 #=======================================
 
+proc numberToBinary*(i: int | float): ByteArray =
+    if i==0: return @[(byte)0]
+    var bytes = toSeq(cast[array[0..7, byte]](i)).reversed
+    var i=0
+    while i < bytes.len and bytes[i] == 0:
+        i += 1
+    while i < bytes.len:
+        result.add(bytes[i])
+        i += 1
