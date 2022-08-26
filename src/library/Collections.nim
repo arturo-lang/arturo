5c33f2c8f3b5a1c5e8e19a5909dab73011e06e60
drkameleon <yaniszaf@gmail.com>
2022-08-26 17:29:53 +0200

    remove debugging output

--- src/library/Collections.nim
+++ src/library/Collections.nim
@@ -542,7 +542,6 @@ proc defineSymbols*() =
                     else:
                         push(VNULL)
                 of Dictionary: 
-                    echo "Collections: IN 'get'"
                     push(GetKey(x.d, $(key)))
                 of Object:
                     push(GetKey(x.o, $(key)))
