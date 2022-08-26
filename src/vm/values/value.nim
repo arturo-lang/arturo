ee1c2b830f0e765811cdedd7c249cceedc802a1d
drkameleon <yaniszaf@gmail.com>
2022-08-26 16:05:09 +0200

    VM/values/value: fix `newUserType`

--- src/vm/values/value.nim
+++ src/vm/values/value.nim
@@ -550,8 +550,11 @@ func newType*(t: ValueKind): Value {.inline.} =
     Value(kind: Type, tpKind: BuiltinType, t: t)
 
 proc newUserType*(n: string, f: ValueArray = @[]): Value {.inline.} =
-    if TypeLookup.hasKey(n):
-        return TypeLookup[n]
+    let lookup = TypeLookup.getOrDefault(n, VNULL)
+    if lookup != VNULL:
+        return lookup
+    # if TypeLookup.hasKey(n):
+    #     return TypeLookup[n]
     else:
         result = Value(kind: Type, tpKind: UserType, t: Object, ts: Prototype(name: n, fields: f, methods: initOrderedTable[string,Value](), inherits: nil))
         TypeLookup[n] = result
