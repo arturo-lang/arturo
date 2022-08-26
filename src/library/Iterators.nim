3330ba16a74939d07048b1a669437fe0488f7611
drkameleon <yaniszaf@gmail.com>
2022-08-26 14:11:37 +0200

    Iterators\cluster: use `hasKeyOrPut` instead (return true if value exists; otherwise insert given value)

--- src/library/Iterators.nim
+++ src/library/Iterators.nim
@@ -241,8 +241,9 @@ proc defineSymbols*() =
             iterateThrough(withIndex, y, items, doForever, false, false):
                 discard execBlock(VNULL, evaluated=preevaled, args=allArgs)
                 let popped = pop()
-                if not sets.hasKey(popped):
-                    sets[popped] = @[]
+                # if not sets.hasKey(popped):
+                #     sets[popped] = @[]
+                discard sets.hasKeyOrPut(popped, @[])
                 sets[popped].add(capturedItems)
 
             if showValue:
