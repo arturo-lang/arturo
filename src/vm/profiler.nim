9567e62fd297cfa9eb739057f820fe6b347283ce
drkameleon <yaniszaf@gmail.com>
2022-09-08 16:59:41 +0200

    VM/profiler: initial implementation

--- src/vm/profiler.nim
+++ src/vm/profiler.nim
@@ -6,46 +6,98 @@
 # @file: vm/profiler.nim
 ######################################################
 
-#=======================================
-# Libraries
-#=======================================
+when defined(PROFILER):
 
-import tables
+    #=======================================
+    # Libraries
+    #=======================================
 
-#=======================================
-# Types
-#=======================================
+    import algorithm, std/monotimes, strformat
+    import strutils, sugar, tables, times
 
-type
-    ProfilerDataRow* = ref object
-        time*: int
-        runs*: int
+    import helpers/terminal
 
-    ProfilerDataTable* = OrderedTable[string, ProfilerDataRow]
+    #=======================================
+    # Types
+    #=======================================
 
-    ProfilerData* = ref object
-        ops*: ProfilerDataRow
-        procs*: ProfilerDataRow
+    type
+        ProfilerDataRow* = ref object
+            time*: int
+            runs*: int
 
-#=======================================
-# Variables
-#=======================================
+        ProfilerDataTable* = OrderedTable[string, ProfilerDataRow]
 
-var
-    PR*: ProfilerData
+    #=======================================
+    # Variables
+    #=======================================
 
-#=======================================
-# Forward declarations
-#=======================================
-
-#=======================================
-# Templates
-#=======================================
+    var
+        PR*: OrderedTable[string, ProfilerDataTable]
 
 #=======================================
 # Helpers
 #=======================================
 
+when defined(PROFILER):
+    proc printProfilerHeader*(what: string) =
+        echo fg(cyanColor) &
+             "========================================================="
+        echo " " & what.toUpperAscii
+        echo "=========================================================" & resetColor()
+
+#=======================================
+# Templates
+#=======================================
+
+template getMetric*(actionContent: untyped): int =
+    let t0 = getMonoTime()
+    actionContent
+    (int)(ticks(getMonoTime()) - ticks(t0))
+
+template addMetricIfNotExists*(name: string, metric: untyped): untyped =
+    if not PR[metric].hasKey(name):
+        PR[metric][name] = ProfilerDataRow(time:0, runs:0)
+    PR[metric][name]
+
+template registerFunction*(name: string, actionContent: untyped): untyped =
+    when defined(PROFILER):
+        var newRow = addMetricIfNotExists(name, "functions")
+        newRow.runs += 1
+        newRow.time += getMetric(actionContent)
+    else:
+        actionContent
+
+template printProfilerDataTable*(what: string) =
+    printProfilerHeader(what)
+    var maxTitle = 0
+    for (title, _) in pairs(PR[what]):
+        if len(title) > maxTitle:
+            maxTitle = len(title)
+
+
+    PR[what].sort((a, b) => cmp(a[1].time / a[1].runs, b[1].time / b[1].runs), SortOrder.Descending)
+
+    for (title, row) in pairs(PR[what]):
+        var timePerRun{.inject.} = (row.time / row.runs / 1_000)
+        echo alignLeft(title, maxTitle+10) & "| " & alignLeft(fmt"{timePerRun:.2f}μs",15) & "|" & $row.runs
+
 #=======================================
 # Methods
 #=======================================
+
+proc initProfiler*() =
+    when defined(PROFILER):
+        PR = {
+            "ops": initOrderedTable[string, ProfilerDataRow](),
+            "procs": initOrderedTable[string, ProfilerDataRow](),
+            "functions": initOrderedTable[string, ProfilerDataRow]()
+        }.toOrderedTable
+    else:
+        discard
+
+proc showProfilerData*() =
+    when defined(PROFILER):
+        printProfilerDataTable("functions")
+    else:
+        discard
