22ab8318805b7f9b6e7764ca5104f4165ea10e7e
drkameleon <yaniszaf@gmail.com>
2022-10-08 13:36:24 +0200

    VM/values/types: also remove `.fname` field (we don't need it either)

--- src/vm/values/types.nim
+++ src/vm/values/types.nim
@@ -308,7 +308,6 @@ type
                         exportable* : bool
                         memoize*    : bool
                     of BuiltinFunction:
-                        fname*      : string
                         # TODO(VM/values/types) `arity` should be common to both User and BuiltIn functions
                         #  Usually, when we want to get a User function's arity, we access its `params.a.len` - this doesn't make any sense. Plus, it's slower.
                         #  labels: vm, values, enhancement
