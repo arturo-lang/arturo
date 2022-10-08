05c4b81e73acc58d36345339efd79376a307d49f
drkameleon <yaniszaf@gmail.com>
2022-10-08 13:02:36 +0200

    VM/types: remove `.alias` field for BuiltinFunction-kind Function values

--- src/vm/values/types.nim
+++ src/vm/values/types.nim
@@ -309,7 +309,6 @@ type
                         memoize*    : bool
                     of BuiltinFunction:
                         fname*      : string
-                        alias*      : SymbolKind
                         prec*       : PrecedenceKind
                         # TODO(VM/values/types) `arity` should be common to both User and BuiltIn functions
                         #  Usually, when we want to get a User function's arity, we access its `params.a.len` - this doesn't make any sense. Plus, it's slower.
