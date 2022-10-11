c5815810eccfafc90bc57d224e1830636f0f7c2b
drkameleon <yaniszaf@gmail.com>
2022-10-11 18:41:35 +0200

    Iterators: [iterateThrough] instead of using reverse, use `while`

--- src/library/Iterators.nim
+++ src/library/Iterators.nim
@@ -95,8 +95,12 @@ template iterateThrough(
                         when rolling:
                             if rollingRight: push(res)
 
-                        for item in capturedItems.reversed:
-                            push(item)
+                        var j = indx+argsLen-1
+                        while j >= indx:
+                            push(collection[j])
+                            j -= 1
+                        # for item in capturedItems.reversed:
+                        #     push(item)
 
                         when rolling:
                             if not rollingRight: push(res)
