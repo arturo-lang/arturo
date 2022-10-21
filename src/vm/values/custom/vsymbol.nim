f307eede3d7965be6a8b37c20375feaef130c2dc
drkameleon <yaniszaf@gmail.com>
2022-10-21 10:40:37 +0200

    moved `SymbolKind` definition to new module + renamed type to `VSymbol`

--- src/vm/values/custom/vsymbol.nim
+++ src/vm/values/custom/vsymbol.nim
@@ -11,6 +11,101 @@
 #=======================================
 
 type 
+    VSymbol* = enum
+        thickarrowleft          # <=
+        thickarrowright         # =>
+        thickarrowboth          # <=>
+        thickarrowdoubleleft    # <<=
+        thickarrowdoubleright   # =>>
+        thickarrowdoubleboth    # <<=>>
+        arrowleft               # <-
+        arrowright              # ->
+        arrowboth               # <->
+        arrowdoubleleft         # <<-
+        arrowdoubleright        # ->>
+        arrowdoubleboth         # <<->>
+        reversearrowleft        # -<
+        reversearrowright       # >-
+        reversearrowboth        # >-<
+        reversearrowdoubleleft  # -<<
+        reversearrowdoubleright # >>-
+        reversearrowdoubleboth  # >>-<<
+        doublearrowleft         # <<
+        doublearrowright        # >>
+        triplearrowleft         # <<<
+        triplearrowright        # >>>
+        longarrowleft           # <--
+        longarrowright          # -->
+        longarrowboth           # <-->
+        longthickarrowleft      # <==
+        longthickarrowright     # ==>
+        longthickarrowboth      # <==>
+        tildeleft               # <~
+        tilderight              # ~>
+        tildeboth               # <~>
+        triangleleft            # <|
+        triangleright           # |>
+        triangleboth            # <|>
+
+        equalless               # =<
+        greaterequal            # >=
+        lessgreater             # <>
+
+        lesscolon               # <:
+        minuscolon              # -:
+        greatercolon            # >:
+
+        tilde                   # ~
+        exclamation             # !
+        doubleexclamation       # !!
+        question                # ?
+        doublequestion          # ??
+        at                      # @
+        sharp                   # #
+        doublesharp             # ## 
+        triplesharp             # ###
+        quadruplesharp          # ####
+        quintuplesharp          # #####
+        sextuplesharp           # ######
+        dollar                  # $
+        percent                 # %
+        caret                   # ^
+        ampersand               # &
+        asterisk                # *
+        minus                   # -
+        doubleminus             # --
+        underscore              # _
+        equal                   # =
+        doubleequal             # ==
+        approxequal             # =~
+        plus                    # +
+        doubleplus              # ++
+
+        lessthan                # <
+        greaterthan             # >
+       
+        slash                   # /
+        slashpercent            # %/
+        doubleslash             # //
+        backslash               # 
+        doublebackslash         #
+        logicaland              #
+        logicalor               #
+        pipe                    # |     
+        turnstile               # |-
+        doubleturnstile         # |=
+
+        ellipsis                # ..
+        longellipsis            # ...
+        dotslash                # ./
+        colon                   # :
+        doublecolon             # ::
+        doublepipe              # ||
+
+        slashedzero             # ø
+        infinite                # ∞
+
+        unaliased               # used only for builtins
 
 #=======================================
 # Methods
