dc20cd9f23ef5568e890c37e26bf5c4828101d7b
drkameleon <yaniszaf@gmail.com>
2022-10-22 13:40:31 +0200

    added documentation

--- src/vm/version.nim
+++ src/vm/version.nim
@@ -20,7 +20,7 @@ import strutils
 #=======================================
 
 const 
-    ArturoVersion*    = static readFile("version/version").strip()
-    ArturoBuild*      = static readFile("version/build").strip()
+    ArturoVersion*    = static readFile("version/version").strip()      ## The current version of Arturo
+    ArturoBuild*      = static readFile("version/build").strip()        ## The current build number of Arturo
 
-    ArturoVersionTxt* = "arturo v/" & ArturoVersion
+    ArturoVersionTxt* = "arturo v/" & ArturoVersion                     ## Default version text
