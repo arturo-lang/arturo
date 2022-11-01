babb03d29693e890e63a2352267e75ecf10540c3
drkameleon <yaniszaf@gmail.com>
2022-11-01 11:48:14 +0100

    VM/values/printable: [dump] added missing opcode values

--- src/vm/values/printable.nim
+++ src/vm/values/printable.nim
@@ -363,7 +363,7 @@ proc dump*(v: Value, level: int=0, isLast: bool=false, muted: bool=false, prepen
                 if op in {opPush, opStore, opCall, opLoad, opStorl, opAttr, opEol}:
                     j += 1
                     instrs.add(newInteger((int)v.trans.instructions[j]))
-                elif op in {opPushX, opStoreX, opCallX, opLoadX, opStorlX, opEolX}:
+                elif op in {opPushX, opStoreX, opCallX, opLoadX, opStorlX, opEolX, opJmpIf, opJmpIfNot, opJmpIfGt, opJmpIfGe, opJmpIfLt, opJmpIfLe}:
                     j += 2
                     instrs.add(newInteger((int)((uint16)(v.trans.instructions[j-1]) shl 8 + (byte)(v.trans.instructions[j]))))
 
