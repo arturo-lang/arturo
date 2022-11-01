d8c365c723990c3624a5f1b2fb8a0cd499abd584
drkameleon <yaniszaf@gmail.com>
2022-11-01 12:46:17 +0100

    VM/values/types: save `.compare` function as a *proc* (=> `doCompare`)

--- src/vm/values/types.nim
+++ src/vm/values/types.nim
@@ -121,6 +121,7 @@ type
         name*       : string
         fields*     : ValueArray
         methods*    : ValueDict
+        doCompare*  : proc (a,b:Value): int
         inherits*   : Prototype
 
     SymbolDict*   = OrderedTable[VSymbol,AliasBinding]
