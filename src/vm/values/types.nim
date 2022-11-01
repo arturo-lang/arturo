ee64374738dca72be1836577d9c54a9c209d8aaf
drkameleon <yaniszaf@gmail.com>
2022-11-01 12:59:45 +0100

    minor fix (`doPrint` takes a Value argument)

--- src/vm/values/types.nim
+++ src/vm/values/types.nim
@@ -121,7 +121,7 @@ type
         name*       : string
         fields*     : ValueArray
         methods*    : ValueDict
-        doPrint*    : proc (): string
+        doPrint*    : proc (v:Value): string
         doCompare*  : proc (a,b:Value): int
         inherits*   : Prototype
 
