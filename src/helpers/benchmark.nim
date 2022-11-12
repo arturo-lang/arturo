4e016767022faca6a8da0cf8b0f953bdcef42b21
drkameleon <yaniszaf@gmail.com>
2022-11-12 13:01:02 +0100

    fifth pass

--- src/helpers/benchmark.nim
+++ src/helpers/benchmark.nim
@@ -21,10 +21,10 @@ template benchmark*(benchmarkName: string, code: untyped) =
         let t0 = getMonoTime()
         code
         let elapsed = float(ticks(getMonoTime()) - ticks(t0))
-        let elapsedStr = (elapsed/(float)1000000).formatFloat(format = ffDecimal, precision = 3)
+        let elapsedStr = (elapsed/1000000.0).formatFloat(format = ffDecimal, precision = 3)
         echo "[benchmark] time: ", elapsedStr, "ms"
 
 template getBenchmark*(code: untyped): float =
     let t0 = getMonoTime()
     code
-    (float(ticks(getMonoTime()) - ticks(t0)))/float(1000000)
+    (float(ticks(getMonoTime()) - ticks(t0)))/1000000.0
