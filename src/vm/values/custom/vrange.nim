4b9abfed0c0d709ab7a7495a431ac5bc2f6c64bf
drkameleon <yaniszaf@gmail.com>
2022-11-15 17:24:45 +0100

    added basic implementation & added `$` overload

--- src/vm/values/custom/vrange.nim
+++ src/vm/values/custom/vrange.nim
@@ -21,13 +21,25 @@ type
         Forward
         Backward
 
-    VRange* = ref object
-        start*  : int | float
-        stop*   : int | float 
-        step*   : int | float
-        order*  : RangeDirection
+    GenericRange[T] = ref object
+        start*      : T
+        stop*       : T
+        step*       : T
+        infinite*   : bool
+        dir*        : RangeDirection
+
+    VRange* = GenericRange[int]
 
 #=======================================
 # Overloads
 #=======================================
 
+func `$`*(v: VRange): string {.inline,enforceNoRaises.} =
+    let start = $(v.start)
+    var stop: string
+    if v.infinite: stop = "∞"
+    else: stop = $(v.stop)
+    result = start & ".." & stop
+
+    if v.step != 1:
+        result &= " (" & $(v.step) & ")"
