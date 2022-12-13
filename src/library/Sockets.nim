061fcf7664a4c1a1e83685e7996a582004949a66
drkameleon <yaniszaf@gmail.com>
2022-12-13 10:55:02 +0100

    make sure the module doesn't cause problems for Web builds

--- src/library/Sockets.nim
+++ src/library/Sockets.nim
@@ -23,40 +23,43 @@ when not defined(WEB):
     import std/net as netsock except Socket
     import nativesockets
 
-    import vm/lib
+import vm/lib
 
 #=======================================
 # Methods
 #=======================================
 
 proc defineSymbols*() =
-        
-    builtin "listen",
-        alias       = unaliased, 
-        rule        = PrefixPrecedence,
-        description = "Start listening on given port and return corresponding socket",
-        args        = {
-            "port"  : {Integer}
-        },
-        attrs       = {
-            "blocking"  : ({String},"set blocking mode (default: false)")
-        },
-        returns     = {Socket},
-        example     = """
-        """:
-            #=======================================================
-            when defined(SAFE): RuntimeError_OperationNotPermitted("")
+    
+    when not defined(WEB):
+        builtin "listen",
+            alias       = unaliased, 
+            rule        = PrefixPrecedence,
+            description = "Start listening on given port and return corresponding socket",
+            args        = {
+                "port"  : {Integer}
+            },
+            attrs       = {
+                "blocking"  : ({String},"set blocking mode (default: false)")
+            },
+            returns     = {Socket},
+            example     = """
+            """:
+                #=======================================================
+                when defined(SAFE): RuntimeError_OperationNotPermitted("")
 
-            let blocking = hadAttr("blocking")
+                let blocking = hadAttr("blocking")
 
-            var socket: netsock.Socket = netsock.newSocket()
-            socket.setSockOpt(OptReuseAddr, true)
-            
-            socket.getFd().setBlocking(blocking)
-            socket.bindAddr(Port(x.i))
-            socket.listen()
+                var socket: netsock.Socket = netsock.newSocket()
+                socket.setSockOpt(OptReuseAddr, true)
+                
+                socket.getFd().setBlocking(blocking)
+                socket.bindAddr(Port(x.i))
+                socket.listen()
 
-            push newSocket(socket)
+                push newSocket(socket)
+    else:
+        discard
 
 #=======================================
 # Add Library
