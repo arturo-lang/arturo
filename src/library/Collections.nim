18fdb454d32873b4fc3fb64b659ebe6041ce2dfa
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2022-12-17 13:27:22 -0300

    add support to literals to `slice` function fixes #844

--- src/library/Collections.nim
+++ src/library/Collections.nim
@@ -1528,7 +1528,7 @@ proc defineSymbols*() =
         rule        = PrefixPrecedence,
         description = "get a slice of collection between given indices",
         args        = {
-            "collection": {String, Block},
+            "collection": {String, Block, Literal},
             "from"      : {Integer},
             "to"        : {Integer}
         },
@@ -1540,7 +1540,18 @@ proc defineSymbols*() =
             print slice 1..10 3 4         ; 4 5
         """:
             #=======================================================
-            if x.kind == String:
+            if x.kind == Literal:
+                ensureInPlace()
+                if InPlaced.kind == String:
+                    if InPlaced.s.len == 0:
+                        SetInPlace newString("")
+                    else:
+                        if y.i >= 0 and z.i <= InPlaced.s.runeLen:
+                            SetInPlace newString Inplaced.s.runeSubStr(y.i, z.i - y.i + 1)
+                else:
+                    if y.i >= 0 and z.i <= InPlaced.a.len-1:
+                        InPlaced.a = InPlaced.a[y.i..z.i]
+            elif x.kind == String:
                 if x.s.len == 0: push(newString(""))
                 else:
                     if y.i >= 0 and z.i <= x.s.runeLen:
