b25e917b1a9a395efd5005e70fc692df4e0bd540
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2022-12-17 13:46:38 -0300

    remove TODO - fixed with PR #881

--- src/library/Collections.nim
+++ src/library/Collections.nim
@@ -1521,8 +1521,6 @@ proc defineSymbols*() =
             else: # Null
                 push(newInteger(0))
 
-    # TODO(Collections/slice) could also work with literal values
-    #  labels: library, enhancement
     builtin "slice",
         alias       = unaliased,
         rule        = PrefixPrecedence,
