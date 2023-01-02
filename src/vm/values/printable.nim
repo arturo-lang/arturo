e7623ccee6c7f9c478000d230f3a9e52d3f89004
drkameleon <yaniszaf@gmail.com>
2023-01-02 12:47:42 +0100

    VM/values/printable: [codify] properly handle possible Function options

--- src/vm/values/printable.nim
+++ src/vm/values/printable.nim
@@ -554,6 +554,16 @@ proc codify*(v: Value, pretty = false, unwrapped = false, level: int=0, isLast: 
             if v.fnKind==UserFunction:
                 result &= "function "
                 result &= codify(newWordBlock(v.params),pretty,unwrapped,level+1, false, safeStrings=safeStrings)
+                if v.inline:
+                    result &= ".inline"
+                if v.memoize:
+                    result &= ".memoize"
+                if not v.imports.isNil:
+                    result &= ".imports:"
+                    result &= codify(newWordBlock(toSeq(keys(v.imports.d))),pretty,unwrapped,level+1, false, safeStrings=safeStrings)
+                if not v.exports.isNil:
+                    result &= ".exports:"
+                    result &= codify(newWordBlock(toSeq(keys(v.exports.d))),pretty,unwrapped,level+1, false, safeStrings=safeStrings)
                 result &= " "
                 result &= codify(v.main,pretty,unwrapped,level+1, true, safeStrings=safeStrings)
             else:
