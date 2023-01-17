8f199dd606a4ea20b360dd9a846f5ac768abbe2d
drkameleon <yaniszaf@gmail.com>
2023-01-17 16:55:58 +0100

    VM/eval: added main structure (comments, etc)

---
+++ src/vm/eval.nim
@@ -0,0 +1,36 @@
+#=======================================================
+# Arturo
+# Programming Language + Bytecode VM compiler
+# (c) 2019-2023 Yanis Zafirópulos
+#
+# @file: vm/eval.nim
+#=======================================================
+
+## This module contains the evaluator for the VM.
+## 
+## The evaluator:
+## - takes a Block of values coming from the parser
+## - passes to the AST generator
+## - interpretes the AST and returns a Translation object
+## 
+## The main entry point is ``doEval``.
+
+#=======================================
+# Libraries
+#=======================================
+
+#=======================================
+# Variables
+#=======================================
+
+#=======================================
+# Helpers
+#=======================================
+
+#=======================================
+# Methods
+#=======================================
+
+#=======================================
+# Main
+#=======================================
