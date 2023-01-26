55fff5414b6f811ca3df1f71e43357447330d461
drkameleon <yaniszaf@gmail.com>
2023-01-26 12:57:08 +0100

    VM/bytecode: remove all "optimization" routines; they didn't do anything really valuable, nor were they in use + part of them has already been incorporated into the evaluator itself

--- src/vm/bytecode.nim
+++ src/vm/bytecode.nim
@@ -17,114 +17,17 @@ when not defined(WEB):
 
     import extras/miniz
 
-import os
-
 import opcodes
 export opcodes
-
-import vm/values/value
-
-import vm/values/custom/[vbinary]
 
 #=======================================
 # Constants
 #=======================================
 
-const 
-    #opPushAny   = opPush0..opPush13
-    #opStoreAny  = opStore0..opStore13
-    opLoadAny   = opLoad0..opLoad13
-    #opCallAny   = opCall0..opCall13
-
 when not defined(WEB):
     const
         BcodeMagic              = uint32(0x86BC0DE0)
         BcodeMagicCompressed    = uint32(0x86BC0DE1)
-
-#=======================================
-# Helpers
-#=======================================
-
-template Op(sth: untyped): untyped = OpCode(sth)
-template By(sth: untyped): untyped = Byte(sth)
-
-template skip(steps: int): untyped =
-    i.inc(steps)
-
-template current(): untyped = a[i]
-template next(): untyped    = 
-    while Op(a[i+1]) in {opEol,opEolX}: 
-        if Op(a[i+1])==opEol: skip(2)
-        else: skip(3)
-    a[i+1]
-
-template consume(num: int = 1): untyped =
-    result[p] = a[initialI]
-    when num > 1:
-        result[p+1] = a[initialI+1]
-        when num > 2:
-            result[p+2] = a[initialI+2]
-    p.inc(num)
-    i.inc(num)
-
-template inject(what: untyped): untyped =
-    result[p] = what
-    p.inc()
-
-template keep(): untyped =
-    result[p] = current()
-    p.inc()
-
-proc optimize(trans: Translation): VBinary =
-    let a = trans.instructions
-    var i = 0
-    var p = 0
-    let aLen = a.len
-    newSeq(result, aLen)
-    while i < aLen:
-        let initial = current
-        let initialI = i
-        #echo fmt"I = {i} -> {Op(initial)}"
-        case Op(initial):
-            of opStore0..opStore13: 
-                if Op(next) in opLoadAny and By(opLoad)-a[i+1]==By(opStore)-initial:
-                    # (opStore*) + (opLoad*) -> (opStorl*)
-                    inject(): By(opStorl0) + initial - By(opStore0) 
-                    skip(2)
-                else:
-                    consume(1)
-            of opPush0..opPush13, opLoad0..opLoad13:
-                if Op(next) == Op(initial):
-                    # (opPush/opLoad*) x N -> (opPush/opLoad*) + (opDup) x N
-                    keep()
-                    while Op(next) == Op(initial):
-                        inject(): By(opDup)
-                        skip(1)
-                    skip(1)
-                else:
-                    consume(1)
-
-            # of opCall0..opCall29:
-            #     let idx = initial - By(opCall0)
-            #     echo fmt"found opCall* -> {idx}"
-            #     if d[idx].kind==Word and Syms[d[idx].s] == AddF:
-            #         inject(): By(opIAdd)
-            #         skip(1)
-            #     else:
-            #         consume(1)
-
-            of opPush,opStore,opLoad,opCall,opStorl,opAttr:
-                consume(2)
-            of opPushX,opStoreX,opLoadX,opCallX,opStorlX:
-                consume(3)
-            of opEol:
-                skip(2)
-            of opEolX:
-                skip(3)
-            else:
-                consume(1)
-
-    result.setLen(p)
 
 #=======================================
 # Methods
@@ -187,6 +90,3 @@ proc readBytecode*(origin: string): (string, seq[byte]) =
             return (dataSegment, codeSegment)               # return the result
     else:
         discard
-
-proc optimizeBytecode*(t: Translation): seq[byte] =
-    result = optimize(t)
