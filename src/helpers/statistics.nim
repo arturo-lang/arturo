0dc0cdcd22e616318d9632379ee8e91b5e0cd0c1
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2023-02-22 15:12:03 -0300

    replace `medianOfMedians` by `quickSelect` algorithm - It would avoid bugs and unwanted complexity

--- src/helpers/statistics.nim
+++ src/helpers/statistics.nim
@@ -1,60 +1,37 @@
-proc distribute[T](container: seq[T], size: int): seq[seq[T]] =
-    ## Distributes elements in subSequences of maximum `size`
-    var
-        count: int = 1
-        current: seq[T] = @[]
+import std/random
 
-    for element in container:
-        current.add element
-        if (count mod size == 0) or (count == container.len):
-            result.add current
-            current = @[]
-        count.inc()
-
-
-proc medianOfMedians*[T](container: seq[T], middle: int): T =
-    ## medianOfMedians returns the smallest nth number of a container
+proc quickSelect*[T](container: seq[T], index: int): T =
+    ## return the smallest `index`th number of a container
 
     #[
         Steps:
-            1. Divide the container into sublists of a maximum of length x,
-            let's say... 5
-            2. Sort each sublist and determine the median
-            3. Use the same function recursively to determine the median of the set
-            4. Use this median as pivot element
-            5. Partition the elements into right, left
-            6. Do this comparation:
-                - if i = k -> x
-                - if i < k -> recuse using A[1, ..., k-1, i]
-                - if i > k -> recurse using A[k+1, ...,i], i-k]
-
-        » Read more on: https://brilliant.org/wiki/median-finding-algorithm/
+            1. check the container's length to use a faster algorithm
+                to its size
+            2. get a pivot using a random algorithm
+            3. partion elements into lower, higher or same value as pivot
+            4. Divide to conquer using these partitions and recursion
+        Inspired from: https://rcoh.me/posts/linear-time-median-finding/
+        by Russell Cohen <https://github.com/rcoh>
     ]#
 
-    const tiny = 5
-
-    if container.len <= tiny:
-        return container.sorted()[middle]
+    if container.len == 1:
+        assert index == 0
+        return container[0]
+    elif container.len < 6:
+        return container.sorted()[index]
 
     var
-        medians, left, right: seq[T]
-        pivot: T
-
-    for list in container.distribute(tiny):
-        medians.add list.medianOfMedians(list.len div 2)
-
-    if medians.len <= tiny:
-        pivot = medians.sorted()[medians.len div 2]
-    else:
-        pivot = medians.medianOfMedians(medians.len div 2)
+        pivot: T = container.sample()
+        left, right, same: seq[T]
 
     for element in container:
         if element > pivot: right.add element
-        elif element < pivot: left.add element
+        elif element == pivot: same.add element
+        else: left.add element
 
-    if middle < left.len:
-        return left.medianOfMedians(middle)
-    elif middle > left.len:
-        return right.medianOfMedians(middle - left.high)
+    if index < left.len:
+        return left.quickSelect(index)
+    elif index < left.len + same.len:
+        return same[0]
     else:
-        return pivot
+        return right.quickSelect(index - left.len - same.len)
