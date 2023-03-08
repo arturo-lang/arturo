b182c4d5d1c3136c2def21cfa939b8413d34396e
drkameleon <yaniszaf@gmail.com>
2023-03-08 10:08:08 +0100

    Collections/split: added TODO

--- src/library/Collections.nim
+++ src/library/Collections.nim
@@ -1873,6 +1873,10 @@ proc defineSymbols*() =
     #   split.every: 3 'b, debug b
     #  ```
     #  labels: library, bug
+
+    # TODO(Collections/split) `.every` not working properly with Literal values?
+    #  see: https://github.com/arturo-lang/arturo/pull/1045#issuecomment-1458674117
+    #  labels: library, bug
     builtin "split",
         alias       = unaliased,
         op          = opSplit,
