8eb5098a5480da9b3c7f59bb8b2c6b95da59bd4e
drkameleon <yaniszaf@gmail.com>
2023-03-16 17:22:44 +0100

    VM/vm: [handleVMErrors] catch *all* exceptions, including Defect's

--- src/vm/vm.nim
+++ src/vm/vm.nim
@@ -169,7 +169,7 @@ template initialize(args: seq[string], filename: string, isFile:bool, scriptData
 template handleVMErrors(blk: untyped): untyped =
     try:
         blk
-    except CatchableError:
+    except CatchableError, Defect:
         let e = getCurrentException()        
         showVMErrors(e)
 
