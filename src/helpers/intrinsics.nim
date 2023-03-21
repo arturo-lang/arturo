6df81b410a7db0ae0700b100e0f5cfeda49172bd
drkameleon <yaniszaf@gmail.com>
2023-03-21 17:33:34 +0100

    Helpers/intrinsics: added missing methods for subtraction & multiplication (with overflow)

--- src/helpers/intrinsics.nim
+++ src/helpers/intrinsics.nim
@@ -7,19 +7,6 @@
 #=======================================================
 
 #=======================================
-# Libraries
-#=======================================
-
-#=======================================
-# Constants
-#=======================================
-
-#=======================================
-# Helpers
-#=======================================
-
-
-#=======================================
 # Methods
 #=======================================
 
@@ -27,5 +14,9 @@
 
 when defined(bit32):
     func addIntWithOverflow*(a, b: int, res: var int): bool {.importc: "__builtin_sadd_overflow", nodecl, nosideeffect.}
+    func subIntWithOverflow*(a, b: int, res: var int): bool {.importc: "__builtin_ssub_overflow", nodecl, nosideeffect.}
+    func mulIntWithOverflow*(a, b: int, res: var int): bool {.importc: "__builtin_smul_overflow", nodecl, nosideeffect.}
 else:
     func addIntWithOverflow*(a, b: int, res: var int): bool {.importc: "__builtin_saddll_overflow", nodecl, nosideeffect.}
+    func subIntWithOverflow*(a, b: int, res: var int): bool {.importc: "__builtin_ssubll_overflow", nodecl, nosideeffect.}
+    func mulIntWithOverflow*(a, b: int, res: var int): bool {.importc: "__builtin_smulll_overflow", nodecl, nosideeffect.}
