8fb499750164d45b7858c921a6b8675feed9bba2
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2023-03-24 16:45:39 -0300

    [Core\pop] rename function to unstack

--- src/library/Core.nim
+++ src/library/Core.nim
@@ -827,6 +827,48 @@ proc defineSymbols*() =
                 execUnscoped(y)
 
             push(newLogical(condition))
+            
+    builtin "unstack",
+        alias       = unaliased, 
+        op          = opNop,
+        rule        = PrefixPrecedence,
+        description = "pop top <number> values from stack",
+        args        = {
+            "number"    : {Integer}
+        },
+        attrs       = {
+            "discard"   : ({Logical},"do not return anything")
+        },
+        returns     = {Any},
+        example     = """
+            1 2 3
+            a: pop 1        ; a: 3
+
+            1 2 3
+            b: pop 2        ; b: [3 2]
+            ..........
+            1 2 3
+            pop.discard 1   ; popped 3 from the stack
+        """:
+            #=======================================================
+            let doDiscard = (hadAttr("discard"))
+
+            if x.i==1:
+                if doDiscard: discard pop()
+                else: discard
+            else:
+                if doDiscard: 
+                    var i = 0
+                    while i<x.i:
+                        discard pop()
+                        i+=1
+                else:
+                    var res: ValueArray
+                    var i = 0
+                    while i<x.i:
+                        res.add pop()
+                        i+=1
+                    push(newBlock(res))
 
     builtin "until",
         alias       = unaliased, 
