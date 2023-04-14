0548521dccfbbd4d73e9d4f60de7aa0589a7c72b
drkameleon <yaniszaf@gmail.com>
2023-04-14 18:35:30 +0200

    and yet another retry (let's see if this one is going to be the one lol)

--- src/vm/values/custom/vquantity_v4.nim
+++ src/vm/values/custom/vquantity_v4.nim
@@ -1,17 +1,52 @@
-import math, sequtils, strutils, sugar, tables
+import macros, math, parseutils
+import sequtils, strutils, sugar, tables
+
+import vm/values/custom/vrational
 
 type
+    # CAREFUL:
+    # The order matters! It's used for the signature calculation
+    # by `getQuantityType`.
+
+    UnitPrefix = enum
+        PAtto   = (-18, "a")
+        PFemto  = (-15, "f")
+        PPico   = (-12, "p")
+        PNano   = ( -9, "n")
+        PMicro  = ( -6, "u")
+        PMilli  = ( -3, "m")
+        PCenti  = ( -2, "c")
+        PDeci   = ( -1, "d")
+        PNone   = (  0, "")
+        PDeka   = (  1, "da")
+        PHecto  = (  2, "h")
+        PKilo   = (  3, "k")
+        PMega   = (  6, "M")
+        PGiga   = (  9, "G")
+        PTera   = ( 12, "T")
+        PPeta   = ( 15, "P")
+        PExa    = ( 18, "E")
+
+        PError  = ( 50, "prefix-error")
+
+    Unit = enum
+        M = "m",
+        S = "s",
+        HR = "hr",
+        IN = "in",
+        UError = "unit-error"
+
     UnitKind = enum
-        KLength
-        KTime
-        KTemperature
-        KMass
-        KCurrent
-        KSubstance
-        KLuminosity
-        KCurrency
-        KInformation
-        KAngle
+        KLength             = "Length"
+        KTime               = "Time"
+        KTemperature        = "Temperature"
+        KMass               = "Mass"
+        KCurrent            = "Current"
+        KSubstance          = "Substance"
+        KLuminosity         = "Luminosity"
+        KCurrency           = "Currency"
+        KInformation        = "Information"
+        KAngle              = "Angle"
     
     QuantityType = enum
         TAcceleration       = "Acceleration"
@@ -64,7 +99,25 @@ type
 
     QuantityTypeSignature = int64
 
+    UnitArray = seq[Unit]
+    Units = tuple
+        n: UnitArray
+        d: UnitArray
+
+    Quantity = tuple
+        value: float
+        converted: float
+        tp: QuantityType
+        units: Units
+        base: bool
+
+{.hints: on.} # Apparently we cannot disable just `Name` hints?
+#{.hint: "Quantity's inner type is currently " & $sizeof(VQuantityObj) & ".".}
+{.hint: "Quantity's inner type is currently " & $sizeof(Quantity) & ".".}
+{.hints: off.}
+
 const
+    BaseUnits = [M, S]
     QuantityTypeSignatures = {
         int64(-312_078)         : TElastance,
         int64(-312_058)         : TResistance,
@@ -116,42 +169,323 @@ const
         int64(512_000_000_000)  : TAngle
     }.toTable
 
+    UnitKinds = {
+        M: KLength,
+        IN: KLength,
+        S: KTime,
+        HR: KTime,
+    }.toTable
+
+    Unity = 1.0
+
+func parseQuantity(s: string): Quantity
+func newQuantity(v: float, units: Units): Quantity
+
+# const
+
+#     UnitDefinitions= {
+#         "M": parseQuantity("1 m"),
+#         "IN": parseQuantity("127/5000 m")
+#     }.toTable
+
+
+
+#     RubyUnits::Unit.define('meter') do |unit|
+#   unit.scalar    = 1
+#   unit.numerator = %w[<meter>]
+#   unit.aliases   = %w[m meter meters metre metres]
+#   unit.kind      = :length
+# end
+
+macro parseEnumFromString*(enumToParse: typedesc, argSym: typed, default: typed): untyped =
+    let enumType    = enumToParse.getTypeInst[1]
+    let enumItems   = enumType.getImpl[2]
+
+    result = nnkCaseStmt.newTree(argSym)
+
+    var fStr = ""
+
+    for f in enumItems:
+        if f.kind == nnkEmpty: continue
+
+        var enumCase: NimNode
+        case f.kind
+            of nnkSym, nnkIdent: fStr = f.strVal
+            of nnkEnumFieldDef:
+                enumCase = f[0]
+                case f[1].kind
+                    of nnkStrLit: fStr = f[1].strVal
+                    of nnkTupleConstr:
+                        fStr = f[1][1].strVal
+                    of nnkIntLit:
+                        fStr = f[0].strVal
+                    else:
+                        let fAst = f[0].getImpl
+                        if fAst.kind == nnkStrLit:
+                            fStr = fAst.strVal
+                        else:
+                            error("Invalid syntax!", f[1])
+            else: discard
+    
+        result.add nnkOfBranch.newTree(newLit(fStr),  enumCase)
+
+    result.add nnkElse.newTree(default)
+
+macro generateEnumParser*(en: untyped, def: untyped) = 
+    let parserName = ident("parse" & en.strVal)
+
+    return quote do:
+        proc `parserName`(p: string): `en` {.inline.} =
+            parseEnumFromString(`en`, p, `def`)
+
+generateEnumParser(UnitPrefix, PError)
+generateEnumParser(Unit, UError)
+
 template getTypeBySignature(signature: QuantityTypeSignature): QuantityType =
     QuantityTypeSignatures.getOrDefault(signature, TUnknown)
 
-func getSignature(numerator, denominator: openArray[UnitKind]): QuantityTypeSignature =
+func getQuantityType(units: Units): QuantityType =
     var vector = newSeq[int64](ord(UnitKind.high) + 1)
-    for unit in numerator:
-        vector[ord(unit)] += 1
-    for unit in denominator:
-        vector[ord(unit)] -= 1
+    for unit in units.n:
+        vector[ord(UnitKinds[unit])] += 1
+    for unit in units.d:
+        vector[ord(UnitKinds[unit])] -= 1
 
     for index, item in vector:
         vector[index] = item * (int(pow(float(20),float(index))))
 
-    result = vector.foldl(a + b, int64(0))
-    # vector.each_with_index { |item, index| vector[index] = item * (20**index) }
-    # @signature = vector.inject(0) { |acc, elem| acc + elem }
+    return getTypeBySignature(vector.foldl(a + b, int64(0)))
 
-    # debugEcho $(vector)
-
-    # return 0
-
-proc debugGetSignature(numerator, denominator: openArray[UnitKind]) =
+proc debugGetType(units: Units) =
     echo "getting signature for: "
-    echo "    - numerator: " & (if numerator.len == 0: "--" else: numerator.map((x) => $(x)).join(" * "))
-    echo "    - denominator: " & (if denominator.len == 0: "--" else: denominator.map((x) => $(x)).join(" * "))
+    echo "    - numerator: " & (if units.n.len == 0: "--" else: units.n.map((x) => $(x)).join(" * "))
+    echo "    - denominator: " & (if units.d.len == 0: "--" else: units.d.map((x) => $(x)).join(" * "))
 
-    let sign = getSignature(numerator, denominator)
-    echo "    = " & $(getTypeBySignature(sign)) & " (" & $sign & ")"
+    echo "    = " & $(getQuantityType(units))
     echo "--------------------------------"
 
+func parseUnits(s: string): Units =
+    func parsePart(k: string): UnitArray =
+        for p in k.split("."):
+            var tp = ""
+            var i = 0
+            while i < p.len and p[i] notin {'1','2','3','4','5'}:
+                tp.add p[i]
+                i += 1
+            
+            var exp = 1
+            if p.len > i:
+                discard parseInt(p[i..p.len-1], exp)
+
+            let utp = parseUnit(tp)
+
+            for i in 0 ..< exp:
+                result.add(utp)
+
+    let parts = s.split("/")
+
+    var pos, neg: UnitArray
+
+    if parts.len > 0:
+        pos = parsePart(parts[0])
+    if parts.len > 1:
+        neg = parsePart(parts[1])
+
+    result = (
+        n: pos,
+        d: neg
+    )
+
+# func `$`(q: Units): string =
+#     $(q[])
+
+func checkIfBase(units: Units): bool =
+    for unit in units.n:
+        if not BaseUnits.contains(unit):
+            return false
+    for unit in units.d:
+        if not BaseUnits.contains(unit):
+           return false
+
+    return true
+
+func newDerivedQuantity(v: float, units: Units): Quantity =
+    result = (
+        value: v,
+        converted: v,
+        tp:  getQuantityType(units),
+        units: units,
+        base: checkIfBase(units)
+    )
+
+func derived(s: string): Quantity =
+    let parts = s.split(" ")
+    if parts.len == 2:
+        let numparts = parts[0].split("/")
+        let units = parseUnits(parts[1])
+
+        var value: float
+        if numparts.len > 1:
+            value = toFloat(initRational(parseInt(numparts[0]), parseInt(numparts[1])))
+        else:
+            value = parseFloat(numparts[0])
+        
+        result = newDerivedQuantity(value, units)
+    else:
+        let units = parseUnits(s)
+        result = newDerivedQuantity(Unity, units)
+
+const
+    UnitDefinitions = {
+        IN: derived("127/5000 m"),
+        HR: derived("3600 s")
+    }.toTable
+
+func newQuantity(v: float, tp: QuantityType, units: Units, base: bool): Quantity =
+    result = (
+        value: v,
+        converted: v,
+        tp: tp,
+        units: units,
+        base: base
+    )
+
+func newQuantity(v: float, units: Units): Quantity =
+    result = (
+        value: v,
+        converted: v,
+        tp:  getQuantityType(units),
+        units: units,
+        base: checkIfBase(units)
+    )
+    if not result.base:
+        for unit in units.n:
+            if UnitDefinitions.contains(unit):
+                result.converted *= UnitDefinitions[unit].converted
+
+        for unit in units.d:
+            if UnitDefinitions.contains(unit):
+                result.converted /= UnitDefinitions[unit].converted
+
+func parseQuantity(s: string): Quantity =
+    let parts = s.split(" ")
+    if parts.len == 2:
+        let numparts = parts[0].split("/")
+        let units = parseUnits(parts[1])
+
+        var value: float
+        if numparts.len > 1:
+            value = toFloat(initRational(parseInt(numparts[0]), parseInt(numparts[1])))
+        else:
+            value = parseFloat(numparts[0])
+        
+        result = newQuantity(value, units)
+    else:
+        let units = parseUnits(s)
+        result = newQuantity(Unity, units)
+
+func `+`(a, b: Quantity): Quantity =
+    if a.tp == b.tp:
+        result = newQuantity(a.converted + b.converted, a.tp, a.units, a.base)
+    else:
+        debugEcho "cannot add incompatible types"
+
+func `-`(a, b: Quantity): Quantity =
+    if a.tp == b.tp:
+        result = newQuantity(a.converted - b.converted, a.tp, a.units, a.base)
+    else:
+        debugEcho "cannot subtract incompatible types"
+
+func `*`(a, b: Quantity): Quantity =
+    let newUnits = (n: a.units.n & b.units.n, d: a.units.d & b.units.d)
+    result = newQuantity(a.converted * b.converted, getQuantityType(newUnits), newUnits, a.base and b.base)
+
+# func `/`(a, b: Quantity): Quantity =
+#     let newUnits = (n: a.units.n.remove(b.units.d), d: a.units.d & b.units.n)
+#     result = newQuantity(a.converted / b.converted, getQuantityType(newUnits), newUnits, a.base and b.base)
+
+func print(q: Quantity): string =
+    result.add $(q.value) & " "
+    var tbl = initCountTable[Unit]()
+    for c in q.units.n:
+        tbl.inc(c)
+
+    var pos: seq[string]
+    for k, v in tbl:
+        var part = $(k)
+        if v > 1:
+            case v:
+                of 2: part.add "²"
+                of 3: part.add "³"
+                else:
+                    part.add "^" & $(v)
+        pos.add(part)
+
+    tbl = initCountTable[Unit]()
+    for c in q.units.d:
+        tbl.inc(c)
+
+    var neg: seq[string]
+    for k, v in tbl:
+        var part = $(k)
+        if v > 1:
+            case v:
+                of 2: part.add "²"
+                of 3: part.add "³"
+                else:
+                    part.add "^" & $(v)
+        neg.add(part)
+
+    result &= pos.join("·")
+    if neg.len > 0:
+        result &= "/" & neg.join("·")
+
+proc debugAdd(str: varargs[string]) =
+    let qa = parseQuantity(str[0])
+    let qb = parseQuantity(str[1])
+
+    echo print(qa) & " + " & print(qb) & " = " & print(qa + qb)
+
+proc debugSub(str: varargs[string]) =
+    let qa = parseQuantity(str[0])
+    let qb = parseQuantity(str[1])
+
+    echo print(qa) & " - " & print(qb) & " = " & print(qa - qb)
+
+proc debugMul(str: varargs[string]) =
+    let qa = parseQuantity(str[0])
+    let qb = parseQuantity(str[1])
+
+    echo print(qa) & " * " & print(qb) & " = " & print(qa * qb)
+
+
 when isMainModule:
-    debugGetSignature([KLength], [])
-    debugGetSignature([KLength, KLength], [])
-    debugGetSignature([KLength, KLength, KLength], [])
-    debugGetSignature([KLength], [KTime])
-    debugGetSignature([KLength], [KTime, KTime])
-    debugGetSignature([KMass], [KLength, KLength])
-    debugGetSignature([KMass], [KLength, KLength, KLength])
-    debugGetSignature([KLength, KMass], [KTime, KTime])
+    let pu1 = parseUnits("m/s")
+    debugGetType(pu1)
+    let pu2 = parseUnits("m/s2")
+    debugGetType(pu2)
+
+    echo $(parseQuantity("3 m/s"))
+
+    echo $(parseQuantity("127/5000 m"))
+    echo $(parseQuantity("5 in"))
+
+    echo $(parseQuantity("2 m2"))
+    echo $(parseQuantity("2 m3"))
+
+    echo $(parseQuantity("2 in2"))
+    echo $(parseQuantity("2 in3"))
+
+    echo $(UnitDefinitions)
+
+    echo "-----------------------------"
+
+    debugAdd "2 m", "3 m"
+    debugAdd "2 m", "3 in"
+
+    debugAdd "2 m/s", "3 m/s"
+    debugAdd "2 m/s", "3 in/s"
+    debugAdd "3 m/s", "2000 m/hr"
+    echo $(parseQuantity("5000 in"))
+    debugAdd "3 m/s", "50000 in/hr"
+    debugAdd "3 m/s", "1270 m/hr"
