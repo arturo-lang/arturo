d068599145ecae60218ce142c8dc3c14a6a767be
drkameleon <yaniszaf@gmail.com>
2023-04-28 18:13:26 +0200

    Helpers/jsonobject: added support for our new Quantity values

--- src/helpers/jsonobject.nim
+++ src/helpers/jsonobject.nim
@@ -54,7 +54,7 @@ proc generateJsonNode*(n: Value): JsonNode =
                 result.add(generateJsonNode(v))
         of Symbol,
            SymbolLiteral: result = newJString($(n.m))
-        of Quantity     : result = generateJsonNode(n.nm)
+        of Quantity     : result = newJString($(n.q))
         of Regex        : result = newJString($(n.rx))
         of Color        : result = newJString($(n))
         of Date         : discard
@@ -136,7 +136,7 @@ when defined(WEB):
                 result = toJs(ret)
             of Symbol,
                SymbolLiteral: result = toJs($(n.m))
-            of Quantity     : result = generateJsObject(n.nm)
+            of Quantity     : result = toJs($(n.q))
             of Regex        : result = toJs($(n.rx))
             of Color        : discard
             of Date         : discard
