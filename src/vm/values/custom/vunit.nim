5f43c2214129bf58e24b7d8895a034b7d3d45c3d
drkameleon <yaniszaf@gmail.com>
2023-05-02 14:04:14 +0200

    added draft implementation for new VUnit module (just separate the two)

---
+++ src/vm/values/custom/vunit.nim
@@ -0,0 +1,9 @@
+#=======================================================
+# Arturo
+# Programming Language + Bytecode VM compiler
+# (c) 2019-2023 Yanis Zafirópulos
+#
+# @file: vm/values/custom/vunit.nim
+#=======================================================
+
+## The internal `:unit` type
