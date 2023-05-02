b20f243b052be2a2fbea01c6d721f47231a2072d
drkameleon <yaniszaf@gmail.com>
2023-05-02 13:35:10 +0200

    fix module name for Quantities

--- src/vm/lib.nim
+++ src/vm/lib.nim
@@ -188,9 +188,14 @@ template constant*(n: string, alias: VSymbol, description: string, v: Value): un
     ## add new constant with given name, alias, description - 
     ## followed by the value it's assigned to
     SetSym(n, v)
+    let moduleName = 
+        when ((static (instantiationInfo().filename).replace(".nim")) != "macros"):
+            (static (instantiationInfo().filename.replace(".nim")))
+        else:
+            "Quantities"
     var vInfo = ValueInfo(
         descr: description,
-        module: static (instantiationInfo().filename).replace(".nim"),
+        module: moduleName,
         kind: v.kind
     )
 
