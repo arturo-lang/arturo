a1a9942b5d44e5b02753f3c1e3ef743ef1895a5a
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2023-05-31 16:27:07 -0300

    [Numbers\atan] add `:rational` support

--- src/library/Numbers.nim
+++ src/library/Numbers.nim
@@ -323,7 +323,7 @@ proc defineSymbols*() =
         rule        = PrefixPrecedence,
         description = "calculate the inverse tangent of given angle",
         args        = {
-            "angle" : {Integer,Floating,Complex,Quantity}
+            "angle" : {Integer,Floating,Complex,Rational,Quantity}
         },
         attrs       = NoAttrs,
         returns     = {Floating,Complex},
