fb02ec8d9bf821d277f21ead603af930fff3217a
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2023-05-31 16:26:21 -0300

    [Numbers\asech] add `:rational` support

--- src/library/Numbers.nim
+++ src/library/Numbers.nim
@@ -260,7 +260,7 @@ proc defineSymbols*() =
         rule        = PrefixPrecedence,
         description = "calculate the inverse hyperbolic secant of given angle",
         args        = {
-            "angle" : {Integer,Floating,Complex,Quantity}
+            "angle" : {Integer,Floating,Complex,Rational,Quantity}
         },
         attrs       = NoAttrs,
         returns     = {Floating,Complex},
