4a920f7f6724674401e44e47316f5f2f90341b28
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2023-05-31 16:25:13 -0300

    [Numbers\actanh] add `:rational` support

--- src/library/Numbers.nim
+++ src/library/Numbers.nim
@@ -201,7 +201,7 @@ proc defineSymbols*() =
         rule        = PrefixPrecedence,
         description = "calculate the inverse hyperbolic cotangent of given angle",
         args        = {
-            "angle" : {Integer,Floating,Complex,Quantity}
+            "angle" : {Integer,Floating,Complex,Rational,Quantity}
         },
         attrs       = NoAttrs,
         returns     = {Floating,Complex},
