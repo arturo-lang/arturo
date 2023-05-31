35e17e28e1c4a83c5a700bb6bda7c2da03dfe94e
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2023-05-31 16:37:46 -0300

    [Numbers\sinh] add `:rational` support

--- src/library/Numbers.nim
+++ src/library/Numbers.nim
@@ -1344,7 +1344,7 @@ proc defineSymbols*() =
         rule        = PrefixPrecedence,
         description = "calculate the hyperbolic sine of given angle",
         args        = {
-            "angle" : {Integer,Floating,Complex,Quantity}
+            "angle" : {Integer,Floating,Complex,Rational,Quantity}
         },
         attrs       = NoAttrs,
         returns     = {Floating,Complex},
