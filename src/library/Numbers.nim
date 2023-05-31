55e31f2129d2d49a4e69850019bf3cc5a5767e5a
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2023-05-31 16:24:44 -0300

    [Numbers\actan] add `:rational` support

--- src/library/Numbers.nim
+++ src/library/Numbers.nim
@@ -180,7 +180,7 @@ proc defineSymbols*() =
         rule        = PrefixPrecedence,
         description = "calculate the inverse cotangent of given angle",
         args        = {
-            "angle" : {Integer,Floating,Complex,Quantity}
+            "angle" : {Integer,Floating,Complex,Rational,Quantity}
         },
         attrs       = NoAttrs,
         returns     = {Floating,Complex},
