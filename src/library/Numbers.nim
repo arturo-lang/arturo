f9e34eab244cbe7a66d3673f9101abffa84f06df
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2023-05-31 16:29:25 -0300

    [Numbers\cosh] add `:rational` support

--- src/library/Numbers.nim
+++ src/library/Numbers.nim
@@ -495,7 +495,7 @@ proc defineSymbols*() =
         rule        = PrefixPrecedence,
         description = "calculate the hyperbolic cosine of given angle",
         args        = {
-            "angle" : {Integer,Floating,Complex,Quantity}
+            "angle" : {Integer,Floating,Complex,Rational,Quantity}
         },
         attrs       = NoAttrs,
         returns     = {Floating,Complex},
