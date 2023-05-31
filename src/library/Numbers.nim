4d1ce9c27d1441a198d8b3512c070d3a4e62f4d3
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2023-05-31 16:37:11 -0300

    [Numbers\sec] add `:rational` support

--- src/library/Numbers.nim
+++ src/library/Numbers.nim
@@ -1281,7 +1281,7 @@ proc defineSymbols*() =
         rule        = PrefixPrecedence,
         description = "calculate the secant of given angle",
         args        = {
-            "angle" : {Integer,Floating,Complex,Quantity}
+            "angle" : {Integer,Floating,Complex,Rational,Quantity}
         },
         attrs       = NoAttrs,
         returns     = {Floating,Complex},
