691e5926a132c4325514d83ac01e515388cf7a5b
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2023-05-31 15:53:01 -0300

    [Numbers\acos] add `:rational` support

--- src/library/Numbers.nim
+++ src/library/Numbers.nim
@@ -96,7 +96,7 @@ proc defineSymbols*() =
         rule        = PrefixPrecedence,
         description = "calculate the inverse cosine of given angle",
         args        = {
-            "angle" : {Integer,Floating,Complex,Quantity}
+            "angle" : {Integer,Floating,Complex,Rational,Quantity}
         },
         attrs       = NoAttrs,
         returns     = {Floating,Complex},
