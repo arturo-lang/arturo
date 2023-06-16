e23d836a6c17d8605c4fecb4df165fb3926417f6
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2023-06-16 14:32:32 -0300

    [Converters\array]: remove TODO

--- src/library/Converters.nim
+++ src/library/Converters.nim
@@ -665,10 +665,6 @@ proc defineSymbols*() =
                         if tp!=TextData:
                             execUnscoped(doParse(x.s, isFile=false))
                         else:
-                            # TODO(Converters/array) should produce valid error messages
-                            #  currently, we are just outputing a string. Preferrable, it should be done
-                            #  with a proper error being thrown and declared in VM/errors
-                            #  labels: enhancement, error handling
                             RuntimeError_FileNotFound(x.s)
                         let arr: ValueArray = sTopsFrom(stop)
                         SP = stop
