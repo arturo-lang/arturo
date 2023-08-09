4aaa8d0f16751d16554fd1c1a8bbd4c113765983
drkameleon <yaniszaf@gmail.com>
2023-08-09 17:23:47 +0200

    added TODO

--- src/helpers/bignums.nim
+++ src/helpers/bignums.nim
@@ -86,6 +86,12 @@ func canonicalize*(x: Rat) =
 #=======================================
 
 func newInt*(x: culong): Int =
+    # TODO(Helpers/bignums) fix warning with `newInt`, `newFloat`, `newRat`
+    #  this triggers the following warning:
+    #  ```
+    #  Warning: A custom '=destroy' hook which takes a 'var T' parameter is deprecated; it should take a 'T' parameter [Deprecated]
+    #  ```
+    #  labels: 3rd-party, enhancement
     new(result, finalizeInt)
     mpz_init_set_ui(result[], x)
 
