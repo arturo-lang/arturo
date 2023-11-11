92d978223ce6db42183445d2831c12f26be3b08c
drkameleon <yaniszaf@gmail.com>
2023-11-11 08:51:03 +0100

    Helpers/package: added `parseVersionCondition` helper method

--- src/helpers/packager.nim
+++ src/helpers/packager.nim
@@ -10,27 +10,78 @@
 # Libraries
 #=======================================
 
-# import algorithm, bitops, std/math, sequtils, sugar
+# when not defined(WEB):
+#     import asyncdispatch, httpClient, os
 
-# when defined(WEB):
-#     import std/jsbigints
-# elif not defined(NOGMP):
-#     import helpers/bignums as BignumsHelper
-
-# when defined(NOGMP):
+# when defined(SAFE):
 #     import vm/errors
-    
-# import vm/values/value
+
+# when not defined(WEB):
+#     import helpers/url
+
+# when defined(PORTABLE):
+#     import tables
+#     import vm/globals
+
+import vm/values/value
+
+#=======================================
+# Types
+#=======================================
+
+type
+    VersionConditionKind* = enum
+        VersionEqual,
+        VersionGreater,
+        VersionGreaterOrEqual,
+        VersionLess,
+        VersionLessOrEqual
+
+    # DataSourceKind* = enum
+    #     WebData,
+    #     FileData,
+    #     TextData
+
+    # DataSource* = (string, DataSourceKind)
+
+#=======================================
+# Helpers
+#=======================================
+
+proc parseVersionCondition*(vsym: Value): VersionConditionKind =
+    case vsym.m:
+        of equal:
+            return VersionIs
+        of greaterthan:
+            return VersionGreater
+        of greaterequal:
+            return VersionGreaterOrEqual
+        of lessthan:
+            return VersionLess
+        of equalless:
+            return VersionLessOrEqual
+        else:
+            # shouldn't reach here
+            return VersionIs
 
 #=======================================
 # Methods
 #=======================================
 
-# func addmod*[T: SomeInteger](a, b, modulus: T): T =
-#     let a_m = if a < modulus: a else: a mod modulus
-#     if b == 0.T: return a_m
-#     let b_m = if b < modulus: b else: b mod modulus
- 
-#     let b_from_m = modulus - b_m
-#     if a_m >= b_from_m: return a_m - b_from_m
-#     return a_m + b_m 
+proc getType*(src: string): DataSource {.inline.} =
+    when not defined(WEB):
+        when defined(PORTABLE):
+            if SymExists("_portable") and GetSym("_portable").d.hasKey("embed") and GetSym("_portable").d["embed"].d.hasKey(src):
+                return (GetSym("_portable").d["embed"].d[src].s, FileData)
+                            
+        if src.isUrl():
+            when defined(SAFE): RuntimeError_OperationNotPermitted("read")
+            let content = waitFor (newAsyncHttpClient().getContent(src))
+            result = (content, WebData)
+        elif src.fileExists():
+            when defined(SAFE): RuntimeError_OperationNotPermitted("read")
+            result = (readFile(src), FileData)
+        else:
+            result = (src, TextData)
+    else:
+        result = (src, TextData)
