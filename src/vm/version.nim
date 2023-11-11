22e4f27074474442c8ce90e873c2705a25ba12b7
drkameleon <yaniszaf@gmail.com>
2023-11-11 06:39:30 +0100

    show version pretty much as it is in the REPL

--- src/vm/version.nim
+++ src/vm/version.nim
@@ -25,4 +25,4 @@ const
     ArturoVersion*    = static readFile("version/version").strip()      ## The current version of Arturo
     ArturoBuild*      = static readFile("version/build").strip()        ## The current build number of Arturo
 
-    ArturoVersionTxt* = "arturo v/" & ArturoVersion                     ## Default version text
+    ArturoVersionTxt* = "arturo v/" & ArturoVersion & " b/" & ArturoBuild                     ## Default version text
