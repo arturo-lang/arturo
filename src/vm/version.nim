2798cb392daa2744f7536047ab78c389af2f89c8
drkameleon <yaniszaf@gmail.com>
2023-11-11 06:49:29 +0100

    cleanup

--- src/vm/version.nim
+++ src/vm/version.nim
@@ -25,4 +25,6 @@ const
     ArturoVersion*    = static readFile("version/version").strip()      ## The current version of Arturo
     ArturoBuild*      = static readFile("version/build").strip()        ## The current build number of Arturo
 
-    ArturoVersionTxt* = "arturo v/" & ArturoVersion & " b/" & ArturoBuild                     ## Default version text
+    ArturoVersionTxt* = "arturo v/" & ArturoVersion &                   ## Default version text
+                        " b/" & ArturoBuild
+                        
