3929717ba0b7d3a1664ed4ab0fbf655b2a22043c
drkameleon <yaniszaf@gmail.com>
2023-11-11 09:14:29 +0100

    rough draft

--- src/helpers/packager.nim
+++ src/helpers/packager.nim
@@ -10,6 +10,11 @@
 # Libraries
 #=======================================
 
+import os, strformat, strutils
+
+when not defined(WEB):
+    import helpers/url
+
 # when not defined(WEB):
 #     import asyncdispatch, httpClient, os
 
@@ -23,7 +28,11 @@
 #     import tables
 #     import vm/globals
 
+import vm/[env]
+
 import vm/values/value
+
+import vm/values/custom/[vversion]
 
 #=======================================
 # Types
@@ -36,6 +45,14 @@ type
         VersionGreaterOrEqual,
         VersionLess,
         VersionLessOrEqual
+
+    VersionSpec* = (VersionConditionKind, VVersion)
+
+    PackageKind* = enum
+        OfficialPackage,
+        UserRepo,
+        LocalFile,
+        InvalidPackage
 
     # DataSourceKind* = enum
     #     WebData,
@@ -51,7 +68,7 @@ type
 proc parseVersionCondition*(vsym: Value): VersionConditionKind =
     case vsym.m:
         of equal:
-            return VersionIs
+            return VersionEqual
         of greaterthan:
             return VersionGreater
         of greaterequal:
@@ -62,26 +79,56 @@ proc parseVersionCondition*(vsym: Value): VersionConditionKind =
             return VersionLessOrEqual
         else:
             # shouldn't reach here
-            return VersionIs
+            return VersionEqual
+
+proc isLocalFile*(src: string): bool =
+    if src.fileExists():
+        return true
+    else:
+        if (src & ".art").fileExists():
+            return true
+        else:
+            return false
+
+proc isLocalPackage*(src: string, version: VersionSpec): bool =
+    let expectedPath = "{HomeDir}/.arturo/packages/cache/{src}".fmt
+    if expectedPath.dirExists():
+        return true
+    else:
+        return false
 
 #=======================================
 # Methods
 #=======================================
 
-proc getType*(src: string): DataSource {.inline.} =
-    when not defined(WEB):
-        when defined(PORTABLE):
-            if SymExists("_portable") and GetSym("_portable").d.hasKey("embed") and GetSym("_portable").d["embed"].d.hasKey(src):
-                return (GetSym("_portable").d["embed"].d[src].s, FileData)
-                            
-        if src.isUrl():
-            when defined(SAFE): RuntimeError_OperationNotPermitted("read")
-            let content = waitFor (newAsyncHttpClient().getContent(src))
-            result = (content, WebData)
-        elif src.fileExists():
-            when defined(SAFE): RuntimeError_OperationNotPermitted("read")
-            result = (readFile(src), FileData)
+proc getPackage*(src: string, version: VersionSpec): PackageKind {.inline.} =
+    if src.isUrl():
+        if ".pkgr.art" in src:
+            return OfficialPackage
         else:
-            result = (src, TextData)
+            return UserRepo
+    elif isLocalFile(src):
+        return LocalFile
     else:
-        result = (src, TextData)
+        if isLocalPackage(src, version):
+            return OfficialPackage
+        else:
+            return InvalidPackage
+
+# proc getType*(src: string): DataSource {.inline.} =
+#     when not defined(WEB):
+#         when defined(PORTABLE):
+#             if SymExists("_portable") and GetSym("_portable").d.hasKey("embed") and GetSym("_portable").d["embed"].d.hasKey(src):
+#                 return (GetSym("_portable").d["embed"].d[src].s, FileData)
+                            
+#         if src.isUrl():
+#             when defined(SAFE): RuntimeError_OperationNotPermitted("read")
+#             let content = waitFor (newAsyncHttpClient().getContent(src))
+#             result = (content, WebData)
+#         elif src.fileExists():
+#             when defined(SAFE): RuntimeError_OperationNotPermitted("read")
+#             result = (readFile(src), FileData)
+#         else:
+#             result = (src, TextData)
+#     else:
+#         result = (src, TextData)
