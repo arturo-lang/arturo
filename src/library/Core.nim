14574b7119150fc75c7f39b9b9f60476271779b7
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2023-11-23 19:05:31 -0300

    `Core\try?` remove TODO

--- src/library/Core.nim
+++ src/library/Core.nim
@@ -811,25 +811,6 @@ proc defineSymbols*() =
             except CatchableError, Defect:
                 push(VTRUE)
 
-    # TODO(Core) add new `catch` method?
-    #  Currently, `try?` works with `else`, pretty much like `if?`
-    #  but we cannot do anything with the exception itself, in case
-    #  this `try?` has failed
-    #
-    #  So, why not add a `catch` method, where we could do something like:
-    #  ```
-    #  try? [
-    #      ; let's try something dangerous
-    #      print 10 / 0
-    #  ]
-    #  catch 'e [
-    #      print "something went terribly wrong..."
-    #      print e
-    #  ]
-    #  ```
-    #  In that case, `e` would hold the Exception, which should preferrably be
-    #  of a distinct Exception type.
-    #  labels: library,new feature,enhancement,open discussion
     builtin "try?",
         alias       = unaliased, 
         op          = opNop,
