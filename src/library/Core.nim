e65501f712dce0011624739540fa693eddc73b32
drkameleon <yaniszaf@gmail.com>
2023-11-29 10:24:37 +0100

    moved TODO

--- src/library/Core.nim
+++ src/library/Core.nim
@@ -42,6 +42,26 @@ import vm/[env, errors, eval, exec, parse]
 #  this could easily work with a new `:exception` built-in type
 #  labels: library, new feature,open discussion
 
+# TODO(Core) add new `catch` method?
+#  Currently, `try?` works with `else`, pretty much like `if?`
+#  but we cannot do anything with the exception itself, in case
+#  this `try?` has failed
+#
+#  So, why not add a `catch` method, where we could do something like:
+#  ```
+#  try? [
+#      ; let's try something dangerous
+#      print 10 / 0
+#  ]
+#  catch 'e [
+#      print "something went terribly wrong..."
+#      print e
+#  ]
+#  ```
+#  In that case, `e` would hold the Exception, which should preferrably be
+#  of a distinct Exception type.
+#  labels: library,new feature,enhancement,open discussion
+
 proc defineLibrary*() =
 
     #----------------------------
@@ -1070,25 +1090,6 @@ proc defineLibrary*() =
             except CatchableError, Defect:
                 push(VTRUE)
 
-    # TODO(Core) add new `catch` method?
-    #  Currently, `try?` works with `else`, pretty much like `if?`
-    #  but we cannot do anything with the exception itself, in case
-    #  this `try?` has failed
-    #
-    #  So, why not add a `catch` method, where we could do something like:
-    #  ```
-    #  try? [
-    #      ; let's try something dangerous
-    #      print 10 / 0
-    #  ]
-    #  catch 'e [
-    #      print "something went terribly wrong..."
-    #      print e
-    #  ]
-    #  ```
-    #  In that case, `e` would hold the Exception, which should preferrably be
-    #  of a distinct Exception type.
-    #  labels: library,new feature,enhancement,open discussion
     builtin "try?",
         alias       = unaliased, 
         op          = opNop,
