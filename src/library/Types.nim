2ac677e81190c2089859c303590e3d9de4ffcead
drkameleon <yaniszaf@gmail.com>
2023-11-29 11:58:13 +0100

    moved `type` from Reflection to Types

--- src/library/Types.nim
+++ src/library/Types.nim
@@ -29,6 +29,26 @@ proc defineLibrary*() =
     # Functions
     #----------------------------
 
+    builtin "type",
+        alias       = unaliased, 
+        op          = opNop,
+        rule        = PrefixPrecedence,
+        description = "get type of given value",
+        args        = {
+            "value" : {Any}
+        },
+        attrs       = NoAttrs,
+        returns     = {Type},
+        example     = """
+            print type 18966          ; :integer
+            print type "hello world"  ; :string
+        """:
+            #=======================================================
+            if xKind != Object:
+                push(newType(xKind))
+            else:
+                push(newUserType(x.proto.name))
+
     #----------------------------
     # Predicates
     #----------------------------
