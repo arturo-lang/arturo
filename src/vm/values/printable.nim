2e6886f8c8b8fa9dcd3539741c5ba6f42a96a2bf
drkameleon <yaniszaf@gmail.com>
2023-12-22 17:01:28 +0100

    adjusted dumping for Object values

--- src/vm/values/printable.nim
+++ src/vm/values/printable.nim
@@ -185,6 +185,10 @@ proc `$`*(v: Value): string {.inline.} =
 
 
 proc dump*(v: Value, level: int=0, isLast: bool=false, muted: bool=false, prepend="") {.exportc.} = 
+
+    proc dumpGeneric(str: string, v: Value) =
+        if not muted:   stdout.write fmt("{resetColor}{str}{fg(grayColor)} :{($(v.kind)).toLowerAscii()}{resetColor}")
+        else:           stdout.write fmt("{str} :{($(v.kind)).toLowerAscii()}")
     
     proc dumpPrimitive(str: string, v: Value) =
         if not muted:   stdout.write fmt("{bold(greenColor)}{str}{fg(grayColor)} :{($(v.kind)).toLowerAscii()}{resetColor}")
@@ -386,8 +390,12 @@ proc dump*(v: Value, level: int=0, isLast: bool=false, muted: bool=false, prepen
                     for i in 0..level: stdout.write "        "
 
                     stdout.write unicode.alignLeft(key & " ", maxLen) & ":"
-
-                    dump(value, level+1, false, muted=muted)
+                    echo "--> " & $(value.kind)
+                    if value.kind == Method:
+                        for i in 0..level: stdout.write "        "
+                        dumpGeneric("(" & value.params.filter((zz) => zz != "this").join(", ") & ")", value)
+                    else:
+                        dump(value, level+1, false, muted=muted)
 
             dumpBlockEnd()
 
