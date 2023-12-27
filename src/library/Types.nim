c2b07d59aa6dbdc3f49f0767eef436d1bbad0aaa
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2023-12-27 18:49:12 -0300

    [Types\defined?]: add documentation

--- src/library/Types.nim
+++ src/library/Types.nim
@@ -604,6 +604,26 @@ proc defineLibrary*() =
         # TODO(Types\defined?) add documentation example
         #  labels: library, documentation, easy
         example     = """
+            defined? :cat
+            ; => false
+            defined? "cat"
+            ; => false
+            defined? 'cat
+            ; => false
+
+            define :cat [
+                init: constructor [name :string age :integer]
+            ]
+
+            defined? :cat
+            ; => true
+            defined? "cat"
+            ; => true
+            defined? 'cat
+            ; => true
+
+            defined? :dog
+            ; => false
         """:
             #=======================================================
             if xKind == Type:
