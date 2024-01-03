25c4f01275e07791ecd9780d4d18982ed3169993
drkameleon <yaniszaf@gmail.com>
2024-01-03 10:49:09 +0100

    removed TODO

--- src/library/Core.nim
+++ src/library/Core.nim
@@ -769,13 +769,6 @@ proc defineLibrary*() =
                 if multiple:
                     push(newBlock(ret))
 
-    # TODO(Core\let) block assignments should properly handle readonly Values
-    #  In a few words: we should make sure that `[a b]: [1 2]` is the same as 
-    #  assigning each value one by one, which means that there should be an *implicit* 
-    #  new Value created for readonly value. Apparently, `setSym` in VM/globals 
-    #  doesn't handle this properly; but it should.
-    #  See also: https://discord.com/channels/765519132186640445/829324913097048065/1099426535569633401
-    #  labels: library, bug, critical
     builtin "let",
         alias       = colon, 
         op          = opNop,
