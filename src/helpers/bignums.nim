12a6a515943af6061ae9345c29c46307fbc8e4ff
drkameleon <yaniszaf@gmail.com>
2024-01-11 18:15:46 +0100

    undo debugging

--- src/helpers/bignums.nim
+++ src/helpers/bignums.nim
@@ -1177,14 +1177,3 @@ func clear*(z: Float) {.inline.} =
 func clear*(z: Rat) {.inline.} =
     GCunref(z)
     finalizeRat(z)
-
-
-#####
-
-var 
-    num1 = 123456789000
-    num2 = 987654321000
-    
-    rat = newRat(num1, num2)
-
-echo $(rat)
