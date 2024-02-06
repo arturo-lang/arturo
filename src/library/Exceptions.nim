bfb8f451ae338d3f85d9b46d88790bef1c0bb5d7
Yanis Zafirópulos <1265028+drkameleon@users.noreply.github.com>
2024-02-06 13:01:06 +0100

    update names

--- src/library/Exceptions.nim
+++ src/library/Exceptions.nim
@@ -78,7 +78,7 @@ proc defineSymbols*() =
             elif xkind == ErrorKind:
                 x.errKind
             else:
-                verror.RuntimeError
+                RuntimeErr
 
             var error = verror.VError(kind: kind)
             if xkind == String:
@@ -173,6 +173,6 @@ proc defineSymbols*() =
     constant "genericError",
         alias       = unaliased,
         description = "a generic error":
-            newErrorKind(verror.RuntimeError)
+            newErrorKind(RuntimeErr)
 
 Libraries.add(defineSymbols)
