d7e3cff596d82b174591017be67ec16d92ecf48a
Yanis Zafirópulos <1265028+drkameleon@users.noreply.github.com>
2024-04-09 09:01:05 +0200

    added missing import

--- src/library/Reflection.nim
+++ src/library/Reflection.nim
@@ -27,7 +27,7 @@ when not defined(WEB):
 import helpers/terminal as TerminalHelper
 
 import vm/lib
-import vm/[errors, eval, exec]
+import vm/[errors, eval, exec, runtime]
 
 #=======================================
 # Definitions
