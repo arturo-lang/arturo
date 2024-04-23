b46a31edd328e0c1279b6a017c92400f095fe434
Yanis Zafirópulos <1265028+drkameleon@users.noreply.github.com>
2024-04-23 18:19:49 +0200

    remove debugging output

--- src/vm/errors.nim
+++ src/vm/errors.nim
@@ -182,12 +182,6 @@ proc printCodePreview(e: VError) =
         if (not IsRepl) and (e.kind != CmdlineErr) and (e.kind != ProgramErr) :
             if e.context.file == "":
                 e.context.line = CurrentLine
-                for i in 0..<FSP:
-                    debugEcho "fsp = " & $(i)
-                    if FrameStack[i].isNil:
-                        debugEcho "\t! is nil"
-                    else:
-                        debugEcho "\t" & $(FrameStack[i][])
                 if (let pcf = postCurrentFrame(); not pcf.isNil):
                     e.context.file = pcf.path
                 else:
