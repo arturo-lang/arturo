7205f2b5540440f758a76b1ef4e49f4e2aa20220
Yanis Zafirópulos <1265028+drkameleon@users.noreply.github.com>
2024-05-14 11:55:06 +0200

    added basic .exe checks + Arturo sources cloning

--- src/vm/bundle.nim
+++ src/vm/bundle.nim
@@ -8,12 +8,30 @@
 
 ## Bundled executable manager
 
+import os
 import osproc
 import vm/vm
 
-const tmpFolder = "_tmpart"
+when defined(DEV):
+    import std/private/osdirs
+
+const tmpFolder = 
+    when defined(DEV):
+        "../_tmpart"
+    else:
+        "_tmpart"
 
 var runBundler  = static readFile("src/scripts/bundler.art")
+
+proc copyDirRecursively(source: string, dest: string) =
+    createDir(dest)
+    for kind, path in walkDir(source):
+        var noSource = splitPath(path).tail
+        if noSource != ".git":
+            if kind == pcDir:
+                copyDirRecursively(path, dest / noSource)
+            else:
+                copyFile(path, dest / noSource, {cfSymlinkAsIs})
 
 proc commandExists(cmd: string) =
     let (_, exitCode) = execCmdEx("[ -x \"$(command -v " & cmd & ")\" ]")
@@ -21,17 +39,28 @@ proc commandExists(cmd: string) =
         echo "`" & cmd & "` command required; cannot proceed!"
         quit(1)
 
+proc cleanUp() =
+    removeDir(tmpFolder)
+
 proc checkNim() = commandExists("nim")
 proc checkGit() = commandExists("git")
 
 proc cloneArturo() =
-    let (_, res) = execCmdEx("git clone https://github.com/arturo-lang/arturo.git " & tmpFolder)
-    if res != 0:
-        echo "something went wrong when cloning Arturo sources..."
-        quit(1)
+    when defined(DEV):
+        copyDirRecursively(getCurrentDir(), tmpFolder)
+    else:
+        let (_, res) = execCmdEx("git clone https://github.com/arturo-lang/arturo.git " & tmpFolder)
+        if res != 0:
+            echo "something went wrong when cloning Arturo sources..."
+            quit(1)
 
 proc generateBundle*(filename: string) =
+    echo "- Checking Nim..."
     checkNim()
+    echo "- Checking Git..."
     checkGit()
+    echo "- Cloning sources..."
     cloneArturo()
+    echo "- Cleaning up..."
+    cleanUp()
     discard run(runBundler, @[], isFile=false)
