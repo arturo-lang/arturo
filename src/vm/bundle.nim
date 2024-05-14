dd94b11ea1dded90d9f043e534a708b22dddf7cd
Yanis Zafirópulos <1265028+drkameleon@users.noreply.github.com>
2024-05-14 12:36:33 +0200

    set dual-mode for `bundle` module

--- src/vm/bundle.nim
+++ src/vm/bundle.nim
@@ -8,59 +8,92 @@
 
 ## Bundled executable manager
 
-import os
-import osproc
-import vm/vm
+when defined(BUNDLE):
+    import vm/[values/value]
+    var 
+        bundled*: ValueDict 
+else:
+    import os
+    import osproc
+    import tables
+    import vm/vm
 
-when defined(DEV):
-    import std/private/osdirs
+    import helpers/path
 
-const tmpFolder = 
     when defined(DEV):
-        "../_tmpart"
-    else:
-        "_tmpart"
+        import std/private/osdirs
 
-var runBundler  = static readFile("src/scripts/bundler.art")
+    const tmpFolder = 
+        when defined(DEV):
+            "../_tmpart"
+        else:
+            "_tmpart"
 
-proc copyDirRecursively(source: string, dest: string) =
-    createDir(dest)
-    for kind, path in walkDir(source):
-        var noSource = splitPath(path).tail
-        if noSource != ".git":
-            if kind == pcDir:
-                copyDirRecursively(path, dest / noSource)
-            else:
-                copyFile(path, dest / noSource, {cfSymlinkAsIs})
+    var runBundler  = static readFile("src/scripts/bundler.art")
 
-proc commandExists(cmd: string) =
-    let (_, exitCode) = execCmdEx("[ -x \"$(command -v " & cmd & ")\" ]")
-    if exitCode != 0:
-        echo "`" & cmd & "` command required; cannot proceed!"
-        quit(1)
+    proc copyDirRecursively(source: string, dest: string) =
+        createDir(dest)
+        for kind, path in walkDir(source):
+            var noSource = splitPath(path).tail
+            if noSource != ".git":
+                if kind == pcDir:
+                    copyDirRecursively(path, dest / noSource)
+                else:
+                    copyFile(path, dest / noSource, {cfSymlinkAsIs})
 
-proc cleanUp() =
-    removeDir(tmpFolder)
-
-proc checkNim() = commandExists("nim")
-proc checkGit() = commandExists("git")
-
-proc cloneArturo() =
-    when defined(DEV):
-        copyDirRecursively(getCurrentDir(), tmpFolder)
-    else:
-        let (_, res) = execCmdEx("git clone https://github.com/arturo-lang/arturo.git " & tmpFolder)
-        if res != 0:
-            echo "something went wrong when cloning Arturo sources..."
+    proc commandExists(cmd: string) =
+        let (_, exitCode) = execCmdEx("[ -x \"$(command -v " & cmd & ")\" ]")
+        if exitCode != 0:
+            echo "`" & cmd & "` command required; cannot proceed!"
             quit(1)
 
-proc generateBundle*(filename: string) =
-    echo "- Checking Nim..."
-    checkNim()
-    echo "- Checking Git..."
-    checkGit()
-    echo "- Cloning sources..."
-    cloneArturo()
-    echo "- Cleaning up..."
-    cleanUp()
-    discard run(runBundler, @[], isFile=false)
+    proc cleanUp() =
+        removeDir(tmpFolder)
+
+    proc checkNim() = commandExists("nim")
+    proc checkGit() = commandExists("git")
+
+    proc cloneArturo() =
+        when defined(DEV):
+            copyDirRecursively(getCurrentDir(), tmpFolder)
+        else:
+            let (_, res) = execCmdEx("git clone https://github.com/arturo-lang/arturo.git " & tmpFolder)
+            if res != 0:
+                echo "something went wrong when cloning Arturo sources..."
+                quit(1)
+
+    proc buildExecutable(filename: string) =
+        let currentFolder = getCurrentDir()
+        setCurrentDir(tmpFolder)
+
+        let fileDetails = parsePathComponents(filename)
+        let binName = fileDetails["filename"].s
+
+        let (_, res) = execCmdEx("./build.nims build " & binName & " --bundle --as " & binName)
+        if res != 0:
+            echo "Something went wrong went building the project..."
+            quit(1)
+
+        copyFile(tmpFolder / "bin" / binName, currentFolder / binName)
+
+        setCurrentDir(currentFolder)
+
+    proc generateBundle*(filename: string) =
+        echo "- Checking Nim..."
+        checkNim()
+        
+        echo "- Checking Git..."
+        checkGit()
+        
+        echo "- Cloning sources..."
+        cloneArturo()
+        
+        echo "- Analyzing code..."
+        discard run(runBundler, @[tmpFolder, filename], isFile=false)
+        
+        # echo "- Building..."
+        # buildExecutable(filename)
+        
+        echo "- Cleaning up..."
+        #cleanUp()
+    
