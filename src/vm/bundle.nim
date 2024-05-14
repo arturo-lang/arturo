21b05f6b2ed9136de46a18af4b9d42e8f0613868
Yanis Zafirópulos <1265028+drkameleon@users.noreply.github.com>
2024-05-14 11:21:21 +0200

    started implementation of bundler module

--- src/vm/bundle.nim
+++ src/vm/bundle.nim
@@ -6,7 +6,32 @@
 # @file: vm/bundle.nim
 #=======================================================
 
-## Executable bundle generator
+## Bundled executable manager
+
+import osproc
+import vm/vm
+
+const tmpFolder = "_tmpart"
+
+var runBundler  = static readFile("src/scripts/bundler.art")
+
+proc commandExists(cmd: string) =
+    let (_, exitCode) = execCmdEx("[ -x \"$(command -v " & cmd & ")\" ]")
+    if exitCode != 0:
+        echo "`" & cmd & "` command required; cannot proceed!"
+        quit(1)
+
+proc checkNim() = commandExists("nim")
+proc checkGit() = commandExists("git")
+
+proc cloneArturo() =
+    let (_, res) = execCmdEx("git clone https://github.com/arturo-lang/arturo.git " & tmpFolder)
+    if res != 0:
+        echo "something went wrong when cloning Arturo sources..."
+        quit(1)
 
 proc generateBundle*(filename: string) =
-    echo "Generating bundle for " & filename
+    checkNim()
+    checkGit()
+    cloneArturo()
+    discard run(runBundler, @[], isFile=false)
