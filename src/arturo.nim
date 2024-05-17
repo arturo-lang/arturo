c2da6c3749061b9e39d6db534175e4c29c98a373
Yanis Zafirópulos <1265028+drkameleon@users.noreply.github.com>
2024-05-17 17:00:20 +0200

    further cleanup

--- src/arturo.nim
+++ src/arturo.nim
@@ -192,7 +192,6 @@ when isMainModule and not defined(WEB):
         var action: CmdAction = noAction
         var runConsole  = static readFile("src/scripts/console.art")
         #var runUpdate   = static readFile("src/scripts/update.art")
-        #var runModule   = static readFile("src/scripts/module.art")
         var muted: bool = not isColorFriendlyTerminal()
 
         var unrecognizedOption = ""
