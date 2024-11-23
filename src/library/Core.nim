2d00f7fd9f9098f45c53b5174d26246bae23d364
Yanis Zafirópulos <1265028+drkameleon@users.noreply.github.com>
2024-11-23 11:33:21 +0100

    Core\when?: added deprecation notice

--- src/library/Core.nim
+++ src/library/Core.nim
@@ -1738,6 +1738,7 @@ proc defineLibrary*() =
         attrs       = NoAttrs,
         returns     = {Logical},
         example     = """
+            ; DEPRECATED!
             a: 2
             case [a]
                 when? [<2] -> print "a is less than 2"
