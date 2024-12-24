a025e4024a26f6a8a098f7a93b0cbed77ea9f8df
Yanis Zafirópulos <1265028+drkameleon@users.noreply.github.com>
2024-12-24 14:01:38 +0100

    a few more changes

--- src/extras/pfd.nim
+++ src/extras/pfd.nim
@@ -3,7 +3,7 @@
 # Sam Hocevar's Portable File Dialogs wrapper
 # for Nim
 #
-# (c) 2024 Yanis Zafirópulos
+# (c) 2019-2025 Yanis Zafirópulos
 # 
 # @license: see LICENSE file
 # @file: extras/pfd.nim
