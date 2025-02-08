330b5066f174409eb546ad29a868ac80b8c46734
Yanis Zafirópulos <1265028+drkameleon@users.noreply.github.com>
2025-02-08 12:03:58 +0100

    removed older definitions + added proper header + cleanup

--- src/extras/gmp.nim
+++ src/extras/gmp.nim
@@ -798,23 +798,11 @@ func mpz_xor*(a2: var mpz_t; a3: mpz_t; a4: mpz_t) {.importc.}
 {.pop.}
 
 #=======================================
-# Methods
+# Destructors
 #=======================================
 
 proc `=destroy`*(x: mm_mpz_struct) =
     mpz_clear(addr x)
 
-# proc `=destroy`*[T: mpf_t](x: var T) =
-#     mpf_clear(x)
-
 proc `=destroy`*(x: mm_mpq_struct) =
     mpq_clear(addr x)
-
-# func finalizeInt*(z: ref mpz_t) =
-#     mpz_clear(z[])
-
-# # func finalizeFloat*(z: ref mpf_t) =
-# #     mpf_clear(z[])
-
-# func finalizeRat*(z: m) =
-#     mpq_clear(z[])
