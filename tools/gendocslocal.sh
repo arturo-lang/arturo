ff052e0d9b1712d7748fe63fa6e74c05c254c2e1
drkameleon <yaniszaf@gmail.com>
2025-10-31 17:28:21 +0100

    debugging + cleanup

--- tools/gendocslocal.sh
+++ tools/gendocslocal.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
-arturo-docg tools/sitegen.art
-arturo-docg tools/indexgen.art
+# arturo-docg tools/sitegen.art
+# arturo-docg tools/indexgen.art
 
 echo "version: \"$(cat version/version)\"" > docs/website/pages/_index.art 
 echo "buildDate: \"$(date -u)\"" >> docs/website/pages/_index.art 
 echo "#[release?: false]" > docs/website/data/setup.art
 
-rm -rf tmpdocs
-mkdir tmpdocs
+# rm -rf tmpdocs
+# mkdir tmpdocs
 cd docs/website
 
 arturo-docg ../../tools/miniwebize/webize.art --build --at: ../../tmpdocs
 cd ../..
 
 rm -rf /Applications/XAMPP/xamppfiles/htdocs/arturodocs
-cp -r tmpdocs /Applications/XAMPP/xamppfiles/htdocs/arturodocs
+mkdir /Applications/XAMPP/xamppfiles/htdocs/arturodocs
+cp -r tmpdocs/* /Applications/XAMPP/xamppfiles/htdocs/arturodocs
