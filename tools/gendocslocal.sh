55f2af304f955eed6732682ebde45d18432c7086
drkameleon <yaniszaf@gmail.com>
2025-10-31 17:46:09 +0100

    added comments

--- tools/gendocslocal.sh
+++ tools/gendocslocal.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+## comment-out for faster testing (e.g. just style/script changes)
 arturo-docg tools/sitegen.art
 arturo-docg tools/indexgen.art
 
@@ -7,8 +8,10 @@ echo "version: \"$(cat version/version)\"" > docs/website/pages/_index.art
 echo "buildDate: \"$(date -u)\"" >> docs/website/pages/_index.art 
 echo "#[release?: false]" > docs/website/data/setup.art
 
+## comment-out for faster testing (see: above)
 rm -rf tmpdocs
 mkdir tmpdocs
+
 cd docs/website
 
 arturo-docg ../../tools/miniwebize/webize.art --build --at: ../../tmpdocs
