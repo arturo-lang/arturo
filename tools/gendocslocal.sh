e1debec937c5b579de7ffe537677733bbf27ed2c
drkameleon <yaniszaf@gmail.com>
2025-10-31 17:45:17 +0100

    restore debugging script

--- tools/gendocslocal.sh
+++ tools/gendocslocal.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-# arturo-docg tools/sitegen.art
+arturo-docg tools/sitegen.art
 arturo-docg tools/indexgen.art
 
 echo "version: \"$(cat version/version)\"" > docs/website/pages/_index.art 
 echo "buildDate: \"$(date -u)\"" >> docs/website/pages/_index.art 
 echo "#[release?: false]" > docs/website/data/setup.art
 
-# rm -rf tmpdocs
-# mkdir tmpdocs
+rm -rf tmpdocs
+mkdir tmpdocs
 cd docs/website
 
 arturo-docg ../../tools/miniwebize/webize.art --build --at: ../../tmpdocs
