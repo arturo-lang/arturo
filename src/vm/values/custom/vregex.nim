3f8c46617adadcf4d82b845954ed16b1b6059b1c
drkameleon <yaniszaf@gmail.com>
2025-11-19 13:41:19 +0100

    fixed minor typo

--- src/vm/values/custom/vregex.nim
+++ src/vm/values/custom/vregex.nim
@@ -25,7 +25,7 @@ elif defined(linux):
     else:   
         {.passL: "-Bstatic -Lsrc/deps/pcre/linux/amd64 -lpcre -Bdynamic".}
 elif defined(freebsd):
-    {.passL: "-Bstatic -src/deps/pcre/deps/freebsd/amd64 -lpcre -Bdynamic".}
+    {.passL: "-Bstatic -src/deps/pcre/freebsd/amd64 -lpcre -Bdynamic".}
 
 #=======================================
 # Libraries
