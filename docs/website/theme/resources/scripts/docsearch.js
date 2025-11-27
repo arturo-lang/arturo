35600be722747c8f9dca33663137b3cd12115a97
drkameleon <yaniszaf@gmail.com>
2025-11-27 16:36:05 +0100

    replaced yet another icon

--- docs/website/theme/resources/scripts/docsearch.js
+++ docs/website/theme/resources/scripts/docsearch.js
@@ -240,7 +240,7 @@
         dropdown.innerHTML = `
             <div class="search-no-results">
                 <div class="search-no-results-icon">
-                    <i class="fa fa-search"></i>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="1.2em" height="1.2em" fill="currentColor" viewBox="0 0 256 256"><path d="M232.49,215.51,185,168a92.12,92.12,0,1,0-17,17l47.53,47.54a12,12,0,0,0,17-17ZM44,112a68,68,0,1,1,68,68A68.07,68.07,0,0,1,44,112Z"></path></svg>
                 </div>
                 <div class="search-no-results-text">
                     No results found for "<strong>${escapeHtml(query)}</strong>"
