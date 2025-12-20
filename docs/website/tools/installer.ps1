a0540766bee649541ad721fd971d78e216244b0b
drkameleon <yaniszaf@gmail.com>
2025-12-20 12:38:49 +0100

    minor fix

--- docs/website/tools/installer.ps1
+++ docs/website/tools/installer.ps1
@@ -319,7 +319,7 @@ function Main {
         Write-Section "Installing..."
         Install-Arturo
         
-        Write-Section "Done! ✓"
+        Write-Section "Done!"
         Show-Footer
     } finally {
         Cleanup
