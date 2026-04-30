95405932b573ff469d94ffd5fa33a2543ee42be5
drkameleon <yaniszaf@gmail.com>
2026-04-30 14:50:29 +0200

    [Dates] convert `before` to use dispatchWithLiteral

--- src/library/Dates.nim
+++ src/library/Dates.nim
@@ -153,11 +153,10 @@ proc defineModule*(moduleName: string) =
                 years=years
             )
             
-            if xKind in {Literal, PathLiteral}:
-                ensureInPlaceAny()
-                SetInPlaceAny(newDate(InPlaced.eobj - ti))
-            else:
-                push(newDate(x.eobj - ti))
+            dispatchWithLiteral:
+                Date(d):
+                    value:   push(newDate(d - ti))
+                    inplace: SetInPlaceAny(newDate(d - ti))
 
     builtin "now",
         alias       = unaliased, 
